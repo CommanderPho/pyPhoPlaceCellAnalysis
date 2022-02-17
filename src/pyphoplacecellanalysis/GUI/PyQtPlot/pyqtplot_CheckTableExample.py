7165bef57fec346c3a04254d02ae587c281f1924
Pho Hale <CommanderPho@users.noreply.github.com>
2022-02-17 10:18:50 -0500

    working neuron types in widget table

--- src/pyphoplacecellanalysis/GUI/PyQtPlot/pyqtplot_CheckTableExample.py
+++ src/pyphoplacecellanalysis/GUI/PyQtPlot/pyqtplot_CheckTableExample.py
@@ -1,19 +1,49 @@
+import importlib
+import sys
+from pathlib import Path
 import pyqtgraph as pg
 from pyqtgraph.Qt import QtCore, QtGui, QtWidgets
 import numpy as np
 
 from pyqtgraph.widgets.FeedbackButton import FeedbackButton
 
+# NeuroPy (Diba Lab Python Repo) Loading
+try:
+    from neuropy import core
+
+    importlib.reload(core)
+except ImportError:
+    sys.path.append(r"C:\Users\Pho\repos\NeuroPy")  # Windows
+    # sys.path.append('/home/pho/repo/BapunAnalysis2021/NeuroPy') # Linux
+    # sys.path.append(r'/Users/pho/repo/Python Projects/NeuroPy') # MacOS
+    print("neuropy module not found, adding directory to sys.path. \n >> Updated sys.path.")
+    from neuropy import core
+
+from neuropy.core.neurons import NeuronType
+
 def checkTableWidgetExample(title='PhoCheckTableWidgetExampleApp'):
     app = pg.mkQApp(title)
     # w = pg.CheckTable(['Column 1','Column 2','Column 3'])
-    col_labels = ['pre', 'maze1', 'post1', 'maze2', 'post2']
+    # col_labels = ['pre', 'maze1', 'post1', 'maze2', 'post2']
+    # col_labels = ['pre', 'maze1', 'post1', 'maze2', 'post2']
+    # col_labels = NeuronType.longClassNames()
+    
+    col_labels = NeuronType.__members__
+    
+    
     w = pg.CheckTable(col_labels)
     w.layout.setSpacing(10)
 
     def on_add_row_clicked(evt):
         w.addRow('New')
         
+    def on_table_check_changed(row, col, state):
+        # note row: int, col: str, state: 0 for unchecked or 2 for checked
+        print(f'on_table_check_changed(row: {row}, col: {col}, state: {state})')
+       
+    w.sigStateChanged.connect(on_table_check_changed)
+    
+     
     window = QtWidgets.QWidget()
     layout = QtGui.QVBoxLayout()
     layout.addWidget(w)
