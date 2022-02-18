1c66a298d1a3949548b31ba5cb845824e512dda7
Pho Hale <CommanderPho@users.noreply.github.com>
2022-02-18 07:22:29 -0500

    successfully generates a simple line plot that can have additional data added via calling the .plot function

---
+++ src/pyphoplacecellanalysis/GUI/PyQtPlot/pyqtplot_SimplePlot.py
@@ -0,0 +1,24 @@
+import pyqtgraph as pg
+from pyqtgraph.Qt import QtCore, QtGui, QtWidgets
+import numpy as np
+
+def plot_simple_graph(y=np.random.normal(size=100)):
+    app = pg.mkQApp("Plotting Example")
+    #mw = QtGui.QMainWindow()
+    #mw.resize(800,800)
+    
+    win = pg.GraphicsLayoutWidget(show=True, title="Basic plotting examples")
+    win.resize(1000,600)
+    win.setWindowTitle('pyqtgraph example: Plotting')
+    
+    # Enable antialiasing for prettier plots
+    pg.setConfigOptions(antialias=True)
+    
+    p1 = win.addPlot(title="Basic array plotting", y=y)
+    
+    return [p1], win, app
+
+
+if __name__ == '__main__':
+    # win, app = plot_simple_graph()
+    pg.exec()
