871efad7bff9443d8a74148b479bba087e9d6877
Pho Hale <CommanderPho@users.noreply.github.com>
2022-03-01 19:43:40 -0500

    In the middle of implementing a Spike3DRaster class that presents a scrollabar for updating the window displayed in the raster plot.

--- src/pyphoplacecellanalysis/GUI/PyQtPlot/pyqtplot_RasterPlot.py
+++ src/pyphoplacecellanalysis/GUI/PyQtPlot/pyqtplot_RasterPlot.py
@@ -10,6 +10,17 @@ from pyphocorehelpers.indexing_helpers import interleave_elements, partition
 from PhoPositionalData.plotting.visualization_window import VisualizationWindow # Used to build "Windows" into the data points such as the window defining the fixed time period preceeding the current time where spikes had recently fired, etc.
 from numpy.lib.stride_tricks import sliding_window_view
 
+# from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QGridLayout, QHBoxLayout, QSlider, QCheckBox 
+
+
+""" This example from the internet displays a dark-themed MainWindow containing an interactive pyqtgraph plot that allows you to inspect eigenvalues and drag an animated slider. """
+
+import qdarkstyle
+import numpy as np
+import time
+
+
+# Note that these raster plots could implement some variant of HideShowSpikeRenderingMixin, SpikeRenderingMixin, etc but these classes frankly suck. 
 
 def plot_raster_plot(x=np.arange(100), y=np.random.normal(size=100)):
     
@@ -110,6 +121,7 @@ def _display_pyqtgraph_raster_plot(curr_spikes_df, debug_print=False):
     # app.exec_()
 
 def _compute_windowed_spikes_raster(curr_spikes_df, render_window_duration=6.0):
+    """ TODO: Not yet implemented: """
     # curr_spikes_df
     
     
@@ -130,10 +142,227 @@ def _compute_windowed_spikes_raster(curr_spikes_df, render_window_duration=6.0):
     unit_split_spikes_df = partition(curr_spikes_df, 'unit_id') # split on the unitID
     
     
+    
+class SliderRunner(QtCore.QThread):
+    update_signal = QtCore.pyqtSignal()
+
+    def __init__(self):
+        QtCore.QThread.__init__(self)
+
+    def run(self):
+        while(True):
+            self.update_signal.emit()
+            time.sleep(.03)
+            
+                
+
+class Spike3DRaster(QtWidgets.QWidget):
+    """docstring for 3d_raster_app."""
+    def __init__(self, *args, **kwargs):
+        super(Spike3DRaster, self).__init__(*args, **kwargs)
+        # Initialize member variables:
+        self.slidebar_val = 0
+        # Configure pyqtgraph config:
+        pg.setConfigOptions(antialias = True)
+        pg.setConfigOption('background', "#1B1B1B")
+        pg.setConfigOption('foreground', "#727272")
+        # build the UI components:
+        self.buildUI()
+
+        
+    def buildUI(self):
+        
+
+        layout = QtWidgets.QGridLayout()
+        layout.setContentsMargins(0, 0, 0, 0)
+        layout.setVerticalSpacing(0)
+        layout.setHorizontalSpacing(0)
+        self.setStyleSheet("background : #1B1B1B; color : #727272")
+        
+        
+        
+        ##### Main Raster Plot Content Top ##########
+        w = gl.GLViewWidget()
+        w.show()
+        w.resize(1000,600)
+        w.setWindowTitle('pyqtgraph: 3D Raster Spikes Plotting')
+        w.setCameraPosition(distance=40)
+        layout.addWidget(w, 0, 0) # add the GLViewWidget to the layout
+        
+        #### Build Graphics Objects #####
+        self._buildGraphics(w) # pass the GLViewWidget
+        
+        ####    Slide Bar Left #######
+        panel_slide_bar = QtWidgets.QWidget()
+        layout_slide_bar = QtWidgets.QHBoxLayout()
+        layout_slide_bar.setContentsMargins(6, 3, 4, 4)
+
+        panel_slide_bar.setLayout(layout_slide_bar)
+
+        self.slider = QtWidgets.QSlider(QtCore.Qt.Horizontal)
+        self.slider.setSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Preferred)
+        self.slider.setRange(0, 100)
+        self.slider.setSingleStep(2)
+        self.slider.setValue(0)
+        self.slider.valueChanged.connect(self.slider_val_changed)
+
+        layout_slide_bar.addWidget(self.slider)
+
+        self.btn_slide_run = QtWidgets.QPushButton(">")
+        self.btn_slide_run.setMinimumHeight(25)
+        self.btn_slide_run.setMinimumWidth(30)
+        self.btn_slide_run.tag = "paused"
+        self.btn_slide_run.clicked.connect(self.btn_slide_run_clicked)
+
+        layout_slide_bar.addWidget(self.btn_slide_run)
+        layout.addWidget(panel_slide_bar, 1, 0) 
+        
+        self.setLayout(layout)
+        self.sliderThread = SliderRunner()
+        self.sliderThread.update_signal.connect(self.increase_slider_val)
+      
+    def _buildGraphics(self, w):
+        # Add axes planes:
+        # X-plane:
+        gx = gl.GLGridItem(color=(255, 155, 155, 76.5))
+        gx.rotate(90, 0, 1, 0)
+        gx.translate(-half_render_window_duration, 0, 0) # shift backwards
+        gx.setSize(20, n_full_cell_grid) # std size in z-dir, n_cell size across
+        gx.setSpacing(10.0, 1) 
+        w.addItem(gx)
+        
+        # Y-plane:
+        gy = gl.GLGridItem(color=(155, 255, 155, 76.5))
+        gy.rotate(90, 1, 0, 0)
+        # gy.translate(0, -10, 0)
+        gy.translate(0, -n_half_cells, 0) # offset by half the number of units in the -y direction
+        gy.setSize(render_window_duration, 20)
+        # gy.setSpacing(1, 1)
+        w.addItem(gy)
+        
+        # XY-plane (with normal in z-dir):
+        gz = gl.GLGridItem(color=(155, 155, 255, 76.5))
+        gz.translate(0, 0, -10) # Shift down by 10 units in the z-dir
+        gz.setSize(render_window_duration, n_full_cell_grid)
+        gz.setSpacing(20.0, 1)
+        # gz.setSize(n_full_cell_grid, n_full_cell_grid)
+        w.addItem(gz)
+        
+        
+         # Custom 3D raster plot:
+        y = np.linspace(-n_half_cells, n_half_cells, n) + 0.5 # add 0.5 so they're centered
+        
+        # Filter based on the current spikes window to show:        
+        curr_render_window_start_time = 30.0
+        curr_render_window_end_time = curr_render_window_start_time + render_window_duration
+        curr_time_windowed_spikes_df = curr_spikes_df[curr_spikes_df[curr_spikes_df.spikes.time_variable_name].between(curr_render_window_start_time, curr_render_window_end_time)]
+        
+        # Plot each unit one at a time:
+        for cell_id in unit_ids:
+            curr_color = pg.mkColor((cell_id, n*1.3))
+            # curr_color.SetAlpha(120) # alpha should be between 0-255
+            curr_color.setAlphaF(0.5)
+            print(f'cell_id: {cell_id}, curr_color: {curr_color.alpha()}')
+            
+            # Filter the dataframe using that column and value from the list
+            curr_cell_df = curr_time_windowed_spikes_df[curr_time_windowed_spikes_df['unit_id']==cell_id].copy()
+            # curr_unit_id = curr_cell_df['unit_id'].to_numpy() # this will map to the y position
+            curr_spike_t = curr_cell_df[curr_cell_df.spikes.time_variable_name].to_numpy() # this will map 
+            yi = y[cell_id] # get the correct y-position for all spikes of this cell
+            # print(f'cell_id: {cell_id}, yi: {yi}')
+            # map the current spike times back onto the range of the window's (-half_render_window_duration, +half_render_window_duration) so they represent the x coordinate
+            curr_x = np.interp(curr_spike_t, (curr_render_window_start_time, curr_render_window_end_time), (-half_render_window_duration, +half_render_window_duration))
+            curr_paired_x = np.squeeze(interleave_elements(np.atleast_2d(curr_x).T, np.atleast_2d(curr_x).T))        
+            
+            # Z-positions:
+            # z = curr_spike_t[np.arange(100)] # get the first 20 spikes for each
+            spike_bottom_zs = np.full_like(curr_x, spike_start_z)
+            spike_top_zs = np.full_like(curr_x, spike_end_z)
+            curr_paired_spike_zs = np.squeeze(interleave_elements(np.atleast_2d(spike_bottom_zs).T, np.atleast_2d(spike_top_zs).T)) # alternating top and bottom z-positions
+        
+            # sp1 = gl.GLScatterPlotItem(pos=pos, size=size, color=color, pxMode=False)
+            # sp1.translate(5,5,0)
+            # w.addItem(sp1)
+            
+            # Build lines:
+            pts = np.column_stack([curr_paired_x, np.full_like(curr_paired_x, yi), curr_paired_spike_zs]) # the middle coordinate is the size of the x array with the value given by yi. yi must be the scalar for this cell.
+            # pts = np.column_stack([x, np.full_like(x, yi), z]) # the middle coordinate is the size of the x array with the value given by yi. yi must be the scalar for this cell.
+            # plt = gl.GLLinePlotItem(pos=pts, color=pg.mkColor((cell_id,n*1.3)), width=(cell_id+1)/10., antialias=True)
+            plt = gl.GLLinePlotItem(pos=pts, color=curr_color, width=0.5, antialias=True, mode='lines') # mode='lines' means that each pair of vertexes draws a single line segement
+            w.addItem(plt)
+
+            # # Adds a helper widget that displays the x/y/z vector at the origin:
+            # ref_axes_indicator = gl.GLAxisItem()
+            # ref_axes_indicator.setSize(x=10.0, y=10.0, z=5.0)
+            # w.addItem(ref_axes_indicator)
+    
+    def btn_slide_run_clicked(self):
+        if self.btn_slide_run.tag == "paused" or self.slidebar_val == 1:
+            if self.slidebar_val == 1:
+                self.slider.setValue(0)
+            
+            self.btn_slide_run.setText("||")
+            self.btn_slide_run.tag = "running"
+            self.sliderThread.start()
+
+        elif self.btn_slide_run.tag == "running":
+            self.btn_slide_run.setText(">")
+            self.btn_slide_run.tag = "paused"
+            self.sliderThread.terminate()
+
+    def increase_slider_val(self):
+        slider_val = self.slider.value()
+        if slider_val < 100:
+            self.slider.setValue(slider_val + 1)
+        else:
+            print("thread ended..")
+            self.btn_slide_run.setText(">")
+            self.btn_slide_run.tag = "paused"
+            self.sliderThread.terminate()
+
+    def slider_val_changed(self, val):
+        self.slidebar_val = val / 100
+
+        #self.updateMatrix()
+        self.updateVector1(self.v1_x, self.v1_y)
+        self.updateVector2(self.v2_x, self.v2_y)
+
+        self.updateGrid()
+        self.updateCircle()
+        self.updateOutput()
+        
+    def computeTransform(self, x, y, t = None):
+        if t == None:
+            v1_x = (1 * (1 - self.slidebar_val)) + (self.v1_x * self.slidebar_val)
+            v1_y = (0 * (1 - self.slidebar_val)) + (self.v1_y * self.slidebar_val)
+
+            v2_y = (1 * (1 - self.slidebar_val)) + (self.v2_y * self.slidebar_val)
+            v2_x = (0 * (1 - self.slidebar_val)) + (self.v2_x * self.slidebar_val)
+        else:
+            v1_x = self.v1_x
+            v1_y = self.v1_y
+            v2_x = self.v2_x
+            v2_y = self.v2_y
+        return ((v1_x * x) + (v2_x * y), (v1_y * x) + (v2_y * y))
+
+    def set_spikes_data(self, curr_spikes_df):
+        unit_ids = np.unique(curr_spikes_df['unit_id'].to_numpy())
+        n = len(unit_ids)
+        render_window_duration = 60.0 # in seconds, 1minute by default
+        spike_start_z = -10.0
+        spike_end_z = 0.1
+        
+        n_half_cells = np.ceil(float(n)/2.0)
+        n_full_cell_grid = 2.0 * n_half_cells # could be one more than n
+        half_render_window_duration = np.ceil(float(render_window_duration)/2.0) # 10 by default
+        
+        print(f'plot_3d_raster_plot(...): unit_ids: {unit_ids}, n: {n}')
+    
+
 
 
 def plot_3d_raster_plot(curr_spikes_df):
-    """ plots a 3-dimensional raster plot for neural spiking data. """
+    """ independent plotting function: plots a 3-dimensional raster plot for neural spiking data. """
 # def plot_3d_raster_plot(x=np.linspace(-10,10,100), y=np.random.normal(size=100), z=None):
     # curr_unit_id = curr_spikes_df['unit_id'].to_numpy() # this will map to the y position
     # curr_spike_t = curr_spikes_df[curr_spikes_df.spikes.time_variable_name].to_numpy() # this will map to the depth dimension in 3D or x-pos in 2D
@@ -220,8 +449,6 @@ def plot_3d_raster_plot(curr_spikes_df):
         curr_color.setAlphaF(0.5)
         print(f'cell_id: {cell_id}, curr_color: {curr_color.alpha()}')
         
-        
-                        
         # Filter the dataframe using that column and value from the list
         curr_cell_df = curr_time_windowed_spikes_df[curr_time_windowed_spikes_df['unit_id']==cell_id].copy()
         # curr_unit_id = curr_cell_df['unit_id'].to_numpy() # this will map to the y position
