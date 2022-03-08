ed1afe1ae9a0dfcd433c609acc813cc2b3142e06
Pho Hale <CommanderPho@users.noreply.github.com>
2022-03-08 00:32:15 -0500

    almost working

--- src/pyphoplacecellanalysis/GUI/PyQtPlot/Flowchart/CustomNodes/Compute/PhoPythonEvalNode.py
+++ src/pyphoplacecellanalysis/GUI/PyQtPlot/Flowchart/CustomNodes/Compute/PhoPythonEvalNode.py
@@ -1,5 +1,13 @@
+import os
+import re
+from typing import final
 from pyqtgraph.Qt import QtGui, QtCore, QtWidgets
+translate = QtCore.QCoreApplication.translate
+
 from pyqtgraph.flowchart.library.common import Node
+from pyqtgraph.parametertree.parameterTypes.file import popupFilePicker
+from pyqtgraph.widgets.FileDialog import FileDialog
+from pyqtgraph import configfile
 
 from pyphocorehelpers.gui.PhoUIContainer import PhoUIContainer
 from pyphoplacecellanalysis.GUI.PyQtPlot.Flowchart.CustomNodes.MiscNodes.ExtendedCtrlNode import ExtendedCtrlNode
@@ -26,15 +34,27 @@ class PhoPythonEvalNode(Node):
     For a script, the text will be executed as the body of a function."""
     nodeName = 'PhoPythonEval'
     
+    sigFileLoaded = QtCore.Signal(object)
+    sigFileSaved = QtCore.Signal(object)
+    
+    
     def __init__(self, name):
+        # Setup member variables
+        self.filePath = None
+        self.currentFileName = None
+        
         Node.__init__(self, name, 
             terminals = {
                 'input': {'io': 'in', 'renamable': True, 'multiable': True},
                 'output': {'io': 'out', 'renamable': True, 'multiable': True},
             },
             allowAddInput=True, allowAddOutput=True)
+        
         self.ui = PhoUIContainer()
         self.buildUI()
+        # self.sigFileLoaded.connect(self.setCurrentFile)
+        self.sigFileSaved.connect(self.on_file_saved)
+        
 
 
     def buildUI(self):
@@ -68,6 +88,13 @@ class PhoPythonEvalNode(Node):
         # self.ui.loadSaveBtnWidget.setLayout(self.ui.metaBtnLayout)
         self.layout.addWidget(self.ui.loadSaveBtnWidget, 2, 0, 1, 2)
         self.ui.root.setLayout(self.layout)
+        
+        # Build custom context menu:
+        self.contextMenu = QtWidgets.QMenu()
+        self.contextMenu.addAction(translate("PhoPythonEval", 'Custom Copy Selection')).triggered.connect(self.copySel)
+        self.contextMenu.addAction(translate("PhoPythonEval", 'Custom Copy All')).triggered.connect(self.copyAll)
+        self.contextMenu.addAction(translate("PhoPythonEval", 'Custom Save Selection')).triggered.connect(self.saveSel)
+        self.contextMenu.addAction(translate("PhoPythonEval", 'Custom Save All')).triggered.connect(self.saveAll)
 
         
     def ctrlWidget(self):
@@ -98,19 +125,61 @@ class PhoPythonEvalNode(Node):
     def saveAsCustomNode(self):
         """ save the current node's code and inputs/outputs to a file for this node. """
         # Get the node's current state:
-        curr_state = self.saveState()
+        # curr_state = self.saveState()
+        # self.save(curr_state)
+        try:
+            if self.currentFileName is None:
+                newFile = self.saveFile()
+            else:
+                newFile = self.saveFile(suggestedFileName=self.currentFileName)
+            #self.ui.saveAsBtn.success("Saved.")
+            #print "Back to saveAsClicked."
+        except:
+            # self.ui.saveBtn.failure("Error")
+            raise
         
-        pass
-    
-    
+
     def loadCustomNodeCode(self):
         """ load - get the loaded state from file or whereever """
         # TODO: get the loaded state from file
-        loaded_state = None
+        # loaded_state = None
         # loaded_state = 
-        self.restoreState(loaded_state)
+        # self.restoreState(loaded_state)
+        newFile = self.loadFile()
     
     
+    # Node functions:
+    # def terminalRenamed(self, term, oldName):
+    #     Node.terminalRenamed(self, term, oldName)
+    #     item = term.joinItem
+    #     item.setText(0, term.name())
+    #     self.update()
+        
+    # def nodeRenamed(self, node, oldName):
+    #     del self._nodes[oldName]
+    #     self._nodes[node.name()] = node
+    #     self.widget().nodeRenamed(node, oldName)
+    #     self.sigChartChanged.emit(self, 'rename', node)
+        
+    # def chartGraphicsItem(self):
+    #     """Return the graphicsItem that displays the internal nodes and
+    #     connections of this flowchart.
+        
+    #     Note that the similar method `graphicsItem()` is inherited from Node
+    #     and returns the *external* graphical representation of this flowchart."""
+    #     return self.viewBox
+
+    def clear(self):
+        """Remove all nodes from this flowchart except the original input/output nodes.
+        """
+        #self.clearTerminals()
+        self.ui.text.clear()
+    
+    def clearTerminals(self):
+        Node.clearTerminals(self)
+        
+        
+    # Process 
         
     def process(self, display=True, **args):
         l = locals()
@@ -137,8 +206,145 @@ class PhoPythonEvalNode(Node):
         #state['terminals'] = self.saveTerminals()
         return state
         
-    def restoreState(self, state):
-        Node.restoreState(self, state)
-        self.setCode(state['text'])
-        self.restoreTerminals(state['terminals'])
-        self.update()
+    def restoreState(self, state, clear=False):
+        self.blockSignals(True)
+        try:
+            if clear:
+                self.clear()
+            Node.restoreState(self, state)
+            self.setCode(state['text'])
+            self.restoreTerminals(state['terminals'])
+            self.update()
+        finally:
+            self.blockSignals(False)
+        
+        
+    # UI Handling Functions:
+    def contextMenuEvent(self, ev):
+        self.contextMenu.popup(ev.globalPos())
+        
+    def serialize(self):
+        """Convert entire table (or just selected area) into tab-separated text values"""
+        state = self.saveState()
+        if 'terminals' not in state:
+            state['terminals'] = self.saveTerminals()
+        # TODO: convert to JSON or something?
+        return state
+
+
+    def copySel(self):
+        """Copy selected data to clipboard."""
+        QtWidgets.QApplication.clipboard().setText(self.serialize())
+
+    def copyAll(self):
+        """Copy all data to clipboard."""
+        QtWidgets.QApplication.clipboard().setText(self.serialize())
+
+    def saveSel(self):
+        """Save selected data to file."""
+        self.save(self.serialize())
+
+    def saveAll(self):
+        """Save all data to file."""
+        self.save(self.serialize())
+        
+        
+        
+    # Extended GUI        
+    # def save(self, data):
+    #     """ displays the user save prompt and writes out the file """
+    #     # getSaveFileName(parent: QWidget = None, caption: str = '', directory: str = '', filter: str = '', initialFilter: str = '', options: Union[QFileDialog.Options, QFileDialog.Option] = 0)
+    #     fileName = QtWidgets.QFileDialog.getSaveFileName(self.ui.root,
+    #         f"{translate('PhoPythonEval', 'Save As')}...",
+    #         'EXTERNAL/CustomNodes',
+    #         f"{translate('PhoPythonEval', 'Custom Eval Node')} (*.pEval)"
+    #     )
+    #     if isinstance(fileName, tuple):
+    #         fileName = fileName[0]  # Qt4/5 API difference
+    #     if fileName == '':
+    #         return
+    #     with open(fileName, 'w') as fd:
+    #         # write out the data
+    #         fd.write(data)
+            
+            
+    def loadFile(self, fileName=None, startDir=None):
+        """Load a Custom Eval Node (``*.pEval``) file.
+        """
+        if fileName is None:
+            if startDir is None:
+                startDir = self.filePath
+            if startDir is None:
+                startDir = '.'
+            self.fileDialog = FileDialog(None, "Load Custom Eval Node..", startDir, "Custom Eval Node (*.pEval)")
+            self.fileDialog.show()
+            self.fileDialog.fileSelected.connect(self.loadFile)
+            return
+            ## NOTE: was previously using a real widget for the file dialog's parent, but this caused weird mouse event bugs..
+        state = configfile.readConfigFile(fileName)
+        self.restoreState(state, clear=True)
+        self.sigFileLoaded.emit(fileName)
+
+    def saveFile(self, fileName=None, startDir=None, suggestedFileName='custom_node.pEval'):
+        """Save this Custom Eval Node to a .pEval file
+        """
+        if fileName is None:
+            if startDir is None:
+                startDir = self.filePath
+            if startDir is None:
+                startDir = '.'
+            self.fileDialog = FileDialog(None, "Save Custom Eval Node..", startDir, "Custom Eval Node (*.pEval)")
+            self.fileDialog.setDefaultSuffix("pEval")
+            self.fileDialog.setAcceptMode(QtWidgets.QFileDialog.AcceptMode.AcceptSave) 
+            self.fileDialog.show()
+            self.fileDialog.fileSelected.connect(self.saveFile)
+            return
+        configfile.writeConfigFile(self.saveState(), fileName)
+        self.sigFileSaved.emit(fileName)
+
+        
+            
+    
+    # def _retrieveFileSelection_gui(self):
+    #     curVal = self.param.value()
+    #     if isinstance(curVal, list) and len(curVal):
+    #         # All files should be from the same directory, in principle
+    #         # Since no mechanism exists for preselecting multiple, the most sensible
+    #         # thing is to select nothing in the preview dialog
+    #         curVal = curVal[0]
+    #         if os.path.isfile(curVal):
+    #             curVal = os.path.dirname(curVal)
+    #     opts = self.param.opts.copy()
+    #     useDir = curVal or opts.get('directory') or os.getcwd()
+    #     startDir = os.path.abspath(useDir)
+    #     if os.path.isfile(startDir):
+    #         opts['selectFile'] = os.path.basename(startDir)
+    #         startDir = os.path.dirname(startDir)
+    #     if os.path.exists(startDir):
+    #         opts['directory'] = startDir
+    #     if opts.get('windowTitle') is None:
+    #         opts['windowTitle'] = self.param.title()
+
+    #     fname = popupFilePicker(None, **opts)
+    #     if not fname:
+    #         return
+    #     self.param.setValue(fname)
+        
+    # Events
+    @QtCore.pyqtSlot(object)
+    def setCurrentFile(self, fileName):
+        print(f'.setCurrentFile(fileName: {fileName})')
+        self.currentFileName = fileName
+        # if fileName is None:
+        #     self.ui.fileNameLabel.setText("<b>[ new ]</b>")
+        # else:
+        #     self.ui.fileNameLabel.setText("<b>%s</b>" % os.path.split(self.currentFileName)[1])
+        # self.resizeEvent(None)
+        return
+        
+    @QtCore.pyqtSlot(object)
+    def on_file_saved(self, fileName):
+        print(f'.on_file_saved(fileName: {fileName})')
+        self.setCurrentFile(fileName)
+        # self.ui.saveBtn.success("Saved.")
+        return
