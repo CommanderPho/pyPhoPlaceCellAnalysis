0cf3e8db8be204307e42d18a38295f67f514772d
Pho Hale <CommanderPho@users.noreply.github.com>
2022-03-17 08:58:42 -0400

    refactored most of the playback controls out into another mixin (not general though), meaning that it shouldn't be too hard to enable/disable these components where I want them

--- src/pyphoplacecellanalysis/GUI/PyQtPlot/Widgets/Mixins/RenderWindowControlsMixin.py
+++ src/pyphoplacecellanalysis/GUI/PyQtPlot/Widgets/Mixins/RenderWindowControlsMixin.py
@@ -3,12 +3,175 @@ from pyqtgraph.Qt import QtCore, QtGui, QtWidgets
 import pyqtgraph.opengl as gl # for 3D raster plot
 
 import numpy as np
+import qtawesome as qta
+from pyphocorehelpers.gui.Qt.ToggleButton import ToggleButtonModel, ToggleButton
+from pyphocorehelpers.gui.Qt.HighlightedJumpSlider import HighlightedJumpSlider
 
 
+
+class RenderPlaybackControlsMixin:
+    """ 
+
+    """
+    
+    WantsPlaybackControls = False
+    
+    def setup_render_playback_controls(self):
+        """ Build the bottom playback controls bar:
+        
+        creates self.ui.panel_bottom_bar
+        
+        """
+        ####################################################
+        ####  Controls Bar Bottom #######
+        ####    Slide Bar Bottom #######
+        self.ui.panel_bottom_bar = QtWidgets.QWidget()
+        self.ui.panel_bottom_bar.setSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Preferred)
+        self.ui.panel_bottom_bar.setMaximumHeight(50.0) # maximum height
+        
+        # Try to make the bottom widget bar transparent:
+        self.ui.panel_bottom_bar.setWindowFlags(self.windowFlags() | QtCore.Qt.FramelessWindowHint)
+        self.ui.panel_bottom_bar.setAttribute(QtCore.Qt.WA_TranslucentBackground)
+        self.ui.panel_bottom_bar.setStyleSheet("background:transparent;")
+        
+        # Playback Slider Bottom Bar:
+        self.ui.layout_slide_bar = QtWidgets.QHBoxLayout()
+        self.ui.layout_slide_bar.setContentsMargins(6, 3, 4, 4)
+        self.ui.panel_bottom_bar.setLayout(self.ui.layout_slide_bar)
+
+        # New Button: Play/Pause
+        self.ui.button_play_pause = ToggleButton()
+        self.ui.button_play_pause.setMinimumHeight(25)
+        self.ui.button_play_pause.setMinimumWidth(30)
+        self.ui.play_pause_model = ToggleButtonModel(None, self)
+        self.ui.play_pause_model.setStateMap(
+            {
+                True: {
+                    "text": "",
+                    "icon": qta.icon("fa.play", scale_factor=0.7, color='white', color_active='orange')
+                },
+                False: {
+                    "text": "",
+                    "icon": qta.icon("fa.pause", scale_factor=0.7, color='white', color_active='orange')
+                }
+            }
+        )
+        self.ui.button_play_pause.setModel(self.ui.play_pause_model)
+        self.ui.button_play_pause.clicked.connect(self.play_pause)
+        self.ui.layout_slide_bar.addWidget(self.ui.button_play_pause)
+        
+        
+        # Playback Slider:
+        # # self.ui.slider = QtWidgets.QSlider(QtCore.Qt.Horizontal)
+        # # self.ui.slider = HighlightedJumpSlider(QtCore.Qt.Horizontal)
+        # self.ui.slider = HighlightedJumpSlider()
+        # self.ui.slider.setSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Preferred)
+        # # self.ui.slider.setFocusPolicy(Qt.NoFocus) # removes ugly focus rectangle frm around the slider
+        # self.ui.slider.setRange(0, 100)
+        # self.ui.slider.setSingleStep(1)
+        # self.ui.slider.setValue(0)
+        # # self.ui.slider.valueChanged.connect(self.slider_val_changed)
+        # # sliderMoved vs valueChanged? vs sliderChange?
+        # self.ui.layout_slide_bar.addWidget(self.ui.slider)
+        
+            
+        # Button: Reverse:
+        self.ui.btnReverse = QtWidgets.QPushButton("Reverse")
+        self.ui.btnReverse.setCheckable(True) # set checkable to make it a toggle button
+        self.ui.btnReverse.setMinimumHeight(25)
+        self.ui.btnReverse.setMinimumWidth(30)
+        self.ui.btnReverse.setStyleSheet("background-color : lightgrey") # setting default color of button to light-grey
+        self.ui.btnReverse.clicked.connect(self.on_reverse_held)
+        self.ui.layout_slide_bar.addWidget(self.ui.btnReverse)
+        
+        # Button: Jump Left:
+        self.ui.btnLeft = QtWidgets.QPushButton("<-")
+        self.ui.btnLeft.setMinimumHeight(25)
+        self.ui.btnLeft.setMinimumWidth(30)
+        self.ui.btnLeft.clicked.connect(self.on_jump_left)
+        self.ui.layout_slide_bar.addWidget(self.ui.btnLeft)
+        
+        # Button: Jump Right:
+        self.ui.btnRight = QtWidgets.QPushButton("->")
+        self.ui.btnRight.setMinimumHeight(25)
+        self.ui.btnRight.setMinimumWidth(30)
+        self.ui.btnRight.clicked.connect(self.on_jump_right)
+        self.ui.layout_slide_bar.addWidget(self.ui.btnRight)
+        
+        # Add the bottom bar:
+        self.ui.layout.addWidget(self.ui.panel_bottom_bar, 1, 0, 1, 2) # Spans both columns (lays under the right_controls panel)
+        
+
+    
+    # Called when the play/pause button is clicked:
+    def play_pause(self):
+        # THIS IS NOT TRUE: the play_pause_model uses inverted logic, so we negate the current state value to determine if is_playing
+        is_playing = self.ui.play_pause_model.getState()
+        # print(f'is_playing: {is_playing}')
+        
+        if (not is_playing) or self.slidebar_val == 1:
+            if self.slidebar_val == 1:
+                # self.ui.slider.setValue(0)
+                pass
+            # self.play_pause_model.setState(not is_playing)
+            self.animationThread.start()
+
+        else:
+            # self.play_pause_model.setState(not is_playing)
+            self.animationThread.terminate()
+            
+        
+        # self.ui.play_pause_model.blockSignals(True)
+        # self.ui.play_pause_model.setState(not is_playing)
+        # self.ui.play_pause_model.blockSignals(False)
+        
+        # if self.ui.btn_slide_run.tag == "paused" or self.slidebar_val == 1:
+        #     if self.slidebar_val == 1:
+        #         self.ui.slider.setValue(0)
+            
+        #     self.ui.btn_slide_run.setText("||")
+        #     self.ui.btn_slide_run.tag = "running"
+        #     self.animationThread.start()
+
+        # elif self.ui.btn_slide_run.tag == "running":
+        #     self.ui.btn_slide_run.setText(">")
+        #     self.ui.btn_slide_run.tag = "paused"
+        #     self.animationThread.terminate()
+
+
+    
+
+    def on_jump_left(self):
+        # Skip back some frames
+        self.shift_animation_frame_val(-5)
+        
+    def on_jump_right(self):
+        # Skip forward some frames
+        self.shift_animation_frame_val(5)
+        
+
+    def on_reverse_held(self):
+        # Change the direction of playback by changing the sign of the updating.
+        # if button is checked    
+        self.is_playback_reversed = self.ui.btnReverse.isChecked()
+        if self.ui.btnReverse.isChecked():
+            # setting background color to light-blue
+            self.ui.btnReverse.setStyleSheet("background-color : lightblue")
+  
+        # if it is unchecked
+        else:
+            # set background color back to light-grey
+            self.ui.btnReverse.setStyleSheet("background-color : lightgrey")
+    
+        
+        
 class RenderWindowControlsMixin:
     """ 
     # self.ui.spinBoxCurrentFrame.blockSignals(True)
-    """    
+    """
+    
+    WantsRenderWindowControls = False
+    
     def setup_render_window_controls(self):
         """ Build the right controls bar:
         
@@ -73,6 +236,12 @@ class RenderWindowControlsMixin:
         self.ui.layout_right_bar.addWidget(self.ui.spinRenderWindowDuration)
 
         self.ui.layout_right_bar.addSpacing(50)
+        # verticalSpacer = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
+        # self.ui.layout_right_bar.addWidget(verticalSpacer)
+        
+        # Add the right controls bar:
+        self.ui.layout.addWidget(self.ui.right_controls_panel, 0, 1, 2, 1) # Span both rows
+        
         
     def disable_render_window_controls(self):
         self.ui.spinAnimationTimeStep.blockSignals(True)
--- src/pyphoplacecellanalysis/GUI/PyQtPlot/Widgets/SpikeRasterBase.py
+++ src/pyphoplacecellanalysis/GUI/PyQtPlot/Widgets/SpikeRasterBase.py
@@ -17,8 +17,7 @@ from pyphoplacecellanalysis.General.Mixins.SpikesRenderingBaseMixin import Spike
 
 from pyphocorehelpers.indexing_helpers import interleave_elements, partition
 from pyphocorehelpers.gui.PhoUIContainer import PhoUIContainer
-from pyphocorehelpers.gui.Qt.ToggleButton import ToggleButtonModel, ToggleButton
-from pyphocorehelpers.gui.Qt.HighlightedJumpSlider import HighlightedJumpSlider
+
 
 # import qdarkstyle
 
@@ -27,7 +26,7 @@ from pyphoplacecellanalysis.General.DataSeriesToSpatial import DataSeriesToSpati
 from pyphoplacecellanalysis.GUI.PyQtPlot.Widgets.GLDebugAxisItem import GLDebugAxisItem
 from pyphoplacecellanalysis.GUI.PyQtPlot.Widgets.GLViewportOverlayPainterItem import GLViewportOverlayPainterItem
 
-from pyphoplacecellanalysis.GUI.PyQtPlot.Widgets.Mixins.RenderWindowControlsMixin import RenderWindowControlsMixin
+from pyphoplacecellanalysis.GUI.PyQtPlot.Widgets.Mixins.RenderWindowControlsMixin import RenderWindowControlsMixin, RenderPlaybackControlsMixin
 
 
 """ 
@@ -91,7 +90,7 @@ class UpdateRunner(QtCore.QThread):
             
                 
 
-class SpikeRasterBase(NeuronIdentityAccessingMixin, SpikeRenderingBaseMixin, SpikesWindowOwningMixin, SpikesDataframeOwningMixin, RenderWindowControlsMixin, QtWidgets.QWidget):
+class SpikeRasterBase(NeuronIdentityAccessingMixin, SpikeRenderingBaseMixin, SpikesWindowOwningMixin, SpikesDataframeOwningMixin, RenderPlaybackControlsMixin, RenderWindowControlsMixin, QtWidgets.QWidget):
     """ Displays a raster plot with the spikes occuring along a plane. 
     
     Usage:
@@ -239,6 +238,10 @@ class SpikeRasterBase(NeuronIdentityAccessingMixin, SpikeRenderingBaseMixin, Spi
         self._spikes_window = SpikesDataframeWindow(spikes_df, window_duration=window_duration, window_start_time=window_start_time)
         
         # Config
+        self.params.wantsRenderWindowControls = SpikeRasterBase.WantsRenderWindowControls
+        self.params.wantsPlaybackControls = SpikeRasterBase.WantsPlaybackControls
+        
+        
         self._playback_update_frequency = SpikeRasterBase.PlaybackUpdateFrequency
         self.is_speed_burst_mode_active = False
         self.speedBurstPlaybackRate = SpikeRasterBase.SpeedBurstPlaybackRate
@@ -341,95 +344,15 @@ class SpikeRasterBase(NeuronIdentityAccessingMixin, SpikeRenderingBaseMixin, Spi
         #### Build Graphics Objects #####
         self._buildGraphics()
         
-        ####################################################
-        ####  Controls Bar Bottom #######
-        ####    Slide Bar Bottom #######
-        self.ui.panel_slide_bar = QtWidgets.QWidget()
-        self.ui.panel_slide_bar.setSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Preferred)
-        self.ui.panel_slide_bar.setMaximumHeight(50.0) # maximum height
-        
-        # Try to make the bottom widget bar transparent:
-        self.ui.panel_slide_bar.setWindowFlags(self.windowFlags() | QtCore.Qt.FramelessWindowHint)
-        self.ui.panel_slide_bar.setAttribute(QtCore.Qt.WA_TranslucentBackground)
-        self.ui.panel_slide_bar.setStyleSheet("background:transparent;")
-        
-        # Playback Slider Bottom Bar:
-        self.ui.layout_slide_bar = QtWidgets.QHBoxLayout()
-        self.ui.layout_slide_bar.setContentsMargins(6, 3, 4, 4)
-        self.ui.panel_slide_bar.setLayout(self.ui.layout_slide_bar)
+        if self.params.wantsPlaybackControls:
+            # Build the bottom playback controls bar:
+            self.setup_render_playback_controls()
 
-        # New Button: Play/Pause
-        self.ui.button_play_pause = ToggleButton()
-        self.ui.button_play_pause.setMinimumHeight(25)
-        self.ui.button_play_pause.setMinimumWidth(30)
-        self.ui.play_pause_model = ToggleButtonModel(None, self)
-        self.ui.play_pause_model.setStateMap(
-            {
-                True: {
-                    "text": "",
-                    "icon": qta.icon("fa.play", scale_factor=0.7, color='white', color_active='orange')
-                },
-                False: {
-                    "text": "",
-                    "icon": qta.icon("fa.pause", scale_factor=0.7, color='white', color_active='orange')
-                }
-            }
-        )
-        self.ui.button_play_pause.setModel(self.ui.play_pause_model)
-        self.ui.button_play_pause.clicked.connect(self.play_pause)
-        self.ui.layout_slide_bar.addWidget(self.ui.button_play_pause)
-        
-        
-        # Playback Slider:
-        # # self.ui.slider = QtWidgets.QSlider(QtCore.Qt.Horizontal)
-        # # self.ui.slider = HighlightedJumpSlider(QtCore.Qt.Horizontal)
-        # self.ui.slider = HighlightedJumpSlider()
-        # self.ui.slider.setSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Preferred)
-        # # self.ui.slider.setFocusPolicy(Qt.NoFocus) # removes ugly focus rectangle frm around the slider
-        # self.ui.slider.setRange(0, 100)
-        # self.ui.slider.setSingleStep(1)
-        # self.ui.slider.setValue(0)
-        # # self.ui.slider.valueChanged.connect(self.slider_val_changed)
-        # # sliderMoved vs valueChanged? vs sliderChange?
-        # self.ui.layout_slide_bar.addWidget(self.ui.slider)
-        
-            
-        # Button: Reverse:
-        self.ui.btnReverse = QtWidgets.QPushButton("Reverse")
-        self.ui.btnReverse.setCheckable(True) # set checkable to make it a toggle button
-        self.ui.btnReverse.setMinimumHeight(25)
-        self.ui.btnReverse.setMinimumWidth(30)
-        self.ui.btnReverse.setStyleSheet("background-color : lightgrey") # setting default color of button to light-grey
-        self.ui.btnReverse.clicked.connect(self.on_reverse_held)
-        self.ui.layout_slide_bar.addWidget(self.ui.btnReverse)
-        
-        # Button: Jump Left:
-        self.ui.btnLeft = QtWidgets.QPushButton("<-")
-        self.ui.btnLeft.setMinimumHeight(25)
-        self.ui.btnLeft.setMinimumWidth(30)
-        self.ui.btnLeft.clicked.connect(self.on_jump_left)
-        self.ui.layout_slide_bar.addWidget(self.ui.btnLeft)
-        
-        # Button: Jump Right:
-        self.ui.btnRight = QtWidgets.QPushButton("->")
-        self.ui.btnRight.setMinimumHeight(25)
-        self.ui.btnRight.setMinimumWidth(30)
-        self.ui.btnRight.clicked.connect(self.on_jump_right)
-        self.ui.layout_slide_bar.addWidget(self.ui.btnRight)
-        
-        # Build the right controls bar:
-        self.setup_render_window_controls() # creates self.ui.right_controls_panel
-        
-        # verticalSpacer = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
-        # self.ui.layout_right_bar.addWidget(verticalSpacer)
+        if self.params.wantsRenderWindowControls:
+            # Build the right controls bar:
+            self.setup_render_window_controls() # creates self.ui.right_controls_panel
         
         # addWidget(widget, row, column, rowSpan, columnSpan, Qt.Alignment alignment = 0)
-        
-        # Add the bottom bar:
-        self.ui.layout.addWidget(self.ui.panel_slide_bar, 1, 0, 1, 2) # Spans both columns (lays under the right_controls panel)
-        
-        # Add the right controls bar:
-        self.ui.layout.addWidget(self.ui.right_controls_panel, 0, 1, 2, 1) # Span both rows
          
         # Set the root (self) layout properties
         self.setLayout(self.ui.layout)
@@ -561,67 +484,6 @@ class SpikeRasterBase(NeuronIdentityAccessingMixin, SpikeRenderingBaseMixin, Spi
         raise NotImplementedError
         
 
-    
-    
-    # Called when the play/pause button is clicked:
-    def play_pause(self):
-        # THIS IS NOT TRUE: the play_pause_model uses inverted logic, so we negate the current state value to determine if is_playing
-        is_playing = self.ui.play_pause_model.getState()
-        # print(f'is_playing: {is_playing}')
-        
-        if (not is_playing) or self.slidebar_val == 1:
-            if self.slidebar_val == 1:
-                # self.ui.slider.setValue(0)
-                pass
-            # self.play_pause_model.setState(not is_playing)
-            self.animationThread.start()
-
-        else:
-            # self.play_pause_model.setState(not is_playing)
-            self.animationThread.terminate()
-            
-        
-        # self.ui.play_pause_model.blockSignals(True)
-        # self.ui.play_pause_model.setState(not is_playing)
-        # self.ui.play_pause_model.blockSignals(False)
-        
-        # if self.ui.btn_slide_run.tag == "paused" or self.slidebar_val == 1:
-        #     if self.slidebar_val == 1:
-        #         self.ui.slider.setValue(0)
-            
-        #     self.ui.btn_slide_run.setText("||")
-        #     self.ui.btn_slide_run.tag = "running"
-        #     self.animationThread.start()
-
-        # elif self.ui.btn_slide_run.tag == "running":
-        #     self.ui.btn_slide_run.setText(">")
-        #     self.ui.btn_slide_run.tag = "paused"
-        #     self.animationThread.terminate()
-
-
-    def on_jump_left(self):
-        # Skip back some frames
-        self.shift_animation_frame_val(-5)
-        
-    def on_jump_right(self):
-        # Skip forward some frames
-        self.shift_animation_frame_val(5)
-        
-
-    def on_reverse_held(self):
-        # Change the direction of playback by changing the sign of the updating.
-        # if button is checked    
-        self.is_playback_reversed = self.ui.btnReverse.isChecked()
-        if self.ui.btnReverse.isChecked():
-            # setting background color to light-blue
-            self.ui.btnReverse.setStyleSheet("background-color : lightblue")
-  
-        # if it is unchecked
-        else:
-            # set background color back to light-grey
-            self.ui.btnReverse.setStyleSheet("background-color : lightgrey")
-    
-    
     
     def shift_animation_frame_val(self, shift_frames: int):
         next_start_timestamp = self.spikes_window.active_window_start_time + (self.animation_playback_direction_multiplier * self.animation_time_step * float(shift_frames))
