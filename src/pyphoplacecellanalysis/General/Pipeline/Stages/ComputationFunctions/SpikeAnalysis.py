b1d358a62cc9acc7a04cfe743dbb8279d4403718
Pho Hale <CommanderPho@users.noreply.github.com>
2022-03-18 20:56:23 -0400

    Disabled new unfinished SpikeAnalysis computation function because it threw errors

--- src/pyphoplacecellanalysis/General/Pipeline/Stages/ComputationFunctions/SpikeAnalysis.py
+++ src/pyphoplacecellanalysis/General/Pipeline/Stages/ComputationFunctions/SpikeAnalysis.py
@@ -16,21 +16,21 @@ class SpikeAnalysisComputations(AllFunctionEnumeratingMixin):
     def _perform_spike_burst_detection_computation(computation_result: ComputationResult, debug_print=False):
         """ Computes periods when the cells are firing in bursts """
         
-        # Get sampling rate:
-        sampling_rate = computation_result.sess.recinfo.dat_sampling_rate # 32552
-        # sampling_rate = computation_result.sess.recinfo.eeg_sampling_rate # 1252
+        # # Get sampling rate:
+        # sampling_rate = computation_result.sess.recinfo.dat_sampling_rate # 32552
+        # # sampling_rate = computation_result.sess.recinfo.eeg_sampling_rate # 1252
         
         
         
-        is_burst = detect_bursts_dual_threshold(sig, fs=sampling_rate, dual_thresh=(1, 2), f_range=(8, 12))
+        # is_burst = detect_bursts_dual_threshold(sig, fs=sampling_rate, dual_thresh=(1, 2), f_range=(8, 12))
         
         
         
-        time_binned_position_resampler = build_position_df_resampled_to_time_windows(computation_result.sess.position.to_dataframe(), time_bin_size=computation_result.computation_config.time_bin_size) # TimedeltaIndexResampler
-        time_binned_position_df = time_binned_position_resampler.nearest() # an actual dataframe
-        computation_result.computed_data['burst_detection'] = {
-         'is_burst': is_burst
-        }
+        # time_binned_position_resampler = build_position_df_resampled_to_time_windows(computation_result.sess.position.to_dataframe(), time_bin_size=computation_result.computation_config.time_bin_size) # TimedeltaIndexResampler
+        # time_binned_position_df = time_binned_position_resampler.nearest() # an actual dataframe
+        # computation_result.computed_data['burst_detection'] = {
+        #  'is_burst': is_burst
+        # }
         """ 
         Access via ['burst_detection']['is_burst']
         Example:
