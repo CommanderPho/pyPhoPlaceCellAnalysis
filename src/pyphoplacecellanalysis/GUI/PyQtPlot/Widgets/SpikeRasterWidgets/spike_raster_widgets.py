94639d25737f4bad329c27bb7a849166de03b8a2
Pho Hale <CommanderPho@users.noreply.github.com>
2022-03-30 03:43:49 -0400

    factored out build_spike_3d_raster_with_2d_controls

--- src/pyphoplacecellanalysis/GUI/PyQtPlot/Widgets/SpikeRasterWidgets/spike_raster_widgets.py
+++ src/pyphoplacecellanalysis/GUI/PyQtPlot/Widgets/SpikeRasterWidgets/spike_raster_widgets.py
@@ -1,3 +1,39 @@
 # .SpikeRasterWidgets
 
-build_spike_3d_raster_with_2d_controls
+from pyqtgraph.Qt import QtCore, QtGui, QtWidgets
+from pyphocorehelpers.gui.Qt.widget_positioning_helpers import WidgetPositioningHelpers
+
+from pyphoplacecellanalysis.GUI.PyQtPlot.Widgets.SpikeRasterWidgets.SpikeRasterBase import SpikeRasterBase
+from pyphoplacecellanalysis.GUI.PyQtPlot.Widgets.SpikeRasterWidgets.Spike2DRaster import Spike2DRaster
+from pyphoplacecellanalysis.GUI.PyQtPlot.Widgets.SpikeRasterWidgets.Spike3DRaster import Spike3DRaster
+
+
+
+def build_spike_3d_raster_with_2d_controls(curr_spikes_df):
+    """ builds a 3D Raster plot for spikes with 2D controls in a separate window
+    
+    Usage:
+    
+    from pyphoplacecellanalysis.GUI.PyQtPlot.Widgets.SpikeRasterWidgets.spike_raster_widgets import build_spike_3d_raster_with_2d_controls
+    curr_epoch_name = 'maze1'
+    curr_epoch = curr_active_pipeline.filtered_epochs[curr_epoch_name] # <NamedTimerange: {'name': 'maze1', 'start_end_times': array([  22.26      , 1739.15336412])};>
+    curr_sess = curr_active_pipeline.filtered_sessions[curr_epoch_name]
+    curr_spikes_df = curr_sess.spikes_df
+    curr_computations_results = curr_active_pipeline.computation_results[curr_epoch_name]
+    # Build the output widget:
+    spike_raster_plt_3d, spike_raster_plt_2d, spike_3d_to_2d_window_connection = build_spike_3d_raster_with_2d_controls(curr_spikes_df)
+    
+    """
+    spike_raster_plt_3d = Spike3DRaster(curr_spikes_df, window_duration=15.0, window_start_time=30.0, neuron_colors=None)
+    # Connect the 2D window scrolled signal to the 3D plot's spikes_window.update_window_start_end function
+    spike_raster_plt_2d = Spike2DRaster(curr_spikes_df, window_duration=15.0, window_start_time=30.0, neuron_colors=None, parent=None) # setting , parent=spike_raster_plt_3d makes a single window
+    spike_raster_plt_2d.setWindowTitle('2D Raster Control Window')
+    spike_3d_to_2d_window_connection = spike_raster_plt_2d.window_scrolled.connect(spike_raster_plt_3d.spikes_window.update_window_start_end)
+    spike_raster_plt_3d.disable_render_window_controls()
+    # spike_raster_plt_3d.setWindowTitle('3D Raster with 2D Control Window')
+    spike_raster_plt_3d.setWindowTitle('Main 3D Raster Window')
+    WidgetPositioningHelpers.move_widget_to_top_left_corner(spike_raster_plt_3d, debug_print=False)
+    WidgetPositioningHelpers.align_3d_and_2d_windows(spike_raster_plt_3d, spike_raster_plt_2d) # Align the two windows
+    spike_raster_plt_2d.update_scroll_window_region(30.0, 30.0+15.0, block_signals=False)
+    return spike_raster_plt_3d, spike_raster_plt_2d, spike_3d_to_2d_window_connection
+
