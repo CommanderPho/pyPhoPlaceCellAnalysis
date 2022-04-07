0f5aaafe83a39986a5818907584b470eaeee974c
Pho Hale <CommanderPho@users.noreply.github.com>
2022-04-07 06:29:24 -0400

    added debug_print_identity_properties, debug_print_identity_properties_from_session to pyphoplacecellanalysis.General.Mixins.DisplayHelpers

--- src/pyphoplacecellanalysis/General/Mixins/DisplayHelpers.py
+++ src/pyphoplacecellanalysis/General/Mixins/DisplayHelpers.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 from pyphocorehelpers.print_helpers import WrappingMessagePrinter
 from pyphocorehelpers.plotting.mixins.figure_param_text_box import add_figure_text_box # for _display_add_computation_param_text_box
@@ -32,3 +33,58 @@ def _display_add_computation_param_text_box(fig, computation_config):
 
 
 
+def debug_print_identity_properties_from_session(curr_sess, debug_print=True):
+    """ 
+    Usage:
+        curr_epoch_name = 'maze1'
+        curr_epoch = curr_active_pipeline.filtered_epochs[curr_epoch_name] # <NamedTimerange: {'name': 'maze1', 'start_end_times': array([  22.26      , 1739.15336412])};>
+        curr_sess = curr_active_pipeline.filtered_sessions[curr_epoch_name]
+        curr_spikes_df = curr_sess.spikes_df
+        curr_computations_results = curr_active_pipeline.computation_results[curr_epoch_name]
+        debug_print_identity_properties_from_session(curr_sess)
+        
+        >> OUTPUT >>:
+            debug_print_identity_properties_from_session(curr_sess, ...): n_cells=40
+                curr_map_keys: [ 2  3  4  5  7  8  9 10 11 12 14 17 18 21 22 23 24 25 26 27 28 29 33 34 38 39 42 44 45 46 47 48 53 55 57 58 61 62 63 64]
+                curr_map_values: [ 0  1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39]
+        
+
+    """
+    curr_map = curr_sess.neurons.reverse_cellID_index_map
+    curr_map_keys = np.array(list(curr_map.keys()))
+    curr_map_values = np.array(list(curr_map.values()))
+    # print(len(curr_sess.neurons.reverse_cellID_index_map))
+    # print(curr_sess.neurons.reverse_cellID_index_map)
+    n_cells = len(curr_map_keys)
+    if debug_print:
+        print(f'debug_print_identity_properties_from_session(curr_sess, ...): n_cells={n_cells}')
+        print(f'\t\t curr_map_keys: {curr_map_keys}\n \t\t curr_map_values: {curr_map_values}')
+    return n_cells, curr_map_keys, curr_map_values
+
+def debug_print_identity_properties(spikes_df, debug_print=True):
+    """ 
+    Usage:
+        curr_epoch_name = 'maze1'
+        curr_epoch = curr_active_pipeline.filtered_epochs[curr_epoch_name] # <NamedTimerange: {'name': 'maze1', 'start_end_times': array([  22.26      , 1739.15336412])};>
+        curr_sess = curr_active_pipeline.filtered_sessions[curr_epoch_name]
+        curr_spikes_df = curr_sess.spikes_df
+        curr_computations_results = curr_active_pipeline.computation_results[curr_epoch_name]
+        debug_print_identity_properties(curr_spikes_df)
+        
+        >> OUTPUT >>:
+            debug_print_identity_properties(spikes_df, ...): n_cells=40
+                unit_ids: [ 0  1  2  3  5  6  7  8  9 10 12 15 16 19 20 21 22 23 24 25 26 27 31 32 36 37 40 42 43 44 45 46 51 53 55 56 59 60 61 62]
+                cell_ids: [ 2  3  4  5  7  8  9 10 11 12 14 17 18 21 22 23 24 25 26 27 28 29 33 34 38 39 42 44 45 46 47 48 53 55 57 58 61 62 63 64]
+            
+    """
+    
+    unit_ids = np.unique(spikes_df['unit_id'].to_numpy())
+    cell_ids = np.unique(spikes_df['aclu'].to_numpy())
+    n_cells = len(unit_ids)
+    if debug_print:
+        print(f'debug_print_identity_properties(spikes_df, ...): n_cells={n_cells}')
+        print(f'\t\t unit_ids: {unit_ids}\n \t\t cell_ids: {cell_ids}')
+    return n_cells, unit_ids, cell_ids
+    
+    
+    
