feddd6f50ad2c378c3e1ec672db25f52e8145c86
Pho Hale <CommanderPho@users.noreply.github.com>
2022-04-12 18:31:02 -0400

    adding new Qt Control Bar

---
+++ src/pyphoplacecellanalysis/GUI/Qt/PlaybackControls/Spike3DRasterBottomPlaybackControlBarBase.py
@@ -0,0 +1,32 @@
+# Spike3DRasterBottomPlaybackControlBar.py
+# Generated from c:\Users\pho\repos\pyPhoPlaceCellAnalysis\src\pyphoplacecellanalysis\GUI\PyQtPlot\Widgets\SpikeRasterWidgets\Spike3DRasterBottomPlaybackControlBar.ui automatically by PhoPyQtClassGenerator VSCode Extension
+import sys
+from datetime import datetime, timezone, timedelta
+import numpy as np
+from enum import Enum
+
+from PyQt5 import QtGui, QtWidgets, uic
+from PyQt5.QtWidgets import QMessageBox, QToolTip, QStackedWidget, QHBoxLayout, QVBoxLayout, QSplitter, QFormLayout, QLabel, QFrame, QPushButton, QTableWidget, QTableWidgetItem
+from PyQt5.QtWidgets import QApplication, QFileSystemModel, QTreeView, QWidget, QHeaderView
+from PyQt5.QtGui import QPainter, QBrush, QPen, QColor, QFont, QIcon
+from PyQt5.QtCore import Qt, QPoint, QRect, QObject, QEvent, pyqtSignal, pyqtSlot, QSize, QDir
+
+## IMPORTS:
+# from pyphoplacecellanalysis.GUI.Qt.PlaybackControls.Spike3DRasterBottomPlaybackControlBarBase import Spike3DRasterBottomPlaybackControlBarBase
+
+
+class Spike3DRasterBottomPlaybackControlBarBase(QWidget):
+	def __init__(self, parent=None):
+		super().__init__(parent=parent) # Call the inherited classes __init__ method
+		self.ui = uic.loadUi("../pyPhoPlaceCellAnalysis/src/pyphoplacecellanalysis/GUI/Qt/PlaybackControls/Spike3DRasterBottomPlaybackControlBar.ui", self) # Load the .ui file
+
+		self.initUI()
+		self.show() # Show the GUI
+
+
+	def initUI(self):
+		pass
+
+
+	def __str__(self):
+ 		return 
