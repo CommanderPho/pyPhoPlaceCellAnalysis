ffc79f44ec43a3ccde90f6837de07992c1fdf639
Pho Hale <CommanderPho@users.noreply.github.com>
2022-04-12 22:02:01 -0400

    cleanup, working

--- src/pyphoplacecellanalysis/GUI/PyQtPlot/Widgets/SpikeRasterWidgets/spike_raster_widgets.py
+++ src/pyphoplacecellanalysis/GUI/PyQtPlot/Widgets/SpikeRasterWidgets/spike_raster_widgets.py
@@ -41,7 +41,10 @@ def build_spike_3d_raster_with_2d_controls(curr_spikes_df, window_duration=15.0,
 
 def build_spike_3d_raster_vedo_with_2d_controls(curr_spikes_df, window_duration=15.0, window_start_time=30.0, neuron_colors=None, neuron_sort_order=None, extant_spike_raster_plt_3d_vedo = None):
     """ builds a vedo-based 3D Raster plot for spikes with 2D controls in a separate window
+
+    # NOTE: It appears this only works if the 2D Raster plot (pyqtgraph-based) is created before the Spike3DRaster_Vedo (Vedo-based). This is probably due to the pyqtgraph's instancing of the QtApplication.
     
+        
     Usage:
     
     from pyphoplacecellanalysis.GUI.PyQtPlot.Widgets.SpikeRasterWidgets.spike_raster_widgets import build_spike_3d_raster_with_2d_controls
@@ -75,47 +78,29 @@ def build_spike_3d_raster_vedo_with_2d_controls(curr_spikes_df, window_duration=
     # Update the 2D Scroll Region to the initial value:
     spike_raster_plt_2d.update_scroll_window_region(window_start_time, window_start_time+window_duration, block_signals=False)
     
-    
+
     # Build the 3D Vedo Raster plotter
     spike_raster_plt_3d_vedo = Spike3DRaster_Vedo(curr_spikes_df, window_duration=window_duration, window_start_time=window_start_time, neuron_colors=neuron_colors, neuron_sort_order=neuron_sort_order)
-    
-
-#     # Connect the 2D window scrolled signal to the 3D plot's spikes_window.update_window_start_end function
-#     spike_raster_plt_2d = Spike2DRaster(curr_spikes_df, window_duration=window_duration, window_start_time=window_start_time, neuron_colors=neuron_colors, neuron_sort_order=neuron_sort_order, parent=None) # setting , parent=spike_raster_plt_3d makes a single window
-    
-#     # Connect the 2D window scrolled signal to the 3D plot's spikes_window.update_window_start_end function
-#     # spike_raster_plt_2d = Spike2DRaster(spike_raster_plt_3d.spikes_df, window_duration=temp_window_duration, window_start_time=temp_window_start_time, neuron_colors=temp_neuron_colors, neuron_sort_order=temp_neuron_sort_order, parent=None) # setting , parent=spike_raster_plt_3d makes a single window
-#     spike_raster_plt_2d.setWindowTitle('2D Raster Control Window')
-
-    spike_3d_to_2d_window_connection = None
+    spike_raster_plt_3d_vedo.setWindowTitle('Main 3D (Vedo) Raster Window')
+    spike_raster_plt_3d_vedo.disable_render_window_controls()
+    # spike_raster_plt_3d.setWindowTitle('3D Raster with 2D Control Window')
     
     # Set the 3D Vedo plots' window to the current values of the 2d plot:
     spike_raster_plt_3d_vedo.spikes_window.update_window_start_end(spike_raster_plt_2d.spikes_window.active_time_window[0], spike_raster_plt_2d.spikes_window.active_time_window[1])
 
+    # Connect the 2D window scrolled signal to the 3D plot's spikes_window.update_window_start_end function
     spike_3d_to_2d_window_connection = None
-    # spike_3d_to_2d_window_connection = spike_raster_plt_2d.window_scrolled.connect(spike_raster_plt_3d_vedo.spikes_window.update_window_start_end)
-
+    spike_3d_to_2d_window_connection = spike_raster_plt_2d.window_scrolled.connect(spike_raster_plt_3d_vedo.spikes_window.update_window_start_end)
     # spike_3d_to_2d_window_connection = spike_raster_plt_2d.window_scrolled.connect(spike_raster_plt_3d.spikes_window.update_window_start_end)
-    # spike_raster_plt_3d.disable_render_window_controls()
-    # spike_raster_plt_3d.setWindowTitle('3D Raster with 2D Control Window')
-    # spike_raster_plt_3d.setWindowTitle('Main 3D (Vedo) Raster Window')
     
+    # Position the Windows As a Stack in the top-left corner:
     WidgetPositioningHelpers.move_widget_to_top_left_corner(spike_raster_plt_3d_vedo, debug_print=False)
     WidgetPositioningHelpers.align_3d_and_2d_windows(spike_raster_plt_3d_vedo, spike_raster_plt_2d) # Align the two windows
+    
+    # Update the scroll position programmatically with block_signals=False to ensure the 3D plot is synced:
     spike_raster_plt_2d.update_scroll_window_region(window_start_time, window_start_time+window_duration, block_signals=False)
     
     return spike_raster_plt_3d_vedo, spike_raster_plt_2d, spike_3d_to_2d_window_connection
-
-# Comes in with a spike_raster_plt_3d_vedo:
-
-# spike_raster_plt_3d_vedo.spikes_df
-
-# spike_raster_plt_3d_vedo.neuron_colors
-# spike_raster_plt_3d_vedo.neuron_sort_order
-
-
-
-
 
 
 
