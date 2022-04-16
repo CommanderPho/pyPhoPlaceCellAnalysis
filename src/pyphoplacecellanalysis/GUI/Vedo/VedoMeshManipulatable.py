a8c5f49ea25aa9414318a7e46a865903857eeb1a
Pho Hale <CommanderPho@users.noreply.github.com>
2022-04-16 04:26:20 -0400

    added vedo_create_if_needed function

--- src/pyphoplacecellanalysis/GUI/Vedo/VedoMeshManipulatable.py
+++ src/pyphoplacecellanalysis/GUI/Vedo/VedoMeshManipulatable.py
@@ -14,6 +14,35 @@ class VedoPlotterHelpers:
     """
     
     @classmethod
+    def vedo_create_if_needed(cls, plotter, item_key_name, render_item, defer_render=False):
+        """ Creates a new mesh if it exists in spike_raster_plt_3d_vedo
+        plotter: Spike3DRaster_Vedo the main 3d plotter.
+        item_key_name: str - like 'new_active_axes'
+        render_item: a valid vedo object that can be added to the plotter, like a Mesh or Text3D
+        defer_render: bool - whether to immediately render after removing an item or not.
+        
+        Requirements:
+            plotter must have the properties: 
+                   plotter.ui.plt
+                plotter.plots.meshes
+        Usage:
+            vedo_create_if_needed(spike_raster_plt_3d_vedo, 'active_window_only_axes', new_mesh)
+        
+        """
+        found_item = plotter.plots.meshes.get(item_key_name, None)
+        if found_item is None:
+            plotter.plots.meshes[item_key_name] = render_item
+            plotter.ui.plt += render_item
+            found_item = render_item # set the found_item to be returned to the new item
+            if not defer_render:
+                plotter.ui.plt.render() # render after adding the new item.
+    
+        # Either way, returns the found item after ensuring it has been added to the .plots.meshes dict with the correct key, and added to the plotter.
+        return found_item
+        
+        
+    
+    @classmethod
     def vedo_remove_if_exists(cls, plotter, item_key_name, defer_render=False):
         """ Removes a mesh if it exists in spike_raster_plt_3d_vedo
         plotter: Spike3DRaster_Vedo the main 3d plotter.
