650bcdb899e27f889bd8f78f32cd590b8cbb7e23
Pho Hale <CommanderPho@users.noreply.github.com>
2022-04-28 10:25:28 -0400

    placefield

---
+++ src/pyphoplacecellanalysis/Pho2D/PyQtPlots/TimeSynchronizedPlotters/TimeSynchronizedPlacefieldsPlotter.py
@@ -0,0 +1,198 @@
+import numpy as np
+import pandas as pd
+from qtpy import QtCore, QtWidgets
+
+from neuropy.analyses.time_dependent_placefields import PfND_TimeDependent
+
+import pyphoplacecellanalysis.External.pyqtgraph as pg
+# from pyphoplacecellanalysis.External.pyqtgraph.Qt import QtCore, QtGui
+from pyphocorehelpers.DataStructure.general_parameter_containers import VisualizationParameters
+from pyphocorehelpers.gui.PhoUIContainer import PhoUIContainer
+
+from pyphoplacecellanalysis.Pho2D.PyQtPlots.TimeSynchronizedPlotters.TimeSynchronizedPlotterBase import TimeSynchronizedPlotterBase
+from pyphoplacecellanalysis.Pho2D.PyQtPlots.plot_placefields import _pyqtplot_build_image_bounds_extent
+
+class TimeSynchronizedPlacefieldsPlotter(TimeSynchronizedPlotterBase):
+    """
+    
+    Usage:
+    
+        included_epochs = None
+        computation_config = active_session_computation_configs[0]
+        print('Recomputing active_epoch_placefields2D...', end=' ')
+        # PfND version:
+        t_list = []
+        ratemaps_list = []
+        active_time_dependent_placefields2D = PfND_TimeDependent(deepcopy(sess.spikes_df.copy()), deepcopy(sess.position), epochs=included_epochs,
+                                          speed_thresh=computation_config.speed_thresh, frate_thresh=computation_config.frate_thresh,
+                                          grid_bin=computation_config.grid_bin, smooth=computation_config.smooth)
+        curr_occupancy_plotter = TimeSynchronizedPlacefieldsPlotter(active_time_dependent_placefields2D)
+        curr_occupancy_plotter.show()
+
+    """
+    # Application/Window Configuration Options:
+    applicationName = 'TimeSynchronizedPlacefieldsPlotterApp'
+    windowName = 'TimeSynchronizedPlacefieldsPlotterWindow'
+    
+    enable_debug_print = False
+    
+    def __init__(self, active_time_dependent_placefields2D, drop_below_threshold: float=0.0000001, application_name=None, parent=None):
+        """_summary_
+        """
+        super().__init__(application_name=application_name, parent=parent) # Call the inherited classes __init__ method
+        
+        self.active_time_dependent_placefields = active_time_dependent_placefields2D
+        
+        self.setup()
+        self.params.drop_below_threshold = drop_below_threshold
+        
+        
+        self.buildUI()
+        # self.show()
+        
+    def setup(self):
+        # self.setup_spike_rendering_mixin() # NeuronIdentityAccessingMixin
+        self.app = pg.mkQApp(self.applicationName)
+        self.params = VisualizationParameters(self.applicationName)
+        ## Build the colormap to be used:
+        # self.params.cmap = pg.ColorMap(pos=np.linspace(0.0, 1.0, 6), color=colors)
+        self.params.cmap = pg.colormap.get('jet','matplotlib') # prepare a linear color map
+        self.params.image_margins = 2.0
+        
+        self.params.image_bounds_extent, self.params.x_range, self.params.y_range = _pyqtplot_build_image_bounds_extent(self.active_time_dependent_placefields.xbin, self.active_time_dependent_placefields.ybin, margin=self.params.image_margins, debug_print=self.enable_debug_print)
+        
+        self.params.nMapsToShow = self.active_time_dependent_placefields.ratemap.n_neurons
+        
+        
+    # def buildUI(self):
+    #     """ for QGridLayout
+    #         addWidget(widget, row, column, rowSpan, columnSpan, Qt.Alignment alignment = 0)
+    #     """
+    #     self.ui = PhoUIContainer()
+        
+    #     self.ui.layout = QtWidgets.QGridLayout()
+    #     self.ui.layout.setContentsMargins(0, 0, 0, 0)
+    #     self.ui.layout.setVerticalSpacing(0)
+    #     self.ui.layout.setHorizontalSpacing(0)
+    #     # self.setStyleSheet("background : #1B1B1B; color : #727272")
+    #     #### Build Graphics Objects #####
+    #     self._buildGraphics()        
+    #     self.setLayout(self.ui.layout)
+    #     self.resize(800,800)
+    #     self.setWindowTitle(self.windowName)
+        
+    def _buildGraphics(self):
+        
+        # root_render_widget
+        self.ui.root_graphics_layout_widget = pg.GraphicsLayoutWidget()
+        
+        
+        
+        curr_ratemap = self.active_time_dependent_placefields.ratemap
+        images = curr_ratemap.tuning_curves # (43, 63, 63)
+        occupancy = curr_ratemap.occupancy
+        
+        for i, curr_included_cell_ID in enumerate(self.active_time_dependent_placefields.ratemap.neuron_ids):            
+            cell_ID = curr_included_cell_ID
+            curr_cell_identifier_string = f'Cell[{cell_ID}]'
+            curr_plot_identifier_string = f'pyqtplot_plot_image_array.{curr_cell_identifier_string}'
+
+            image = np.squeeze(images[i,:,:])
+            # Pre-filter the data:
+            image = np.array(image.copy()) / np.nanmax(image) # note scaling by maximum here!
+            if self.params.drop_below_threshold is not None:
+                image[np.where(occupancy < self.params.drop_below_threshold)] = np.nan # null out the occupancy
+
+            # Build the image item:
+            img_item = pg.ImageItem(image=image, levels=(0,1))
+                
+            curr_plot = self.ui.root_graphics_layout_widget.addPlot(row=curr_row, col=curr_col, name=curr_plot_identifier_string, title=curr_cell_identifier_string)
+            curr_plot.addItem(img_item)  # add ImageItem to PlotItem
+            curr_plot.showAxes(True)
+   
+            # Update the image:
+            img_item.setImage(image, rect=self.params.image_bounds_extent)
+            img_item.setLookupTable(self.params.cmap.getLookupTable(nPts=256))
+
+            # curr_plot.setXRange(global_min_x-margin, global_max_x+margin)
+            # curr_plot.setYRange(global_min_y-margin, global_max_y+margin)
+            curr_plot.setXRange(*self.params.x_range)
+            curr_plot.setYRange(*self.params.y_range)
+
+            # Interactive Color Bar:
+            bar = pg.ColorBarItem(values= (0, 1), colorMap=self.params.cmap, width=5, interactive=False) # prepare interactive color bar
+            # Have ColorBarItem control colors of img and appear in 'plot':
+            bar.setImageItem(img_item, insert_in=curr_plot)
+
+            img_item_array.append(img_item)
+            plot_array.append(curr_plot)
+            other_components_array.append({'color_bar':bar})
+        
+
+        
+        
+        
+        # Build a single image view to display the image:
+        self.ui.imv = pg.ImageView()
+        self.ui.layout.addWidget(self.ui.imv, 0, 0) # add the GLViewWidget to the layout at 0, 0
+        # Set the color map:
+        self.ui.imv.setColorMap(self.params.cmap)
+    
+    
+    def update(self, t):
+        # Compute the updated placefields/occupancy for the time t:
+        with np.errstate(divide='ignore', invalid='ignore'):
+            self.active_time_dependent_placefields.update(t)
+        # # Update the plots:
+        # self._update_plots()
+
+    def _update_plots(self):
+        """
+        
+        """
+        if self.enable_debug_print:
+            print(f'TimeSynchronizedPlacefieldsPlotter._update_plots()')
+            
+        # Update the existing one:
+        
+        # Update the plots:
+        curr_t = self.active_time_dependent_placefields.last_t
+        curr_ratemap = self.active_time_dependent_placefields.ratemap
+        
+        # image = curr_ratemap.occupancy
+        # image = self.active_time_dependent_placefields.curr_normalized_occupancy
+        # image_title = 'curr_normalized_occupancy'
+        
+        image = self.active_time_dependent_placefields.curr_seconds_occupancy.copy()
+        image_title = 'curr_seconds_occupancy'
+        
+        if self.params.drop_below_threshold is not None:
+            # image[np.where(occupancy < self.params.drop_below_threshold)] = np.nan # null out the occupancy
+            image[np.where(image < self.params.drop_below_threshold)] = np.nan # null out the occupancy
+        
+        self.ui.imv.setImage(image, xvals=self.active_time_dependent_placefields.xbin)
+        self.setWindowTitle(f'{self.windowName} - {image_title} t = {curr_t}')
+    
+    
+    # @QtCore.Slot(float, float)
+    # def on_window_changed(self, start_t, end_t):
+    #     # called when the window is updated
+    #     if self.enable_debug_print:
+    #         print(f'TimeSynchronizedPlacefieldsPlotter.on_window_changed(start_t: {start_t}, end_t: {end_t})')
+    #     if self.enable_debug_print:
+    #         profiler = pg.debug.Profiler(disabled=True, delayed=True)
+            
+    #     self.update(end_t)
+    #     self._update_plots()
+        
+    #     if self.enable_debug_print:
+    #         profiler('Finished calling _update_plots()')
+            
+            
+# included_epochs = None
+# computation_config = active_session_computation_configs[0]
+# active_time_dependent_placefields2D = PfND_TimeDependent(deepcopy(sess.spikes_df.copy()), deepcopy(sess.position), epochs=included_epochs,
+#                                   speed_thresh=computation_config.speed_thresh, frate_thresh=computation_config.frate_thresh,
+#                                   grid_bin=computation_config.grid_bin, smooth=computation_config.smooth)
+# curr_occupancy_plotter = TimeSynchronizedPlacefieldsPlotter(active_time_dependent_placefields2D)
+# curr_occupancy_plotter.show()
