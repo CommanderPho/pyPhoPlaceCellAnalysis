cca5f4825a65b409873af33fd85cbe46f4015ad0
Pho Hale <CommanderPho@users.noreply.github.com>
2022-06-15 18:48:00 -0400

    factored out save/restore functionality into SaveRestoreStateParamHelpers

---
+++ src/pyphoplacecellanalysis/GUI/PyQtPlot/Params/SaveRestoreStateParamHelpers.py
@@ -0,0 +1,36 @@
+""" Simple helper functions that are used to add Save/Restore state buttons to a ParameterTree
+
+Known to be used by:
+	PhoPy3DPositionAnalysis2021\LibrariesExamples\PyQtPlot\PyQtGraph_PipelineFilterParameterTree_Testing.py
+
+"""
+
+def default_parameters_save_restore_state_button_children():
+    """ Builds the default save/restore state buttons for a parameter item """
+    return {
+        'name': 'Save/Restore functionality', 'type': 'group', 'children': [
+        {'name': 'Save State', 'type': 'action'},
+        {
+            'name': 'Restore State', 'type': 'action', 'children': [
+            {'name': 'Add missing items', 'type': 'bool', 'value': True},
+            {'name': 'Remove extra items', 'type': 'bool', 'value': True},
+        ]},
+    ]}
+
+
+def add_save_restore_btn_functionality(p):
+    """ Sets up the Save/Restore State Button Functionality """
+    # Save/Restore State Button Functionality:
+    def save():
+        global state
+        state = p.saveState()
+
+    def restore():
+        global state
+        add = p['Save/Restore functionality', 'Restore State', 'Add missing items']
+        rem = p['Save/Restore functionality', 'Restore State', 'Remove extra items']
+        p.restoreState(state, addChildren=add, removeChildren=rem)
+
+    # Looks like here it tries to find the child named 'Save/Restore functionality' > 'Save State' to bind the buttons
+    p.param('Save/Restore functionality', 'Save State').sigActivated.connect(save)
+    p.param('Save/Restore functionality', 'Restore State').sigActivated.connect(restore)
