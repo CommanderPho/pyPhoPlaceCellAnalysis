378ef33e7df915e341c3636d64710fa542a96f01
Pho Hale <CommanderPho@users.noreply.github.com>
2022-06-17 19:13:31 -0400

    added custom function setup_custom_neuron_identity_axis

--- src/pyphoplacecellanalysis/GUI/PyQtPlot/Widgets/Mixins/Render2DNeuronIdentityLinesMixin.py
+++ src/pyphoplacecellanalysis/GUI/PyQtPlot/Widgets/Mixins/Render2DNeuronIdentityLinesMixin.py
@@ -1,14 +1,42 @@
-
+import numpy as np
 
 
 class Render2DNeuronIdentityLinesMixin:
     """ renders the horizontal lines separating the neurons on the 2D raster plots 
     
     TODO: This is not really a mixin, need to figure out how I want these used.
+    
+    Usage:
+        from pyphoplacecellanalysis.GUI.PyQtPlot.Widgets.Mixins.Render2DNeuronIdentityLinesMixin import Render2DNeuronIdentityLinesMixin
+        
+        v_axis_item = Render2DNeuronIdentityLinesMixin.setup_custom_neuron_ticks(main_plot_widget, spike_raster_window.spike_raster_plt_2d.n_cells)
+        v_axis_item = Render2DNeuronIdentityLinesMixin.add_lines(main_plot_widget)
+        
 
     """
     @staticmethod
-    def add_lines(plot_widget):
+    def setup_custom_neuron_identity_axis(plot_widget, n_cells):
+        """ Completely sets up the custom 2D neuron identity axis vertical/y-axis) by adding one minor tick per neuron and displaying the horizontal grid. """
+        v_axis_item = Render2DNeuronIdentityLinesMixin._setup_custom_neuron_ticks(plot_widget, n_cells)
+        v_axis_item = Render2DNeuronIdentityLinesMixin._add_lines(plot_widget)
+        return v_axis_item
+    
+    
+    @staticmethod
+    def _setup_custom_neuron_ticks(plot_widget, n_cells):
+        """ Build custom ticks with one for each neuron """
+        neuron_id_ticks = [
+            [(float(i),f'{i}') for i in np.arange(0, 5, n_cells)], # [ (majorTickValue1, majorTickString1), (majorTickValue2, majorTickString2), ... ],
+            [(float(i),'') for i in np.arange(n_cells+1)], #[ (minorTickValue1, minorTickString1), (minorTickValue2, minorTickString2), ... ],
+        ]
+        v_axis_item = plot_widget.axes['left']['item'] # AxisItem 
+        v_axis_item.setTicks(neuron_id_ticks)
+        v_axis_item.setStyle(stopAxisAtTick=(True, True)) # (max, min)
+        return v_axis_item
+
+        
+    @staticmethod
+    def _add_lines(plot_widget):
         """ Sets up the y-axis ticks/grid to display one tick per unit (meaning one tick per cell) and label them every 5 ticks 
 
         Inputs:       
