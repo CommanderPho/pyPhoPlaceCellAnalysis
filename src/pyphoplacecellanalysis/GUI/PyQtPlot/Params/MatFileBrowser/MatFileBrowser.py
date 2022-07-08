3ff822af86c67c7a7f5324c6a0ef6919ac580cd9
Pho Hale <CommanderPho@users.noreply.github.com>
2022-07-07 20:36:04 -0400

    bump

--- src/pyphoplacecellanalysis/GUI/PyQtPlot/Params/MatFileBrowser/MatFileBrowser.py
+++ src/pyphoplacecellanalysis/GUI/PyQtPlot/Params/MatFileBrowser/MatFileBrowser.py
@@ -9,7 +9,8 @@ from pyphoplacecellanalysis.PhoPositionalData.load_exported import import_mat_fi
 
 
 import scipy.io # used for not HDF format files:
-import warnings
+from warnings import warn
+
 from pyphoplacecellanalysis.GUI.PyQtPlot.pyqtplot_DataTreeWidget import plot_dataTreeWidget # for GUI
 
 class MatFileBrowser(object):
@@ -31,7 +32,7 @@ class MatFileBrowser(object):
         return mat_files # 'RatS-Day5TwoNovel-2020-12-04_07-55-09'
     
     @classmethod
-    def whos_mat_file(cls, mat_file, debug_print=False):
+    def whos_mat_file(cls, mat_file, ignored_keys = ['#subsystem#', '#refs#'], debug_h5py_dump=False, debug_print=False):
         """ Prints the names of the variables within the .mat file without having to load them.
             Performs two methods: first HDF5-based for newer .mat files, and then falling back to scipy.io.whosmat based for older ones.
         
@@ -39,12 +40,14 @@ class MatFileBrowser(object):
             loaded_data: dict<Path, List<str>> - each path has a value of the found top-level variable names for that file
         """
         meta_info_dict = {}
-        ignored_keys = ['#subsystem#', '#refs#']
+        
         try:
             with h5py.File(mat_file,'r') as f:
                 found_data_dict = {a_key:meta_info_dict for a_key in list(f.keys()) if a_key not in ignored_keys} ## filter the ignored keys
                 # found_keys = [a_key for a_key in list(f.keys()) if a_key not in ignored_keys] ## filter the ignored keys
-                
+                if debug_h5py_dump:
+                    found_data_dict['__dump__'] = f
+                    
         except OSError as e:
             # Try whosmat:
             try:
@@ -53,7 +56,7 @@ class MatFileBrowser(object):
                 # found_keys = [a_key for a_key, a_data_size, a_data_type in found_temp_data_tuples if a_key not in ignored_keys] ## filter the ignored keys    
             except OSError as e:
                 # Actually failed
-                warning(f'{mat_file} - !!ERROR: {e}')
+                warn(f'{mat_file} - !!ERROR: {e}')
                 found_data_dict = None
                 
         if debug_print:
@@ -61,37 +64,18 @@ class MatFileBrowser(object):
         return found_data_dict
     
     @classmethod
-    def whos_mat_files(cls, found_files, debug_print=False):
+    def whos_mat_files(cls, found_files, ignored_keys = ['#subsystem#', '#refs#'], debug_print=False):
         """ Prints the names of the variables within the .mat files without having to load them.
             Performs two methods: first HDF5-based for newer .mat files, and then falling back to scipy.io.whosmat based for older ones.
         
         Returns:
             loaded_data: dict<Path, List<str>> - each path has a value of the found top-level variable names for that file
         """
-        loaded_data = {}
-        ignored_keys = ['#subsystem#', '#refs#']
-        for found_file in found_files:
-            try:
-                with h5py.File(found_file,'r') as f:
-                    found_keys = [a_key for a_key in list(f.keys()) if a_key not in ignored_keys] ## filter the ignored keys
-                    loaded_data[found_file] = found_keys
-                    if debug_print:
-                        print(f'{found_file}: {found_keys}')
-            except OSError as e:
-                # Try whosmat:
-                try:
-                    # [('behavioral_epochs', (3, 6), 'double'),
-                     # ('behavioral_periods', (668, 6), 'double')]
-                    found_data = scipy.io.whosmat(found_file)
-                    found_keys = [a_key for a_key, a_data_size, a_data_type in found_data if a_key not in ignored_keys] ## filter the ignored keys
-                    loaded_data[found_file] = found_keys
-                    if debug_print:
-                        print(f'{found_file}: {found_keys}')
-                except OSError as e:
-                    # Actually failed
-                    warning(f'{found_file} - !!ERROR: {e}')
-                    loaded_data[found_file] = None
-        return loaded_data
+        return {found_file:cls.whos_mat_file(found_file, ignored_keys=ignored_keys, debug_print=debug_print) for found_file in found_files}
+        # loaded_data = {}
+        # for found_file in found_files:
+        #     loaded_data[found_file] = cls.whos_mat_file(found_file, debug_print=debug_print)
+        # return loaded_data
     
     @classmethod
     def build_browsing_gui(cls, loaded_data_dict, debug_print=False):
@@ -115,7 +99,9 @@ class MatFileBrowser(object):
         #     'loaded_data_dict':loaded_data_dict
         # }
         # d = {str(a_file):file_keys for a_file, file_keys in loaded_data_dict.items()} # as simple array
-        d = {str(a_file):{a_file_key:'TODO' for a_file_key in file_keys} for a_file, file_keys in loaded_data_dict.items()} # as nested variable names
-        tree, app = plot_dataTreeWidget(data=d, title='PhoOutputDataTreeApp')
+        # d = {str(a_file):{a_file_key:'TODO' for a_file_key in file_keys} for a_file, file_keys in loaded_data_dict.items()} # as nested variable names
+        d = {str(a_file):file_info_dict for a_file, file_info_dict in loaded_data_dict.items()} # as nested variable names
+        # basedir
+        tree, app = plot_dataTreeWidget(data=d, title=f'MAT Browsing GUI: {len(loaded_data_dict)} .mat files')
         return tree, app
     
