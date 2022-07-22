e9c436badab958ce8ae314d25532875cf3fdf6bc
Pho Hale <CommanderPho@users.noreply.github.com>
2022-07-22 13:25:07 -0400

    cleanup documentation for potentially unfinished class

--- src/pyphoplacecellanalysis/Pho2D/PyQtPlots/plot_spikes_raster_2D.py
+++ src/pyphoplacecellanalysis/Pho2D/PyQtPlots/plot_spikes_raster_2D.py
@@ -2,15 +2,16 @@ import pyphoplacecellanalysis.External.pyqtgraph as pg
 from pyphoplacecellanalysis.External.pyqtgraph.Qt import QtCore, QtGui, QtWidgets
 
 
-## Create a subclass of GraphicsObject.
-## The only required methods are paint() and boundingRect() 
-## (see QGraphicsItem documentation)
-
-
-
-
-  
 class SpikesRasterItem(pg.GraphicsObject):
+    """ A GraphicsObject that renders a 2D Spikes raster
+    
+     Creates a subclass of GraphicsObject.
+        The only required methods are paint() and boundingRect() 
+        (see QGraphicsItem documentation)
+        
+        TODO: #unused?
+        
+    """
     def __init__(self, config, data = None):
         pg.GraphicsObject.__init__(self)
         self._config = config
@@ -60,50 +61,3 @@ class SpikesRasterItem(pg.GraphicsObject):
 
 
   
-
-# class SpikesRasterItem(pg.GraphicsObject):
-#     def __init__(self, data):
-#         pg.GraphicsObject.__init__(self)
-#         self._data = []
-#         self.setData(data)
-    
-#     def setData(self, data):
-# 		## data must have fields: time, open, close, min, max
-#         self._data = data
-#         self.generatePicture()
-        
-    
-#     def generatePicture(self):
-#         ## pre-computing a QPicture object allows paint() to run much more quickly, 
-#         ## rather than re-drawing the shapes every time.
-#         self.picture = QtGui.QPicture()
-#         p = QtGui.QPainter(self.picture)
-#         p.setPen(pg.mkPen('w'))
-        
-        
-        
-        
-#         for (t, v) in self._data:
-#             if v != 0:
-#                 p.drawLine(QtCore.QPointF(t, 0), QtCore.QPointF(t, v))
-                
-                
-        
-#         w = (self.data[1][0] - self.data[0][0]) / 3.
-#         for (t, open, close, min, max) in self.data:
-#             p.drawLine(QtCore.QPointF(t, min), QtCore.QPointF(t, max))
-#             if open > close:
-#                 p.setBrush(pg.mkBrush('r'))
-#             else:
-#                 p.setBrush(pg.mkBrush('g'))
-#             p.drawRect(QtCore.QRectF(t-w, open, w*2, close-open))
-#         p.end()
-    
-#     def paint(self, p, *args):
-#         p.drawPicture(0, 0, self.picture)
-    
-#     def boundingRect(self):
-#         ## boundingRect _must_ indicate the entire area that will be drawn on
-#         ## or else we will get artifacts and possibly crashing.
-#         ## (in this case, QPicture does all the work of computing the bouning rect for us)
-#         return QtCore.QRectF(self.picture.boundingRect())
