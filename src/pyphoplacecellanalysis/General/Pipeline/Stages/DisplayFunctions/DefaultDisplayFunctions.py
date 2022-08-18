08c486d358e79c67bf35cdce945d0caf26de11ef
Pho Hale <CommanderPho@users.noreply.github.com>
2022-08-18 16:20:40 -0400

    safely wrapped kwargs with overriding_dict_with

--- src/pyphoplacecellanalysis/General/Pipeline/Stages/DisplayFunctions/DefaultDisplayFunctions.py
+++ src/pyphoplacecellanalysis/General/Pipeline/Stages/DisplayFunctions/DefaultDisplayFunctions.py
@@ -1,3 +1,4 @@
+from neuropy.utils.dynamic_container import overriding_dict_with # required for _display_2d_placefield_result_plot_raw
 from pyphocorehelpers.mixins.member_enumerating import AllFunctionEnumeratingMixin
 from pyphoplacecellanalysis.General.Pipeline.Stages.DisplayFunctions.DisplayFunctionRegistryHolder import DisplayFunctionRegistryHolder
 
@@ -8,12 +9,12 @@ class DefaultDisplayFunctions(AllFunctionEnumeratingMixin, metaclass=DisplayFunc
     
     def _display_1d_placefield_validations(computation_result, active_config, **kwargs):
         """ Renders all of the flat 1D place cell validations with the yellow lines that trace across to their horizontally drawn placefield (rendered on the right of the plot) """
-        out_figures_list = plot_1d_placecell_validations(computation_result.computed_data['pf1D'], active_config.plotting_config, **({'modifier_string': 'lap_only', 'should_save': False} | kwargs))
+        out_figures_list = plot_1d_placecell_validations(computation_result.computed_data['pf1D'], active_config.plotting_config, **overriding_dict_with(lhs_dict={'modifier_string': 'lap_only', 'should_save': False}, **kwargs))
         return out_figures_list
 
     def _display_2d_placefield_result_plot_raw(computation_result, active_config, **kwargs):
         """ produces a stupid figure """
-        out_figures_list = computation_result.computed_data['pf2D'].plot_raw(**({'label_cells': True} | kwargs)); # Plots an overview of each cell all in one figure
+        out_figures_list = computation_result.computed_data['pf2D'].plot_raw(**overriding_dict_with(lhs_dict={'label_cells': True}, **kwargs)); # Plots an overview of each cell all in one figure
         return out_figures_list
 
 
