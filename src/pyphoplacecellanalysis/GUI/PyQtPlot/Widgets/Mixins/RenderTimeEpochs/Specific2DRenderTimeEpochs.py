a162d7fbd3aaf52ca770acacff0e7621ab8215ea
Pho Hale <CommanderPho@users.noreply.github.com>
2022-09-05 15:22:03 -0400

    class-based method

--- src/pyphoplacecellanalysis/GUI/PyQtPlot/Widgets/Mixins/RenderTimeEpochs/Specific2DRenderTimeEpochs.py
+++ src/pyphoplacecellanalysis/GUI/PyQtPlot/Widgets/Mixins/RenderTimeEpochs/Specific2DRenderTimeEpochs.py
@@ -21,14 +21,12 @@ class General2DRenderTimeEpochs(object):
         super(General2DRenderTimeEpochs, self).__init__()
     
     @classmethod
-    def build_epochs_dataframe_formatter(debug_print=False, **kwargs):
+    def build_epochs_dataframe_formatter(cls, **kwargs):
         def _add_interval_dataframe_visualization_columns_general_epoch(active_df):
             """ Adds the remaining _required_interval_visualization_columns specifically for PBEs
             """
             num_intervals = np.shape(active_df)[0]
-            if debug_print:
-                print(f'num_intervals: {num_intervals}') # num_intervals: 206
-
+            
             ## parameters:
             y_location = 0.0
             height = 1.0
@@ -65,24 +63,6 @@ class General2DRenderTimeEpochs(object):
         general_epochs_interval_datasource = IntervalsDatasource.init_from_epoch_object(active_epochs_obj, cls.build_epochs_dataframe_formatter(**kwargs), datasource_name='intervals_datasource_from_general_Epochs_obj')
         return general_epochs_interval_datasource
 
-    # @classmethod
-    # def build_render_time_epochs(cls, curr_sess, **kwargs):
-    #     """ builds the 
-        
-    #     """
-    #     if isinstance(curr_sess, DataSession):
-    #         active_Epochs = curr_sess.epochs # <Epoch> object
-    #     elif isinstance(curr_sess, Epoch):
-    #         active_Epochs = curr_sess  # <Epoch> object passed directly
-    #     else:
-    #         raise NotImplementedError
-        
-    #     interval_datasource = cls.build_render_time_epochs_datasource(active_epochs_obj=active_Epochs, **kwargs)
-    #     active_interval_rects_item = Render2DEventRectanglesHelper.build_IntervalRectsItem_from_interval_datasource(interval_datasource)
-    #     active_interval_rects_item.setToolTip('General')
-        
-    #     return active_interval_rects_item
-    
     @classmethod
     def add_render_time_epochs(cls, curr_sess, destination_plot, **kwargs):
         """ directly-called method 
@@ -95,12 +75,151 @@ class General2DRenderTimeEpochs(object):
         else:
             raise NotImplementedError
         interval_datasource = cls.build_render_time_epochs_datasource(active_epochs_obj=active_Epochs, **kwargs)
-        # active_interval_rects_item = cls.build_render_time_epochs(curr_sess=curr_sess)
-        # destination_plot.addPlot(active_interval_rects_item)
         out_rects = destination_plot.add_rendered_intervals(interval_datasource, name='GeneralEpochs', debug_print=True)
         # return out_rects
         
+class SessionEpochs2DRenderTimeEpochs(General2DRenderTimeEpochs):
+    """docstring for SessionEpochs2DRenderTimeEpochs."""
+    # def __init__(self, arg):
+    #     super(SessionEpochs2DRenderTimeEpochs, self).__init__()
         
+    @classmethod
+    def build_epochs_dataframe_formatter(cls, **kwargs):
+        def _add_interval_dataframe_visualization_columns_general_epoch(active_df):
+            """ Adds the remaining _required_interval_visualization_columns specifically for PBEs
+            """
+            num_intervals = np.shape(active_df)[0]
+            # if debug_print:
+            #     print(f'num_intervals: {num_intervals}') # num_intervals: 206
+
+            ## parameters:
+            y_location = 0.0
+            height = 1.0
+            # pen_color = pg.mkColor('red')
+            # brush_color = pg.mkColor('red')
+
+            ## parameters:
+            pen_color = [pg.mkColor('red'), pg.mkColor('cyan')]
+            brush_color = [pg.mkColor('red'), pg.mkColor('cyan')]
+            
+            ## Add the missing parameters to the dataframe:
+            active_df['series_vertical_offset'] = kwargs.setdefault('series_vertical_offset', y_location)
+            active_df['series_height'] = kwargs.setdefault('series_height', height)
+
+            if isinstance(pen_color, (list, tuple)):
+                active_df['pen'] = kwargs.setdefault('pen', [pg.mkPen(a_pen_color) for a_pen_color in pen_color])
+            else:
+                # Scalar value assignment:
+                active_df['pen'] = kwargs.setdefault('pen', pg.mkPen(pen_color)) 
+                
+            if isinstance(brush_color, (list, tuple)):
+                active_df['brush'] = kwargs.setdefault('brush', [pg.mkBrush(a_color) for a_color in brush_color])  
+            else:
+                # Scalar value assignment:
+                active_df['brush'] = kwargs.setdefault('brush', pg.mkBrush(brush_color))
+            
+            return active_df
+
+        return _add_interval_dataframe_visualization_columns_general_epoch
+    
+    
+    
+class Laps2DRenderTimeEpochs(General2DRenderTimeEpochs):
+    """docstring for Laps2DRenderTimeEpochs."""
+    # def __init__(self, arg):
+    #     super(SessionEpochs2DRenderTimeEpochs, self).__init__()
+        
+    @classmethod
+    def build_epochs_dataframe_formatter(cls, **kwargs):
+        def _add_interval_dataframe_visualization_columns_general_epoch(active_df):
+            """ Adds the remaining _required_interval_visualization_columns specifically for PBEs
+            """
+            num_intervals = np.shape(active_df)[0]
+            # if debug_print:
+            #     print(f'num_intervals: {num_intervals}') # num_intervals: 206
+
+            ## parameters:
+            y_location = 0.0
+            height = 1.0
+            pen_color = pg.mkColor('red')
+            brush_color = pg.mkColor('red')
+            
+            ## Add the missing parameters to the dataframe:
+            active_df['series_vertical_offset'] = kwargs.setdefault('series_vertical_offset', y_location)
+            active_df['series_height'] = kwargs.setdefault('series_height', height)
+            if isinstance(pen_color, (list, tuple)):
+                active_df['pen'] = kwargs.setdefault('pen', [pg.mkPen(a_pen_color) for a_pen_color in pen_color])
+            else:
+                # Scalar value assignment:
+                active_df['pen'] = kwargs.setdefault('pen', pg.mkPen(pen_color)) 
+                
+            if isinstance(brush_color, (list, tuple)):
+                active_df['brush'] = kwargs.setdefault('brush', [pg.mkBrush(a_color) for a_color in brush_color])  
+            else:
+                # Scalar value assignment:
+                active_df['brush'] = kwargs.setdefault('brush', pg.mkBrush(brush_color))
+            
+            return active_df
+
+        return _add_interval_dataframe_visualization_columns_general_epoch
+    
+    @classmethod
+    def add_render_time_epochs(cls, curr_sess, destination_plot, **kwargs):
+        """ directly-called method 
+        destination_plot should implement add_rendered_intervals
+        """
+        if isinstance(curr_sess, DataSession):
+            active_Epochs = curr_sess.laps.as_epoch_obj() # <Epoch> object
+        elif isinstance(curr_sess, Laps):
+            active_Epochs = curr_sess.as_epoch_obj()
+        elif isinstance(curr_sess, Epoch):
+            active_Epochs = curr_sess
+        else:
+            raise NotImplementedError
+        interval_datasource = cls.build_render_time_epochs_datasource(active_epochs_obj=active_Epochs, **kwargs)
+        out_rects = destination_plot.add_rendered_intervals(interval_datasource, name='Laps', debug_print=True)
+        
+        
+        
+class PBE_2DRenderTimeEpochs(General2DRenderTimeEpochs):
+    """docstring for PBE_2DRenderTimeEpochs."""
+    # def __init__(self, arg):
+    #     super(SessionEpochs2DRenderTimeEpochs, self).__init__()
+        
+    @classmethod
+    def build_epochs_dataframe_formatter(cls, **kwargs):
+        def _add_interval_dataframe_visualization_columns_general_epoch(active_df):
+            """ Adds the remaining _required_interval_visualization_columns specifically for PBEs
+            """
+            num_intervals = np.shape(active_df)[0]
+            # if debug_print:
+            #     print(f'num_intervals: {num_intervals}') # num_intervals: 206
+
+            ## parameters:
+            y_location = 0.0
+            height = 2.5
+            pen_color = pg.mkColor('w')
+            brush_color = pg.mkColor('grey')
+            
+            ## Add the missing parameters to the dataframe:
+            active_df['series_vertical_offset'] = kwargs.setdefault('series_vertical_offset', y_location)
+            active_df['series_height'] = kwargs.setdefault('series_height', height)
+            if isinstance(pen_color, (list, tuple)):
+                active_df['pen'] = kwargs.setdefault('pen', [pg.mkPen(a_pen_color) for a_pen_color in pen_color])
+            else:
+                # Scalar value assignment:
+                active_df['pen'] = kwargs.setdefault('pen', pg.mkPen(pen_color)) 
+                
+            if isinstance(brush_color, (list, tuple)):
+                active_df['brush'] = kwargs.setdefault('brush', [pg.mkBrush(a_color) for a_color in brush_color])  
+            else:
+                # Scalar value assignment:
+                active_df['brush'] = kwargs.setdefault('brush', pg.mkBrush(brush_color))
+            
+            return active_df
+
+        return _add_interval_dataframe_visualization_columns_general_epoch
+      
         
 
 class Specific2DRenderTimeEpochsHelper:
@@ -162,9 +281,9 @@ class Specific2DRenderTimeEpochsHelper:
             active_PBEs_df['pen'] = kwargs.setdefault('pen', pg.mkPen(pen_color)) 
             active_PBEs_df['brush'] = kwargs.setdefault('brush', pg.mkBrush(brush_color))  
             
-            # new bounds:
-            new_y_max = (y_location+height)
-            print(f'new_y_max: {new_y_max}')
+            # # new bounds:
+            # new_y_max = (y_location+height)
+            # print(f'new_y_max: {new_y_max}')
             
             return active_PBEs_df
 
