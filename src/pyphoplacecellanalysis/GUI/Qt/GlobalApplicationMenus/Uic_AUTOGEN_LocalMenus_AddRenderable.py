876e4c3dfcdbb72d659ba2f36065de5f94ca47f6
Pho Hale <CommanderPho@users.noreply.github.com>
2022-09-07 19:17:42 -0400

    Fixed imports of resource for default generated Uic_AUTOGEN_* files

--- src/pyphoplacecellanalysis/GUI/Qt/GlobalApplicationMenus/Uic_AUTOGEN_LocalMenus_AddRenderable.py
+++ src/pyphoplacecellanalysis/GUI/Qt/GlobalApplicationMenus/Uic_AUTOGEN_LocalMenus_AddRenderable.py
@@ -11,12 +11,16 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 
-
 class Ui_LocalMenus_AddRenderable(object):
     def setupUi(self, LocalMenus_AddRenderable):
         LocalMenus_AddRenderable.setObjectName("LocalMenus_AddRenderable")
         LocalMenus_AddRenderable.resize(703, 126)
         LocalMenus_AddRenderable.setWindowTitle("LocalMenus")
+        LocalMenus_AddRenderable.setStyleSheet("background-color: rgb(71, 58, 46);\n"
+"border-color: rgb(207, 207, 207);\n"
+"background-color: rgba(71, 65, 60, 180);\n"
+"color: rgb(244, 244, 244);\n"
+"border-color: rgb(0, 0, 0);")
         self.centralwidget = QtWidgets.QWidget(LocalMenus_AddRenderable)
         self.centralwidget.setObjectName("centralwidget")
         LocalMenus_AddRenderable.setCentralWidget(self.centralwidget)
@@ -43,9 +47,11 @@ class Ui_LocalMenus_AddRenderable(object):
         self.actionAddTimeCurves_Position.setText("Position")
         self.actionAddTimeCurves_Position.setObjectName("actionAddTimeCurves_Position")
         self.actionAddTimeCurves_Custom = QtWidgets.QAction(LocalMenus_AddRenderable)
+        self.actionAddTimeCurves_Custom.setEnabled(False)
         self.actionAddTimeCurves_Custom.setText("Custom...")
         self.actionAddTimeCurves_Custom.setObjectName("actionAddTimeCurves_Custom")
         self.actionAddCustomRenderable = QtWidgets.QAction(LocalMenus_AddRenderable)
+        self.actionAddCustomRenderable.setEnabled(False)
         self.actionAddCustomRenderable.setText("Add Custom...")
         self.actionAddCustomRenderable.setObjectName("actionAddCustomRenderable")
         self.actionAddTimeIntervals_PBEs = QtWidgets.QAction(LocalMenus_AddRenderable)
@@ -58,32 +64,44 @@ class Ui_LocalMenus_AddRenderable(object):
         self.actionAddTimeIntervals_Laps.setText("Laps")
         self.actionAddTimeIntervals_Laps.setObjectName("actionAddTimeIntervals_Laps")
         self.actionAddTimeIntervals_Custom = QtWidgets.QAction(LocalMenus_AddRenderable)
+        self.actionAddTimeIntervals_Custom.setEnabled(False)
         self.actionAddTimeIntervals_Custom.setText("Custom...")
         self.actionAddTimeIntervals_Custom.setObjectName("actionAddTimeIntervals_Custom")
         self.actionAddTimeCurves_Random = QtWidgets.QAction(LocalMenus_AddRenderable)
         self.actionAddTimeCurves_Random.setText("Random")
         self.actionAddTimeCurves_Random.setObjectName("actionAddTimeCurves_Random")
+        self.actionClear_all_Time_Curves = QtWidgets.QAction(LocalMenus_AddRenderable)
+        self.actionClear_all_Time_Curves.setObjectName("actionClear_all_Time_Curves")
+        self.actionClear_all_Time_Intervals = QtWidgets.QAction(LocalMenus_AddRenderable)
+        self.actionClear_all_Time_Intervals.setObjectName("actionClear_all_Time_Intervals")
+        self.actionClear_all_Renderables = QtWidgets.QAction(LocalMenus_AddRenderable)
+        self.actionClear_all_Renderables.setObjectName("actionClear_all_Renderables")
         self.menuAddRenderable_Time_Curves.addAction(self.actionAddTimeCurves_Position)
         self.menuAddRenderable_Time_Curves.addAction(self.actionAddTimeCurves_Random)
         self.menuAddRenderable_Time_Curves.addAction(self.actionAddTimeCurves_Custom)
+        self.menuAddRenderable_Time_Curves.addSeparator()
+        self.menuAddRenderable_Time_Curves.addAction(self.actionClear_all_Time_Curves)
         self.menuAddRenderable_Time_Intervals.addAction(self.actionAddTimeIntervals_PBEs)
         self.menuAddRenderable_Time_Intervals.addAction(self.actionAddTimeIntervals_Session_Epochs)
         self.menuAddRenderable_Time_Intervals.addAction(self.actionAddTimeIntervals_Laps)
         self.menuAddRenderable_Time_Intervals.addAction(self.actionAddTimeIntervals_Custom)
+        self.menuAddRenderable_Time_Intervals.addSeparator()
+        self.menuAddRenderable_Time_Intervals.addAction(self.actionClear_all_Time_Intervals)
         self.menuAdd_Renderable.addAction(self.menuAddRenderable_Time_Curves.menuAction())
         self.menuAdd_Renderable.addAction(self.menuAddRenderable_Time_Intervals.menuAction())
         self.menuAdd_Renderable.addAction(self.actionAddCustomRenderable)
+        self.menuAdd_Renderable.addSeparator()
+        self.menuAdd_Renderable.addAction(self.actionClear_all_Renderables)
         self.menubar.addAction(self.menuAdd_Renderable.menuAction())
 
         self.retranslateUi(LocalMenus_AddRenderable)
         QtCore.QMetaObject.connectSlotsByName(LocalMenus_AddRenderable)
 
     def retranslateUi(self, LocalMenus_AddRenderable):
-        pass
-# import ActionIcons_rc
-# import GuiResources_rc
-# import breeze_rc
-
-## CUSTOM: DO NOT OVERWRITE:
-import pyphoplacecellanalysis.Resources.ActionIcons
-import pyphoplacecellanalysis.Resources.GuiResources
+        _translate = QtCore.QCoreApplication.translate
+        self.actionClear_all_Time_Curves.setText(_translate("LocalMenus_AddRenderable", "Clear all Time Curves"))
+        self.actionClear_all_Time_Intervals.setText(_translate("LocalMenus_AddRenderable", "Clear all Time Intervals"))
+        self.actionClear_all_Renderables.setText(_translate("LocalMenus_AddRenderable", "Clear all Renderables"))
+from pyphoplacecellanalysis.Resources import ActionIcons
+from pyphoplacecellanalysis.Resources import GuiResources
+from pyphoplacecellanalysis.Resources import breeze
---
+++ src/pyphoplacecellanalysis/Resources/__init__.py
@@ -0,0 +1,8 @@
+# C:\Users\pho\repos\pyPhoPlaceCellAnalysis\src\pyphoplacecellanalysis\External\breeze_style_sheets\breeze_resources.py
+
+## This imports the compiled `breeze_resources` from the breeze_stylesheets so that pyuic5 can compile correct imports of resources when the `--import-from=pyphoplacecellanalysis.Resources` argument is provided.
+## The complete correct pyuic5 call is: `"C:/Users/pho/miniconda3/envs/phoviz_ultimate/Scripts/pyuic5.exe" C:\Users\pho\repos\pyPhoPlaceCellAnalysis\src\pyphoplacecellanalysis\GUI\Qt\GlobalApplicationMenus\LocalMenus_AddRenderable.ui -o C:\Users\pho\repos\pyPhoPlaceCellAnalysis\src\pyphoplacecellanalysis\GUI\Qt\GlobalApplicationMenus\Uic_AUTOGEN_LocalMenus_AddRenderable.py --resource-suffix="" --execute --import-from=pyphoplacecellanalysis.Resources`
+
+from pyphoplacecellanalysis.External.breeze_style_sheets import breeze_resources as breeze
+
+
