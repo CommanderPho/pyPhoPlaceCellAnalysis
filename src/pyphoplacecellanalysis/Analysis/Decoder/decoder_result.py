c3ca75becf3b4a2a633ecfb8981c63ded1b9df84
Pho Hale <CommanderPho@users.noreply.github.com>
2022-09-15 17:31:28 -0400

    Moved most decoding-related helpers into Analysis/Decoder

--- src/pyphoplacecellanalysis/Analysis/Decoder/decoder_result.py
+++ src/pyphoplacecellanalysis/Analysis/Decoder/decoder_result.py
@@ -190,11 +190,9 @@ class DecoderResultDisplayingPlot2D(DecoderResultDisplayingBaseClass):
         if drop_below_threshold is not None:
             curr_p_x_given_n[np.where(curr_p_x_given_n < drop_below_threshold)] = np.nan # null out the p_x_given_n below certain values
             
-            
         ## 2022-09-15 - Testing
         curr_p_x_given_n = np.nan_to_num(curr_p_x_given_n, nan=0.0, posinf=1.0, neginf=0.0) # IDK if this is smart or not
-            
-            
+    
         ## Seems to work:
         curr_p_x_given_n = np.rot90(curr_p_x_given_n, k=-1)
         curr_p_x_given_n = np.fliplr(curr_p_x_given_n)
