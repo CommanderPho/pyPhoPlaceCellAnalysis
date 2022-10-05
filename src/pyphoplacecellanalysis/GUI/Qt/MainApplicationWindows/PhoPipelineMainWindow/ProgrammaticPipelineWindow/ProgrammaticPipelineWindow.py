bf7837830c36adea281f53c50f80d82288a8a273
Pho Hale <CommanderPho@users.noreply.github.com>
2022-10-05 18:05:33 -0400

    original

---
+++ src/pyphoplacecellanalysis/GUI/Qt/MainApplicationWindows/PhoPipelineMainWindow/ProgrammaticPipelineWindow/ProgrammaticPipelineWindow.py
@@ -0,0 +1,36 @@
+# ProgrammaticPipelineWindow.py
+# Generated from c:\Users\pho\repos\pyPhoPlaceCellAnalysis\src\pyphoplacecellanalysis\GUI\Qt\MainApplicationWindows\PhoPipelineMainWindow\ProgrammaticPipelineWindow\ProgrammaticPipelineWindow.ui automatically by PhoPyQtClassGenerator VSCode Extension
+import sys
+import os
+
+from PyQt5 import QtGui, QtWidgets, uic
+from PyQt5.QtWidgets import QMessageBox, QToolTip, QStackedWidget, QHBoxLayout, QVBoxLayout, QSplitter, QFormLayout, QLabel, QFrame, QPushButton, QTableWidget, QTableWidgetItem
+from PyQt5.QtWidgets import QApplication, QFileSystemModel, QTreeView, QWidget, QHeaderView
+from PyQt5.QtGui import QPainter, QBrush, QPen, QColor, QFont, QIcon
+from PyQt5.QtCore import Qt, QPoint, QRect, QObject, QEvent, pyqtSignal, pyqtSlot, QSize, QDir
+
+## IMPORTS:
+# 
+
+## Define the .ui file path
+path = os.path.dirname(os.path.abspath(__file__))
+uiFile = os.path.join(path, 'ProgrammaticPipelineWindow.ui')
+
+class ProgrammaticPipelineWindow(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent=parent) # Call the inherited classes __init__ method
+        self.ui = uic.loadUi(uiFile, self) # Load the .ui file
+
+        self.initUI()
+        self.show() # Show the GUI
+
+    def initUI(self):
+        pass
+
+
+## Start Qt event loop
+if __name__ == '__main__':
+    app = QApplication([])
+    widget = ProgrammaticPipelineWindow()
+    widget.show()
+    sys.exit(app.exec_())
