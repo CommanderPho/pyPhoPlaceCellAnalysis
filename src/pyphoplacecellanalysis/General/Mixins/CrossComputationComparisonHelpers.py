219a6b45629f2814a91c648dccfcfbf3a3e546c2
Pho Hale <CommanderPho@users.noreply.github.com>
2022-11-28 13:06:00 -0500

    added `SetPartition` and `SplitPartitionMembership` classes to standardized output results from `_compare_computation_results(...)`

--- src/pyphoplacecellanalysis/General/Mixins/CrossComputationComparisonHelpers.py
+++ src/pyphoplacecellanalysis/General/Mixins/CrossComputationComparisonHelpers.py
@@ -11,7 +11,46 @@ from pyphoplacecellanalysis.General.Model.ComputationResults import ComputationR
 from neuropy.utils.colors_util import get_neuron_colors # required for build_neurons_color_map 
 
 
-def _compare_computation_results(lhs_computation_results, rhs_computation_results):
+from enum import Enum
+
+
+class SplitPartitionMembership(Enum):
+	"""Docstring for SplitPartitionMembership."""
+	LEFT_ONLY = 0
+	SHARED = 1
+	RIGHT_ONLY = 2
+	
+
+class SetPartition(object):
+	""" Converted from a one-off structure produced by `_compare_computation_results` as illustrated below:
+		pf_neurons_diff = DynamicContainer(lhs_only=pf_neurons_lhs_unique, rhs_only=pf_neurons_rhs_unique, intersection=pf_neurons_both, either=pf_neurons_either,
+			 shared=DynamicContainer(n_neurons=n_neurons, shared_fragile_neuron_IDXs=shared_fragile_neuron_IDXs, pairs=shared_fragile_neuron_IDXs_to_pairs, missing_neuron_IDXs=all_missing_IDXs, missing_neuron_ids=pf_neurons_missing_from_any, aclu_to_shared_fragile_IDX_map=aclu_to_shared_fragile_IDX_map)) 
+
+		TODO: perhaps bring computations in here?
+	"""
+	def __init__(self, lhs_only, rhs_only, either, intersection, shared_structure):
+		super(SetPartition, self).__init__()
+		self.lhs_only = lhs_only
+		self.rhs_only = rhs_only
+		self.intersection = intersection
+		self.either = either
+		self._shared = shared_structure
+
+	@property
+	def n_neurons(self):
+		"""The n_neurons property."""
+		return len(self.either)
+		
+	@property
+	def shared(self):
+		""" DynamicContainer(n_neurons=self.n_neurons, shared_fragile_neuron_IDXs=shared_fragile_neuron_IDXs, pairs=shared_fragile_neuron_IDXs_to_pairs, missing_neuron_IDXs=all_missing_IDXs, missing_neuron_ids=pf_neurons_missing_from_any, aclu_to_shared_fragile_IDX_map=aclu_to_shared_fragile_IDX_map) """
+		return self._shared
+	@shared.setter
+	def shared(self, value):
+		self._shared = value
+
+
+def _compare_computation_results(lhs_computation_results, rhs_computation_results) -> SetPartition:
 	"""Computes the differences between two separate computation results, such as those computed for different epochs
 
 	Args:
@@ -67,7 +106,11 @@ def _compare_computation_results(lhs_computation_results, rhs_computation_result
 	shared_fragile_neuron_IDXs_to_pairs = list(zip(lhs_shared_IDXs_map, rhs_shared_IDXs_map)) # [(0, None), (1, 0), (None, 1), (2, 2), (3, None), (4, 3), (5, 4), (None, 5), (6, 6), (7, 7), (8, 8), (None, 9), (9, 10), (10, 11), (11, 12), (12, 13), (13, 14), (14, 15), (15, 16), (16, 17), (17, 18), (18, 19), (19, 20), (20, 21), (21, 22), (22, 23), (23, 24), (24, 25), (25, 26), (26, 27), (27, 28), (28, 29), (29, 30), (30, 31), (31, 32), (32, 33), (33, 34), (34, 35), (35, 36), (36, 37), (37, 38), (38, 39), (39, 40), (40, 41), (41, 42), (42, 43), (43, 44), (44, 45), (45, 46), (46, 47), (47, 48), (48, 49), (49, 50), (50, 51), (51, 52), (52, 53), (53, 54), (54, 55), (55, 56), (56, 57), (57, 58), (58, 59), (59, 60), (60, 61), (61, 62), (62, 63), (63, 64), (64, None), (65, 65), (66, None)]
 	# shared_fragile_neuron_IDXs_to_pairs 
 
-	pf_neurons_diff = DynamicContainer(lhs_only=pf_neurons_lhs_unique, rhs_only=pf_neurons_rhs_unique, intersection=pf_neurons_both, either=pf_neurons_either, shared=DynamicContainer(n_neurons=n_neurons, shared_fragile_neuron_IDXs=shared_fragile_neuron_IDXs, pairs=shared_fragile_neuron_IDXs_to_pairs, missing_neuron_IDXs=all_missing_IDXs, missing_neuron_ids=pf_neurons_missing_from_any, aclu_to_shared_fragile_IDX_map=aclu_to_shared_fragile_IDX_map)) 
+	# pf_neurons_diff = DynamicContainer(lhs_only=pf_neurons_lhs_unique, rhs_only=pf_neurons_rhs_unique, intersection=pf_neurons_both, either=pf_neurons_either,
+	#  	shared=DynamicContainer(n_neurons=n_neurons, shared_fragile_neuron_IDXs=shared_fragile_neuron_IDXs, pairs=shared_fragile_neuron_IDXs_to_pairs, missing_neuron_IDXs=all_missing_IDXs, missing_neuron_ids=pf_neurons_missing_from_any, aclu_to_shared_fragile_IDX_map=aclu_to_shared_fragile_IDX_map))
+	pf_neurons_diff = SetPartition(lhs_only=pf_neurons_lhs_unique, rhs_only=pf_neurons_rhs_unique, intersection=pf_neurons_both, either=pf_neurons_either,
+		shared_structure=DynamicContainer(n_neurons=n_neurons, shared_fragile_neuron_IDXs=shared_fragile_neuron_IDXs, pairs=shared_fragile_neuron_IDXs_to_pairs, missing_neuron_IDXs=all_missing_IDXs, missing_neuron_ids=pf_neurons_missing_from_any, aclu_to_shared_fragile_IDX_map=aclu_to_shared_fragile_IDX_map)
+		)
 	return pf_neurons_diff
 
 
