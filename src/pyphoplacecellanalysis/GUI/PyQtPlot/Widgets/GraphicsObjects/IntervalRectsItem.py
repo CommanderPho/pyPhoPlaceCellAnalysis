00b5fd9f21466e2498abd3567642e68fccc632df
Pho Hale <CommanderPho@users.noreply.github.com>
2022-12-05 21:50:47 -0500

    factored `RectangleRenderTupleHelpers` with helpful QPen and QBrush serialization/deserialization methods out into `pyphoplacecellanalysis.GUI.PyQtPlot.Widgets.helpers.RectangleRenderTupleHelpers`

--- src/pyphoplacecellanalysis/GUI/PyQtPlot/Widgets/GraphicsObjects/IntervalRectsItem.py
+++ src/pyphoplacecellanalysis/GUI/PyQtPlot/Widgets/GraphicsObjects/IntervalRectsItem.py
@@ -6,6 +6,7 @@ import copy
 import numpy as np
 import pyphoplacecellanalysis.External.pyqtgraph as pg
 from pyphoplacecellanalysis.External.pyqtgraph import QtCore, QtGui, QtWidgets
+from pyphoplacecellanalysis.GUI.PyQtPlot.Widgets.helpers import RectangleRenderTupleHelpers
 
 
 ## Create a subclass of GraphicsObject.
@@ -242,44 +243,6 @@ class IntervalRectsItem(pg.GraphicsObject):
         self.setOpacity(a/255.)
         
         
-
-
-
-class RectangleRenderTupleHelpers:
-    """ class for use in copying, serializing, etc the list of tuples used by IntervalRectsItem """
-    @staticmethod
-    def QPen_to_dict(a_pen):
-        return {'color': pg.colorStr(a_pen.color()),'width':a_pen.widthF()}
-
-    @staticmethod
-    def QBrush_to_dict(a_brush):
-        return {'color': pg.colorStr(a_brush.color())} # ,'gradient':a_brush.gradient()
-
-    
-    @classmethod
-    def get_serialized_data(cls, tuples_data):
-        """ converts the list of (float, float, float, float, QPen, QBrush) tuples into a list of (float, float, float, float, pen_color_hex:str, brush_color_hex:str) for serialization. """            
-        return [(start_t, series_vertical_offset, duration_t, series_height, cls.QPen_to_dict(pen), cls.QBrush_to_dict(brush)) for (start_t, series_vertical_offset, duration_t, series_height, pen, brush) in tuples_data]
-
-    
-    @staticmethod
-    def get_deserialized_data(seralized_tuples_data):
-        """ converts the list of (float, float, float, float, pen_color_hex:str, brush_color_hex:str) tuples back to the original (float, float, float, float, QPen, QBrush) list
-        
-        Inverse operation of .get_serialized_data(...).
-        
-        Usage:
-            seralized_tuples_data = RectangleRenderTupleHelpers.get_serialized_data(tuples_data)
-            tuples_data = RectangleRenderTupleHelpers.get_deserialized_data(seralized_tuples_data)
-        """ 
-        return [(start_t, series_vertical_offset, duration_t, series_height, pg.mkPen(pen_color_hex), pg.mkBrush(**brush_color_hex)) for (start_t, series_vertical_offset, duration_t, series_height, pen_color_hex, brush_color_hex) in seralized_tuples_data]
-
-    @classmethod
-    def copy_data(cls, tuples_data):
-        seralized_tuples_data = cls.get_serialized_data(tuples_data).copy()
-        return cls.get_deserialized_data(seralized_tuples_data)
-            
-
 
 # ==================================================================================================================== #
 # MAIN TESTING                                                                                                         #
--- src/pyphoplacecellanalysis/GUI/PyQtPlot/Widgets/helpers.py
+++ src/pyphoplacecellanalysis/GUI/PyQtPlot/Widgets/helpers.py
@@ -1,5 +1,7 @@
 ### Graphics Items Children Discovery - Programmatically getting rows/columns of GraphicsLayoutWidget
+from collections import namedtuple
 import numpy as np
+import pyphoplacecellanalysis.External.pyqtgraph as pg
 import pyphoplacecellanalysis.External.pyqtgraph.graphicsItems as graphicsItems
 from pyphoplacecellanalysis.External.pyqtgraph.graphicsItems.PlotItem import PlotItem #, PlotCurveItem
 from pyphoplacecellanalysis.External.pyqtgraph.graphicsItems.ScatterPlotItem import ScatterPlotItem #, PlotCurveItem
@@ -68,3 +70,82 @@ def recover_graphics_layout_widget_item_indicies(graphics_layout_widget, debug_p
         print(f'Found Items:\n\trow_indicies: {plot_items_row_indicies}\n\tindicies: {plot_items_col_indicies}\n\titems: {found_items_list}\n(found_unique_rows: {found_item_rows}, found_unique_cols: {found_item_cols})\n(found_max_row: {found_max_row}, found_max_col: {found_max_col})')
         # print(f'plot_items_row_indicies: {plot_items_row_indicies}\nplot_items_col_indicies: {plot_items_col_indicies}\nfound_items_list: {found_items_list}\n(found_unique_rows: {found_item_rows}\nfound_unique_cols: {found_item_cols})\n(found_max_row: {found_max_row}, found_max_col: {found_max_col})')
     return found_item_rows, found_item_cols, found_items_list, (found_max_row, found_max_col)
+
+
+
+
+# ==================================================================================================================== #
+# RectangleRenderTupleHelpers                                                                                          #
+# ==================================================================================================================== #
+QPenTuple = namedtuple('QPenTuple', ['color', 'width'])
+QBrushTuple = namedtuple('QBrushTuple', ['color'])
+
+class RectangleRenderTupleHelpers:
+    """ class for use in copying, serializing, etc the list of tuples used by IntervalRectsItem
+
+    Refactored out of `pyphoplacecellanalysis.GUI.PyQtPlot.Widgets.GraphicsObjects.IntervalRectsItem.IntervalRectsItem` on 2022-12-05 
+
+    Usage:
+
+        from pyphoplacecellanalysis.GUI.PyQtPlot.Widgets.helpers import RectangleRenderTupleHelpers
+
+    Known Usages:
+        Used in `IntervalRectsItem` and `CustomIntervalRectsItem` to copy themselves
+
+        # Copy Constructors: _________________________________________________________________________________________________ #
+        def __copy__(self):
+            independent_data_copy = RectangleRenderTupleHelpers.copy_data(self.data)
+            return CustomIntervalRectsItem(independent_data_copy)
+        
+        def __deepcopy__(self, memo):
+            independent_data_copy = RectangleRenderTupleHelpers.copy_data(self.data)
+            return CustomIntervalRectsItem(independent_data_copy)
+            # return CustomIntervalRectsItem(copy.deepcopy(self.data, memo))
+
+
+    """
+    @staticmethod
+    def QPen_to_dict(a_pen):
+        return {'color': pg.colorStr(a_pen.color()),'width':a_pen.widthF()}
+
+    @staticmethod
+    def QBrush_to_dict(a_brush):
+        return {'color': pg.colorStr(a_brush.color())} # ,'gradient':a_brush.gradient()
+
+
+    @staticmethod
+    def QPen_to_tuple(a_pen):
+        return QPenTuple(color=pg.colorStr(a_pen.color()), width=a_pen.widthF())
+
+    @staticmethod
+    def QBrush_to_tuple(a_brush):
+        return QBrushTuple(color=pg.colorStr(a_brush.color()))
+
+
+    
+    @classmethod
+    def get_serialized_data(cls, tuples_data):
+        """ converts the list of (float, float, float, float, QPen, QBrush) tuples into a list of (float, float, float, float, pen_color_hex:str, brush_color_hex:str) for serialization. """            
+        return [(start_t, series_vertical_offset, duration_t, series_height, cls.QPen_to_dict(pen), cls.QBrush_to_dict(brush)) for (start_t, series_vertical_offset, duration_t, series_height, pen, brush) in tuples_data]
+
+    
+    @staticmethod
+    def get_deserialized_data(seralized_tuples_data):
+        """ converts the list of (float, float, float, float, pen_color_hex:str, brush_color_hex:str) tuples back to the original (float, float, float, float, QPen, QBrush) list
+        
+        Inverse operation of .get_serialized_data(...).
+        
+        Usage:
+            seralized_tuples_data = RectangleRenderTupleHelpers.get_serialized_data(tuples_data)
+            tuples_data = RectangleRenderTupleHelpers.get_deserialized_data(seralized_tuples_data)
+        """ 
+        return [(start_t, series_vertical_offset, duration_t, series_height, pg.mkPen(pen_color_hex), pg.mkBrush(**brush_color_hex)) for (start_t, series_vertical_offset, duration_t, series_height, pen_color_hex, brush_color_hex) in seralized_tuples_data]
+
+    @classmethod
+    def copy_data(cls, tuples_data):
+        seralized_tuples_data = cls.get_serialized_data(tuples_data).copy()
+        return cls.get_deserialized_data(seralized_tuples_data)
+
+
+
+
