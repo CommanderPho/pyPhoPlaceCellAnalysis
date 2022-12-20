fc9e6cc7634c86d02cc544940c02f1fdc84ef8ce
Pho Hale <CommanderPho@users.noreply.github.com>
2022-12-20 11:48:24 -0500

    minor clean

--- src/pyphoplacecellanalysis/Pho2D/PyQtPlots/TimeSynchronizedPlotters/TimeSynchronizedPlotterBase.py
+++ src/pyphoplacecellanalysis/Pho2D/PyQtPlots/TimeSynchronizedPlotters/TimeSynchronizedPlotterBase.py
@@ -16,7 +16,6 @@ class TimeSynchronizedPlotterBase(QtWidgets.QWidget):
     
         included_epochs = None
         computation_config = active_session_computation_configs[0]
-        print('Recomputing active_epoch_placefields2D...', end=' ')
         # PfND version:
         t_list = []
         ratemaps_list = []
