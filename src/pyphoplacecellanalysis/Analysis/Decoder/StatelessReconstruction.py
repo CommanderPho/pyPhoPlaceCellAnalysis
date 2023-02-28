ef3ad559316b8c2b416cf6c299ce5f861eacc1a1
Pho Hale <PhoHale@gmail.com>
2023-02-28 08:26:07 -0500

    cleanup

--- src/pyphoplacecellanalysis/Analysis/Decoder/StatelessReconstruction.py
+++ src/pyphoplacecellanalysis/Analysis/Decoder/StatelessReconstruction.py
@@ -3,6 +3,9 @@ from dataclasses import dataclass
 
 @dataclass
 class PositionDecoder(object):
-	"""Docstring for ClassName."""
+	"""Basically just the result from a specific decoding. Meant to be a cleaner way  for PositionDecoder.
+
+	See also: DecoderResultDisplayingBaseClass
+	"""
 	property: type
 	
