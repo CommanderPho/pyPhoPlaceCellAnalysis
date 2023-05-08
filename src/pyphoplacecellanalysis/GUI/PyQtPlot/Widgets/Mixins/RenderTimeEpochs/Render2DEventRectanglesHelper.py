b43e102ae80e1571f3978f8f5c8c2cd90c4f0a80
Pho Hale <PhoHale@gmail.com>
2023-05-08 15:27:47 -0400

    cleanupo

--- src/pyphoplacecellanalysis/GUI/PyQtPlot/Widgets/Mixins/RenderTimeEpochs/Render2DEventRectanglesHelper.py
+++ src/pyphoplacecellanalysis/GUI/PyQtPlot/Widgets/Mixins/RenderTimeEpochs/Render2DEventRectanglesHelper.py
@@ -4,13 +4,8 @@
 from copy import deepcopy
 import numpy as np
 import pandas as pd
-# import matplotlib.pyplot as plt
-# from indexed import IndexedOrderedDict
-# from lazy_property import LazyProperty, LazyWritableProperty
 
 from neuropy.core import Epoch
-
-# from pyphocorehelpers.print_helpers import print_dataframe_memory_usage
 
 import pyphoplacecellanalysis.External.pyqtgraph as pg
 # from pyphoplacecellanalysis.External.pyqtgraph.Qt import QtCore, QtGui, QtWidgets
