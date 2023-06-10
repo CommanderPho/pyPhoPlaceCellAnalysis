80d662efd1799fc8c65298a6dc9ee3b920244265
Pho Hale <PhoHale@gmail.com>
2023-06-09 20:04:17 -0400

    Neptuner improvements. Added `upload_figures` function

--- src/pyphoplacecellanalysis/General/Batch/NeptuneAiHelpers.py
+++ src/pyphoplacecellanalysis/General/Batch/NeptuneAiHelpers.py
@@ -6,6 +6,8 @@ from neptune.types import File
 import pathlib
 from pathlib import Path
 
+from neuropy.utils.result_context import IdentifyingContext
+
 from pyphocorehelpers.function_helpers import function_attributes
 
 """
@@ -62,6 +64,9 @@ class Neptuner(object):
     project: neptune.Project = field(init=False)
     run: neptune.Run = field(init=False)
 
+    outputs = field(init=False)
+    figures = field(init=False)
+
     def __attrs_post_init__(self):
         self.project = neptune.init_project(project=self.project_name, api_token=self.api_token)
         self.run = None
@@ -72,26 +77,31 @@ class Neptuner(object):
             self.run.stop()
         self.project.stop()
         print(f'Neptuner stoppped.')
-                
-    def __del__(self):
-        # body of destructor
-        self.stop()
-        self.run = None
-        self.project = None
+
+    ## The destructor apparently isn't called reliably and sometimes it is called even when the object still exists?                
+    # def __del__(self):
+    #     # body of destructor
+    #     self.stop()
+    #     self.run = None
+    #     self.project = None
 
 
     def run_with_pipeline(self, curr_active_pipeline):
         """ starts a new run with the provided pipeline. """
         assert self.run is None, f"run_new_pipeline(...) called while the Neptuner object already has an active self.run object!"
         if self.run is None:
-            self.run = neptune.init_run(project=self.project_name, api_token=self.api_token, source_files=[])
+            self.run = neptune.init_run(project=self.project_name, api_token=self.api_token, source_files=[]) # see git_ref=GitRef(repository_path="/path/to/repo")
             # Add the session_context properties to the run: {'format_name': 'kdiba', 'animal': 'vvp01', 'exper_name': 'two', 'session_name': '2006-4-09_16-40-54'}
             for k, v in curr_active_pipeline.get_session_context().to_dict().items():
                 self.run[k] = v # add the properties to the run
 
+            self.run["sys/tags"].add(list(curr_active_pipeline.get_session_context().as_tuple())) # adds the session tags ('kdiba', 'gor01', 'one', '2006-6-09_1-22-43')
+            
+
             # session_descriptor_string: a string describing the context of the session like 'sess_kdiba_2006-6-07_11-26-53'
             self.run['session_descriptor_string'] = curr_active_pipeline.sess.get_description() # 'kdiba_vvp01_two_2006-4-09_16-40-54_sess'
-
+            self.outputs = self.run['outputs']
+            self.figures = self.outputs['figures']
 
     @classmethod
     def init_with_pipeline(cls, curr_active_pipeline):
@@ -100,6 +110,75 @@ class Neptuner(object):
         new.run_with_pipeline(curr_active_pipeline=curr_active_pipeline)
         return new
 
+
+    def upload_figures(self, curr_active_pipeline, should_use_fig_objects=False, debug_print=False):
+        """ 
+            should_use_fig_objects: bool (False):  if True, the raw matplotlib.Figure objects are uploaded. Otherwise, the previously saved files are uploaded.
+        """
+        assert not should_use_fig_objects, f"should_use_fig_objects = True is not currently working. Save the figures to file first and then use should_use_fig_objects = False"
+        sess_context = curr_active_pipeline.get_session_context()
+        succeeded_fig_paths = []
+        failed_fig_paths = []
+
+        for a_fig_path, fig_metadata in curr_active_pipeline.registered_output_files.items():
+            ## Get list of figures output and register them to neptune:
+            a_fig_context: IdentifyingContext = fig_metadata['context']
+            a_fig_specific_context: IdentifyingContext = a_fig_context.subtracting(sess_context) # subtract off the common session-level context.
+            # a_full_figure_path_key = f"session/{active_session_context_string}/figures/{str(a_fig_context)}"
+            a_full_figure_path_key: str = a_fig_specific_context.get_description(separator='/', include_property_names=True, key_value_separator=':')
+            if debug_print:
+                # print(f'a_fig_specific_context: {a_fig_specific_context}, a_full_figure_path_key: {a_full_figure_path_key}\n\ta_fig_path: {a_fig_path}, a_fig_context: {a_fig_context}\n')
+                print(f'Processing: {a_fig_specific_context}, full_key: {a_full_figure_path_key}')
+
+            fig_objects = fig_metadata.get('fig', None)
+            if (fig_objects is not None) and should_use_fig_objects:
+                # upload the actual figure objects
+                try:
+                    # Try to extract and upload the actual figure objects
+                    if not isinstance(fig_objects, (tuple, list)):
+                        fig_objects = (fig_objects,) # wrap in a tuple or convert list to tuple
+                    elif isinstance(fig_objects, list):
+                        fig_objects = tuple(fig_objects) # convert the list to a tuple
+                        
+                    # now fig_objects better be a tuple
+                    assert isinstance(fig_objects, tuple), f"{fig_objects} should be a tuple!"
+
+                    if len(fig_objects) == 1:
+                        # single element, use .upload(...)
+                        a_fig_obj = fig_objects[0]
+                        self.figures[a_full_figure_path_key].upload(a_fig_obj)
+                    else:
+                        # otherwise multi-figure, use append for a series list:
+                        for a_fig_obj in fig_objects:
+                            # also ,  step=epoch for custom index
+                            self.figures[a_full_figure_path_key].append(a_fig_obj) #, name=f"{a_full_figure_path_key}", description=f"{a_full_figure_path_key}" append the whole series of related figures
+                            
+                    if debug_print:
+                        print(f'\tSuccess: "{a_full_figure_path_key}": {len(fig_objects)} uploaded!')
+                    succeeded_fig_paths.append(a_fig_path)
+                    
+                except Exception as e:
+                    if debug_print:
+                        print(f'\tSkipping "{a_full_figure_path_key}": fig_objects found but raised exception {e}.')
+                    failed_fig_paths.append(a_fig_path)
+                    # raise e		
+            else:
+                if should_use_fig_objects:
+                    if debug_print:
+                        print(f'\tSkipping "{a_full_figure_path_key}": No figure objects found')
+                    failed_fig_paths.append(a_fig_path)
+                else:
+                    # this is the mode we expected -- upload as file
+                    try:
+                        self.figures[a_full_figure_path_key].upload(File(str(a_fig_path)))
+                        succeeded_fig_paths.append(a_fig_path)
+                        if debug_print:
+                            print(f'\tSuccess: "{a_full_figure_path_key}": file at path {str(a_fig_path)} uploaded!')
+                    except Exception as e:
+                        raise e
+                    
+        # end for loop
+        return succeeded_fig_paths, failed_fig_paths
 
 
 
