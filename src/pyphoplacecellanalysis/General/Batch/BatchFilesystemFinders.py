d7fafb34465f11a39e4f0c8ccb80dca8125228fe
Pho Hale <PhoHale@gmail.com>
2023-08-21 16:35:47 -0400

    find_externally_computed_session_h5_files

--- src/pyphoplacecellanalysis/General/Batch/BatchFilesystemFinders.py
+++ src/pyphoplacecellanalysis/General/Batch/BatchFilesystemFinders.py
@@ -28,6 +28,23 @@ def find_externally_computed_ripple_files(project_path, exclude_dirs=[]):
     return included_py_files
 
 
+def find_externally_computed_session_h5_files(project_path, exclude_dirs=[]):
+    # Find all .py files in the project directory and its subdirectories
+    if not isinstance(project_path, pathlib.Path):
+        project_path = pathlib.Path(project_path)
+    # py_files = project_path.glob("**/output/global_computations.h5")
+    py_files = project_path.glob("**/output/pipeline_results.h5")
+    py_files = [file_path for file_path in py_files] # to list
+
+    excluded_py_files = []
+    if exclude_dirs is not None:
+        # Find all .py files in the project directory and its subdirectories, excluding the 'my_exclude_dir' directory
+        exclude_paths = [project_path.joinpath(a_dir) for a_dir in exclude_dirs]
+        for an_exclude_path in exclude_paths:
+            excluded_py_files.extend([file_path for file_path in an_exclude_path.glob("**/*.py")])
+
+    included_py_files = [x for x in py_files if x not in excluded_py_files]
+    return included_py_files
 # # 2023-07-06 - New Ideas and External Ripple Detection Progress
 # @define()
 # class PipelineDataModificationRecipie:
