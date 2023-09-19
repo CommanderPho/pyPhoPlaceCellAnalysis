fa83b6abbe8b4f29fddd59c884026f29fd6d7d05
Pho Hale <PhoHale@gmail.com>
2023-09-19 19:04:16 -0400

    added working saveFile helper for jupyter widget

--- src/pyphoplacecellanalysis/GUI/IPyWidgets/pipeline_ipywidgets.py
+++ src/pyphoplacecellanalysis/GUI/IPyWidgets/pipeline_ipywidgets.py
@@ -3,6 +3,78 @@ from IPython.display import display
 from pyphocorehelpers.gui.Jupyter.JupyterButtonRowWidget import JupyterButtonRowWidget
 from pyphocorehelpers.Filesystem.open_in_system_file_manager import reveal_in_system_file_manager
 
+import pyphoplacecellanalysis.External.pyqtgraph as pg
+from pyphoplacecellanalysis.External.pyqtgraph.Qt import QtGui, QtCore, QtWidgets
+# from pyphoplacecellanalysis.External.pyqtgraph.parametertree.parameterTypes.file import popupFilePicker
+from pyphoplacecellanalysis.External.pyqtgraph.widgets.FileDialog import FileDialog
+
+from pathlib import Path
+from silx.gui import qt
+from silx.gui.dialog.ImageFileDialog import ImageFileDialog
+from silx.gui.dialog.DataFileDialog import DataFileDialog
+import silx.io
+
+# AbstractDataFileDialog
+
+def saveFile(on_save_file_callback, caption:str="Save as..", startDir=None, suggestedFileName='test.h5', filter="H5py File (*.h5)", default_suffix:str="h5"):
+	"""Save this Custom Eval Node to a .pEval file
+
+	fileDialog = saveFile(lambda fileName: print(f'_on_save_file(fileName: {fileName})'), caption="Save pickle as..", startDir=None, suggestedFileName='test.pkl', filter="Pickle File (*.pkl)", default_suffix="pkl")
+
+	fileDialog = saveFile(lambda fileName: print(f'_on_save_file(fileName: {fileName})'), caption="Save HDF5 file as..", startDir=None, suggestedFileName='test.h5', filter="H5py File (*.h5)", default_suffix="h5")
+
+	"""
+	if startDir is None:
+		startDir = '.'
+	if suggestedFileName is not None:
+		startFile = Path(startDir).joinpath(suggestedFileName).resolve()
+	else:
+		startFile = Path(startDir).resolve()
+	fileDialog: FileDialog = FileDialog(None, caption, str(startFile), filter)
+	# fileDialog.setDefaultFilename(suggestedFileName)
+	fileDialog.setDefaultSuffix(default_suffix)
+	fileDialog.setAcceptMode(QtWidgets.QFileDialog.AcceptMode.AcceptSave) 
+	fileDialog.show()
+	fileDialog.fileSelected.connect(on_save_file_callback)
+	fileDialog.exec_() # open modally
+	return fileDialog
+
+
+def openDialogAtHome():
+	# Clear the dialog
+	path = qt.QDir.homePath()
+	# dialog = self.createDialog()
+
+	dialog = DataFileDialog()
+	
+	# Execute the dialog as modal
+	result = dialog.exec()
+	if result:
+		print("Selection:")
+		print(dialog.selectedFile())
+		print(dialog.selectedUrl())
+	else:
+		print("Nothing selected")
+
+	dialog.setDirectory(path)
+
+	# # Execute the dialog as modal
+	# result = dialog.exec()
+	# self.printResult(dialog, result)
+	return dialog, result
+
+
+def try_save_pickle_as(original_file_path, file_confirmed_callback):
+	original_file_path: Path = Path(original_file_path).resolve()
+	def _perform_try_save_pickle_as(fileName):
+		print(f'_perform_try_save_pickle_as(fileName: {fileName}, original_file_path: {original_file_path})')
+		# TODO: perform the copy here:
+		return file_confirmed_callback(fileName)
+
+	fileDialog = saveFile(_perform_try_save_pickle_as, caption="Save pickle as..", startDir=original_file_path.parent, suggestedFileName=f'{original_file_path.stem}_bak.pkl', filter="Pickle File (*.pkl)", default_suffix="pkl")
+	# dialog = saveFile(lambda fileName: print(f'_on_save_file(fileName: {fileName})'), caption="Save HDF5 file as..", startDir=None, suggestedFileName='test.h5', filter="H5py File (*.h5)", default_suffix="h5")
+	return
+
 
 def interactive_pipeline_files(curr_active_pipeline, defer_display:bool=False) -> JupyterButtonRowWidget:
 	"""	Displays a row of four buttons relating to the curr_active_pipeline that reveal the Output Folder, global pickle, pipeline pickle, and .h5 export path in the system file explorer.
@@ -18,6 +90,7 @@ def interactive_pipeline_files(curr_active_pipeline, defer_display:bool=False) -
 			("global pickle", lambda _: reveal_in_system_file_manager(curr_active_pipeline.global_computation_results_pickle_path)),
 			("pipeline pickle", lambda _: reveal_in_system_file_manager(curr_active_pipeline.pickle_path)),
 			(".h5 export", lambda _: reveal_in_system_file_manager(curr_active_pipeline.h5_export_path)),
+			("TEST - Dialog", lambda _: try_save_pickle_as(curr_active_pipeline.global_computation_results_pickle_path)),
 			# ("ViTables .h5 export", lambda _: reveal_in_system_file_manager(curr_active_pipeline.h5_export_path))
 		]
 		
@@ -27,14 +100,23 @@ def interactive_pipeline_files(curr_active_pipeline, defer_display:bool=False) -
 		button_defns.append(("Figure Export Folder", lambda _: reveal_in_system_file_manager(figure_output_path)))
 
 	# Create and display the button
-	button_executor = JupyterButtonRowWidget(button_defns=button_defns, defer_display=defer_display)
+	button_executor = JupyterButtonRowWidget(button_defns=button_defns, defer_display=True)
+	# updating_button_executor = JupyterButtonRowWidget(button_defns=updating_button_defns, defer_display=True)
+
+	# combined_button_executor = widgets.VBox((widgets.HBox(button_executor.button_list), widgets.HBox(updating_button_executor.button_list)))
+	# combined_button_executor = widgets.VBox((button_executor.button_list, updating_button_executor.button_list))
+	# return display(combined_button_executor)
 	return button_executor
 
 
 def fullwidth_path_widget(a_path):
 	left_label = widgets.Label("session path:", layout=widgets.Layout(width='auto'))
-	right_label = widgets.Label(a_path, layout=widgets.Layout(width='auto', flex='1'))
-	hbox = widgets.HBox([left_label, right_label], layout=widgets.Layout(display='flex'))
+	right_label = widgets.Label(a_path, layout=widgets.Layout(width='auto', flex='1 1 auto', margin='0px'))
+	reveal_button = widgets.Button(description='Reveal', layout=widgets.Layout(flex='0 1 auto', width='auto', margin='0px'), disabled=(not Path(a_path).resolve().exists()), button_style='info', tooltip='Reveal in System Explorer', icon='folder-tree')
+	reveal_button.on_click(lambda _: reveal_in_system_file_manager(a_path))
+	box_layout = widgets.Layout(display='flex', flex_flow='row', align_items='stretch', width='70%')
+	hbox = widgets.Box(children=[left_label, right_label, reveal_button], layout=box_layout)
+	# hbox = widgets.HBox([left_label, right_label, reveal_button], layout=box_layout)
 	return hbox
 
 
@@ -81,5 +163,19 @@ def interactive_pipeline_widget(curr_active_pipeline):
 	session_path = str(curr_active_pipeline.get_output_path())
 	_session_path_widget = fullwidth_path_widget(a_path=session_path)
 	_button_executor = interactive_pipeline_files(curr_active_pipeline, defer_display=True)
-	_out_widget = widgets.VBox([_session_path_widget, _button_executor.root_widget])
+
+	updating_button_defns = [("Reload display functions...", lambda _: curr_active_pipeline.reload_default_display_functions()),
+			("Reload computation functions...", lambda _: curr_active_pipeline.reload_default_computation_functions()),
+			# ("pipeline pickle", lambda _: reveal_in_system_file_manager(curr_active_pipeline.pickle_path)),
+			# ("Try .h5 export", lambda _: curr_active_pipeline.export_pipeline_to_h5('output/2023_09_19-pipeline_test.h5') reveal_in_system_file_manager(curr_active_pipeline.h5_export_path)),
+			# ("TEST - Dialog", lambda _: try_save_pickle_as(curr_active_pipeline.global_computation_results_pickle_path)),
+			# ("ViTables .h5 export", lambda _: reveal_in_system_file_manager(curr_active_pipeline.h5_export_path))
+		]
+	updating_button_executor = JupyterButtonRowWidget(button_defns=updating_button_defns, defer_display=True)
+	# combined_button_executor = widgets.VBox((widgets.HBox(button_executor.button_list), widgets.HBox(updating_button_executor.button_list)))
+
+	# _out_widget = widgets.VBox([_session_path_widget, _button_executor.root_widget])
+	_out_widget = widgets.VBox([_session_path_widget, _button_executor.root_widget, updating_button_executor.root_widget])
+
+
 	return _out_widget
