7ec69ac51e3d30299298b39d42568b8822d28d7c
Pho Hale <PhoHale@gmail.com>
2023-09-22 01:44:38 -0400

    improved output

--- src/pyphoplacecellanalysis/SpecificResults/fourthYearPresentation.py
+++ src/pyphoplacecellanalysis/SpecificResults/fourthYearPresentation.py
@@ -2,6 +2,8 @@
 Contains code related to Pho Hale's 4th Year PhD Presentation on 2023-09-25
 
 """
+import numpy as np
+import pandas as pd
 from pyphoplacecellanalysis.General.Pipeline.Stages.ComputationFunctions.MultiContextComputationFunctions.LongShortTrackComputations import TruncationCheckingResults
 from pyphoplacecellanalysis.General.Mixins.CrossComputationComparisonHelpers import SplitPartitionMembership
 from pyphoplacecellanalysis.General.Pipeline.Stages.ComputationFunctions.MultiContextComputationFunctions.LongShortTrackComputations import JonathanFiringRateAnalysisResult
@@ -11,9 +13,11 @@ from pyphoplacecellanalysis.General.Pipeline.Stages.DisplayFunctions.MultiContex
 from neuropy.utils.matplotlib_helpers import perform_update_title_subtitle
 from pyphoplacecellanalysis.Pho2D.track_shape_drawing import add_vertical_track_bounds_lines, add_track_shapes
 from neuropy.plotting.ratemaps import plot_ratemap_1D
+from neuropy.core.epoch import Epoch
 from pyphoplacecellanalysis.General.Model.Configs.LongShortDisplayConfig import LongShortDisplayConfigManager
 
-
+from neuropy.utils.matplotlib_helpers import draw_epoch_regions
+import matplotlib.pyplot as plt
 
 
 def fig_remapping_cells(curr_active_pipeline):
@@ -83,3 +87,31 @@ def fig_remapping_cells(curr_active_pipeline):
 	graphics_output_dict['trivially_remapping_endcap_aclus'] = curr_active_pipeline.display('_display_short_long_pf1D_comparison', active_context.adding_context_if_missing(cell_subset='triv_remap_endcap'), included_any_context_neuron_ids=trivially_remapping_endcap_aclus, reuse_axs_tuple=None, single_figure=True, shared_kwargs=shared_kwargs, title_string="Trivially Remapping Cells", subtitle_string="1D Placefields", **top_level_shared_kwargs)
 
 	return graphics_output_dict
+
+
+def fig_surprise_results(curr_active_pipeline):
+	
+	global_results = curr_active_pipeline.computation_results['maze'].computed_data
+	active_extended_stats = global_results['extended_stats']
+	active_relative_entropy_results = active_extended_stats['pf_dt_sequential_surprise']
+	post_update_times = active_relative_entropy_results['post_update_times'] # (4152,) = (n_post_update_times,)
+	snapshot_differences_result_dict = active_relative_entropy_results['snapshot_differences_result_dict']
+	time_intervals = active_relative_entropy_results['time_intervals']
+	long_short_rel_entr_curves_frames = active_relative_entropy_results['long_short_rel_entr_curves_frames'] # (4152, 108, 63) = (n_post_update_times, n_neurons, n_xbins)
+	short_long_rel_entr_curves_frames = active_relative_entropy_results['short_long_rel_entr_curves_frames'] # (4152, 108, 63) = (n_post_update_times, n_neurons, n_xbins)
+	flat_relative_entropy_results = active_relative_entropy_results['flat_relative_entropy_results'] # (149, 63) - (nSnapshots, nXbins)
+	flat_jensen_shannon_distance_results = active_relative_entropy_results['flat_jensen_shannon_distance_results'] # (149, 63) - (nSnapshots, nXbins)
+	flat_jensen_shannon_distance_across_all_positions = np.sum(np.abs(flat_jensen_shannon_distance_results), axis=1) # sum across all position bins # (4152,) - (nSnapshots)
+	flat_surprise_across_all_positions = np.sum(np.abs(flat_relative_entropy_results), axis=1) # sum across all position bins # (4152,) - (nSnapshots)
+
+	fig, ax = plt.subplots()
+	ax.plot(post_update_times, flat_surprise_across_all_positions)
+	ax.set_ylabel('Relative Entropy across all positions')
+	ax.set_xlabel('t (seconds)')
+	epochs_collection, epoch_labels = draw_epoch_regions(curr_active_pipeline.sess.epochs, ax, facecolor=('red','cyan'), alpha=0.1, edgecolors=None, labels_kwargs={'y_offset': -0.05, 'size': 14}, defer_render=True, debug_print=False)
+	laps_epochs_collection, laps_epoch_labels = draw_epoch_regions(curr_active_pipeline.sess.laps.as_epoch_obj(), ax, facecolor='red', edgecolors='black', labels_kwargs={'y_offset': -16.0, 'size':8}, defer_render=True, debug_print=False)
+	replays_epochs_collection, replays_epoch_labels = draw_epoch_regions(active_filter_epoch_obj, ax, facecolor='orange', edgecolors=None, labels_kwargs=None, defer_render=False, debug_print=False)
+	fig.suptitle('flat_surprise_across_all_positions')
+	fig.show()
+
+	return fig, ax
