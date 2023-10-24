df6a548981ca479a93aa59bdd786251a076192e8
Pho Hale <PhoHale@gmail.com>
2023-10-24 05:23:38 -0400

    commented out an incorrect assert

--- src/pyphoplacecellanalysis/General/Batch/NonInteractiveProcessing.py
+++ src/pyphoplacecellanalysis/General/Batch/NonInteractiveProcessing.py
@@ -204,7 +204,7 @@ def batch_load_session(global_data_root_parent_path, active_data_mode_name, base
     if (use_direction_dependent_laps or (len(active_session_computation_configs) > 3)):
         # lap_direction_suffix_list = ['_odd', '_even', '_any'] # ['maze1_odd', 'maze1_even', 'maze1_any', 'maze2_odd', 'maze2_even', 'maze2_any', 'maze_odd', 'maze_even', 'maze_any']
         lap_direction_suffix_list = ['_odd', '_even', ''] # no '_any' prefix, instead reuses the existing names
-        assert len(lap_direction_suffix_list) == len(active_session_computation_configs), f"len(lap_direction_suffix_list): {len(lap_direction_suffix_list)}, len(active_session_computation_configs): {len(active_session_computation_configs)}, "
+        # assert len(lap_direction_suffix_list) == len(active_session_computation_configs), f"len(lap_direction_suffix_list): {len(lap_direction_suffix_list)}, len(active_session_computation_configs): {len(active_session_computation_configs)}, "
     else:
         lap_direction_suffix_list = ['']
 
