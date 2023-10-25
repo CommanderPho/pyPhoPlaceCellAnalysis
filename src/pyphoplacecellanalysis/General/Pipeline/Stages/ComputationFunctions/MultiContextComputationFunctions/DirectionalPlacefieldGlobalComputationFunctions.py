0cf61d360a21c5f7895c8677593b86e6b1379c56
Pho Hale <PhoHale@gmail.com>
2023-10-25 09:39:52 -0400

    bug fixes

--- src/pyphoplacecellanalysis/General/Pipeline/Stages/ComputationFunctions/MultiContextComputationFunctions/DirectionalPlacefieldGlobalComputationFunctions.py
+++ src/pyphoplacecellanalysis/General/Pipeline/Stages/ComputationFunctions/MultiContextComputationFunctions/DirectionalPlacefieldGlobalComputationFunctions.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 from functools import wraps
+from copy import deepcopy
 
 from pyphocorehelpers.mixins.member_enumerating import AllFunctionEnumeratingMixin
 from pyphocorehelpers.function_helpers import function_attributes
@@ -32,58 +33,58 @@ class DirectionalLapsHelpers:
     # lap_direction_suffix_list = ['_odd', '_even', ''] # no '_any' prefix, instead reuses the existing names
     split_directional_laps_name_parts = ['odd_laps', 'even_laps'] # , 'any_laps'
 
-	# ['maze_even_laps', 'maze_odd_laps']
+    # ['maze_even_laps', 'maze_odd_laps']
 
-	@classmethod
-	def split_specific_epoch_to_directional_laps(cls, a_name: str, a_sess, a_result, curr_active_pipeline, add_created_configs_to_pipeline:bool=True, debug_print=False):
-		""" 
+    @classmethod
+    def split_specific_epoch_to_directional_laps(cls, a_name: str, a_sess, a_result, curr_active_pipeline, add_created_configs_to_pipeline:bool=True, use_direction_dependent_laps=True, debug_print=False):
+        """ 
 
-			a_name, a_sess, a_result = global_epoch_name, global_session, global_results
-			curr_epoch_directional_lap_specific_configs, curr_epoch_split_directional_laps_dict, curr_epoch_split_directional_laps_config_names = cls.split_specific_epoch_to_directional_laps(a_name, a_sess, a_result, curr_active_pipeline, add_created_configs_to_pipeline=add_created_configs_to_pipeline)
+            a_name, a_sess, a_result = global_epoch_name, global_session, global_results
+            curr_epoch_directional_lap_specific_configs, curr_epoch_split_directional_laps_dict, curr_epoch_split_directional_laps_config_names = cls.split_specific_epoch_to_directional_laps(a_name, a_sess, a_result, curr_active_pipeline, add_created_configs_to_pipeline=add_created_configs_to_pipeline)
 
-		"""
-		directional_lap_specific_configs = {}
+        """
+        directional_lap_specific_configs = {}
 
-		split_directional_laps_config_names = [f'{a_name}_{a_lap_dir_description}' for a_lap_dir_description in cls.split_directional_laps_name_parts] # ['maze_odd_laps', 'maze_even_laps']
-		if debug_print:
-			print(f'\tsplit_directional_laps_config_names: {split_directional_laps_config_names}')
+        split_directional_laps_config_names = [f'{a_name}_{a_lap_dir_description}' for a_lap_dir_description in cls.split_directional_laps_name_parts] # ['maze_odd_laps', 'maze_even_laps']
+        if debug_print:
+            print(f'\tsplit_directional_laps_config_names: {split_directional_laps_config_names}')
 
-		# 'build_lap_computation_epochs(...)' based mode:
-		desired_computation_epochs = build_lap_computation_epochs(a_sess, use_direction_dependent_laps=use_direction_dependent_laps)
-		even_lap_specific_epochs, odd_lap_specific_epochs, any_lap_specific_epochs = desired_computation_epochs
+        # 'build_lap_computation_epochs(...)' based mode:
+        desired_computation_epochs = build_lap_computation_epochs(a_sess, use_direction_dependent_laps=use_direction_dependent_laps)
+        even_lap_specific_epochs, odd_lap_specific_epochs, any_lap_specific_epochs = desired_computation_epochs
 
-		split_directional_laps_dict = dict(zip(split_directional_laps_config_names, (even_lap_specific_epochs, odd_lap_specific_epochs)))
+        split_directional_laps_dict = dict(zip(split_directional_laps_config_names, (even_lap_specific_epochs, odd_lap_specific_epochs)))
 
-		# # manual mode:
-		# lap_specific_epochs = a_sess.laps.as_epoch_obj().get_non_overlapping().filtered_by_duration(1.0, 30.0) # set this to the laps object
-		# any_lap_specific_epochs = lap_specific_epochs.label_slice(lap_specific_epochs.labels[np.arange(len(a_sess.laps.lap_id))])
-		# even_lap_specific_epochs = lap_specific_epochs.label_slice(lap_specific_epochs.labels[np.arange(0, len(a_sess.laps.lap_id), 2)])
-		# odd_lap_specific_epochs = lap_specific_epochs.label_slice(lap_specific_epochs.labels[np.arange(1, len(a_sess.laps.lap_id), 2)])
+        # # manual mode:
+        # lap_specific_epochs = a_sess.laps.as_epoch_obj().get_non_overlapping().filtered_by_duration(1.0, 30.0) # set this to the laps object
+        # any_lap_specific_epochs = lap_specific_epochs.label_slice(lap_specific_epochs.labels[np.arange(len(a_sess.laps.lap_id))])
+        # even_lap_specific_epochs = lap_specific_epochs.label_slice(lap_specific_epochs.labels[np.arange(0, len(a_sess.laps.lap_id), 2)])
+        # odd_lap_specific_epochs = lap_specific_epochs.label_slice(lap_specific_epochs.labels[np.arange(1, len(a_sess.laps.lap_id), 2)])
 
-		# split_directional_laps_dict = {'even_laps': even_lap_specific_epochs, 'odd_laps': odd_lap_specific_epochs, 'any_laps': any_lap_specific_epochs}
+        # split_directional_laps_dict = {'even_laps': even_lap_specific_epochs, 'odd_laps': odd_lap_specific_epochs, 'any_laps': any_lap_specific_epochs}
 
-		if debug_print:
-			print(f'any_lap_specific_epochs: {any_lap_specific_epochs}\n\teven_lap_specific_epochs: {even_lap_specific_epochs}\n\todd_lap_specific_epochs: {odd_lap_specific_epochs}\n') # lap_specific_epochs: {lap_specific_epochs}\n\t
+        if debug_print:
+            print(f'any_lap_specific_epochs: {any_lap_specific_epochs}\n\teven_lap_specific_epochs: {even_lap_specific_epochs}\n\todd_lap_specific_epochs: {odd_lap_specific_epochs}\n') # lap_specific_epochs: {lap_specific_epochs}\n\t
 
-		for a_split_directional_laps_config_name, lap_dir_epochs in split_directional_laps_dict.items():
-			if debug_print:
-				print(f'\ta_split_directional_laps_config_name: {a_split_directional_laps_config_name}')
-			active_config_copy = deepcopy(curr_active_pipeline.active_configs[a_name])
-			# active_config_copy.computation_config.pf_params.computation_epochs = active_config_copy.computation_config.pf_params.computation_epochs.label_slice(odd_lap_specific_epochs.labels)
-			## Just overwrite directly:
-			active_config_copy.computation_config.pf_params.computation_epochs = lap_dir_epochs
-			directional_lap_specific_configs[a_split_directional_laps_config_name] = active_config_copy
-			if add_created_configs_to_pipeline:
-				curr_active_pipeline.active_configs[a_split_directional_laps_config_name] = active_config_copy
-				# When a new config is added, new results and stuff should be added too.
-				curr_active_pipeline.filtered_sessions[a_split_directional_laps_config_name] = curr_active_pipeline.filtered_sessions[a_name]
-				curr_active_pipeline.filtered_epochs[a_split_directional_laps_config_name] = curr_active_pipeline.filtered_epochs[a_name]
-				curr_active_pipeline.filtered_contexts[a_split_directional_laps_config_name] = curr_active_pipeline.filtered_contexts[a_name]
+        for a_split_directional_laps_config_name, lap_dir_epochs in split_directional_laps_dict.items():
+            if debug_print:
+                print(f'\ta_split_directional_laps_config_name: {a_split_directional_laps_config_name}')
+            active_config_copy = deepcopy(curr_active_pipeline.active_configs[a_name])
+            # active_config_copy.computation_config.pf_params.computation_epochs = active_config_copy.computation_config.pf_params.computation_epochs.label_slice(odd_lap_specific_epochs.labels)
+            ## Just overwrite directly:
+            active_config_copy.computation_config.pf_params.computation_epochs = lap_dir_epochs
+            directional_lap_specific_configs[a_split_directional_laps_config_name] = active_config_copy
+            if add_created_configs_to_pipeline:
+                curr_active_pipeline.active_configs[a_split_directional_laps_config_name] = active_config_copy
+                # When a new config is added, new results and stuff should be added too.
+                curr_active_pipeline.filtered_sessions[a_split_directional_laps_config_name] = curr_active_pipeline.filtered_sessions[a_name]
+                curr_active_pipeline.filtered_epochs[a_split_directional_laps_config_name] = curr_active_pipeline.filtered_epochs[a_name]
+                curr_active_pipeline.filtered_contexts[a_split_directional_laps_config_name] = curr_active_pipeline.filtered_contexts[a_name]
 
-				curr_active_pipeline.computation_results[a_split_directional_laps_config_name] = None # empty
+                curr_active_pipeline.computation_results[a_split_directional_laps_config_name] = None # empty
 
-			# end loop over split_directional_lap types:
-		return directional_lap_specific_configs, split_directional_laps_dict, split_directional_laps_config_names
+            # end loop over split_directional_lap types:
+        return directional_lap_specific_configs, split_directional_laps_dict, split_directional_laps_config_names
 
     @classmethod
     def split_to_directional_laps(cls, curr_active_pipeline, include_includelist=None, add_created_configs_to_pipeline:bool=True, debug_print=False):
@@ -93,66 +94,66 @@ class DirectionalLapsHelpers:
         if add_created_configs_to_pipeline is False, just returns the built configs and doesn't add them to the pipeline.
 
         """
-		if include_includelist is None:
-			use_global_epoch_only_mode: bool = True # 2023-10-24 - 4:19pm - Duplicates only the `global_epoch_name` results for the directional laps and then filters from there
-			long_epoch_name, short_epoch_name, global_epoch_name = curr_active_pipeline.find_LongShortGlobal_epoch_names()
+        if include_includelist is None:
+            use_global_epoch_only_mode: bool = True # 2023-10-24 - 4:19pm - Duplicates only the `global_epoch_name` results for the directional laps and then filters from there
+            long_epoch_name, short_epoch_name, global_epoch_name = curr_active_pipeline.find_LongShortGlobal_epoch_names()
             # include_includelist = owning_pipeline_reference.active_completed_computation_result_names # ['maze', 'sprinkle']
-			include_includelist = [global_epoch_name] # ['maze'] # only for maze
-		
-		computed_base_epoch_names = []
+            include_includelist = [global_epoch_name] # ['maze'] # only for maze
+        
+        computed_base_epoch_names = []
         lap_estimation_parameters = curr_active_pipeline.sess.config.preprocessing_parameters.epoch_estimation_parameters.laps
         assert lap_estimation_parameters is not None
         use_direction_dependent_laps: bool = lap_estimation_parameters.get('use_direction_dependent_laps', True)
-		if debug_print:
-	        print(f'split_to_directional_laps(...): use_direction_dependent_laps: {use_direction_dependent_laps}')
+        if debug_print:
+            print(f'split_to_directional_laps(...): use_direction_dependent_laps: {use_direction_dependent_laps}')
         long_epoch_name, short_epoch_name, global_epoch_name = curr_active_pipeline.find_LongShortGlobal_epoch_names()
         long_session, short_session, global_session = [curr_active_pipeline.filtered_sessions[an_epoch_name] for an_epoch_name in [long_epoch_name, short_epoch_name, global_epoch_name]]
         long_results, short_results, global_results = [curr_active_pipeline.computation_results[an_epoch_name]['computed_data'] for an_epoch_name in [long_epoch_name, short_epoch_name, global_epoch_name]]
         ## After all top-level computations are done, compute the subsets for direction laps
         directional_lap_specific_configs = {}
-		split_directional_laps_dict = {}
-		split_directional_laps_config_names = []
+        split_directional_laps_dict = {}
+        split_directional_laps_config_names = []
 
         if use_direction_dependent_laps:
-			if debug_print:
-	            print(f'split_to_directional_laps(...) processing for directional laps...')
+            if debug_print:
+                print(f'split_to_directional_laps(...) processing for directional laps...')
             # for a_name, a_sess, a_result in zip((long_epoch_name, short_epoch_name, global_epoch_name), (long_session, short_session, global_session), (long_results, short_results, global_results)):
 
-			for an_epoch_name in include_includelist:
-				# for `use_global_epoch_only_mode == True` mode:
-				# an_epoch_name, a_sess, a_result = global_epoch_name, global_session, global_results
-				a_sess, a_result = curr_active_pipeline.filtered_sessions[an_epoch_name], curr_active_pipeline.computation_results[an_epoch_name]['computed_data']
+            for an_epoch_name in include_includelist:
+                # for `use_global_epoch_only_mode == True` mode:
+                # an_epoch_name, a_sess, a_result = global_epoch_name, global_session, global_results
+                a_sess, a_result = curr_active_pipeline.filtered_sessions[an_epoch_name], curr_active_pipeline.computation_results[an_epoch_name]['computed_data']
 
-				curr_epoch_directional_lap_specific_configs, curr_epoch_split_directional_laps_dict, curr_epoch_split_directional_laps_config_names = cls.split_specific_epoch_to_directional_laps(an_epoch_name, a_sess, a_result, curr_active_pipeline, add_created_configs_to_pipeline=add_created_configs_to_pipeline)
+                curr_epoch_directional_lap_specific_configs, curr_epoch_split_directional_laps_dict, curr_epoch_split_directional_laps_config_names = cls.split_specific_epoch_to_directional_laps(an_epoch_name, a_sess, a_result, curr_active_pipeline, add_created_configs_to_pipeline=add_created_configs_to_pipeline)
 
-				directional_lap_specific_configs |= curr_epoch_directional_lap_specific_configs
-				split_directional_laps_dict |= curr_epoch_split_directional_laps_dict
-				split_directional_laps_config_names.extend(curr_epoch_split_directional_laps_dict)
+                directional_lap_specific_configs |= curr_epoch_directional_lap_specific_configs
+                split_directional_laps_dict |= curr_epoch_split_directional_laps_dict
+                split_directional_laps_config_names.extend(curr_epoch_split_directional_laps_dict)
 
-				# end loop over filter epochs:
-				if debug_print:
-					print(f'split_directional_laps_config_names: {split_directional_laps_config_names}')
+                # end loop over filter epochs:
+                if debug_print:
+                    print(f'split_directional_laps_config_names: {split_directional_laps_config_names}')
 
-				## Actually do the filtering now. We have 
-				updated_active_session_pseudo_filter_configs = {k:curr_active_pipeline.active_configs[k].filter_config['filter_function'] for k in split_directional_laps_config_names} # split_directional_laps_config_names: ['maze_odd_laps', 'maze_even_laps']
-				curr_active_pipeline.filter_sessions(updated_active_session_pseudo_filter_configs, changed_filters_ignore_list=['maze1','maze2','maze'], debug_print=False)
+                ## Actually do the filtering now. We have 
+                updated_active_session_pseudo_filter_configs = {k:curr_active_pipeline.active_configs[k].filter_config['filter_function'] for k in split_directional_laps_config_names} # split_directional_laps_config_names: ['maze_odd_laps', 'maze_even_laps']
+                curr_active_pipeline.filter_sessions(updated_active_session_pseudo_filter_configs, changed_filters_ignore_list=['maze1','maze2','maze'], debug_print=False)
 
-				## Perform the computations which builds the computation results:
-				_out = curr_active_pipeline.perform_computations(computation_functions_name_includelist=['pf_computation', 'pfdt_computation', 'firing_rate_trends', 'position_decoding'],
-					enabled_filter_names=split_directional_laps_config_names,
-					# computation_kwargs_list=[dict(ndim=1)],
-					fail_on_exception=True, debug_print=True)
+                ## Perform the computations which builds the computation results:
+                _out = curr_active_pipeline.perform_computations(computation_functions_name_includelist=['pf_computation', 'pfdt_computation', 'firing_rate_trends', 'position_decoding'],
+                    enabled_filter_names=split_directional_laps_config_names,
+                    # computation_kwargs_list=[dict(ndim=1)],
+                    fail_on_exception=True, debug_print=True)
 
-				computed_base_epoch_names.append(an_epoch_name)
-				
-			# end for an_epoch_name
+                computed_base_epoch_names.append(an_epoch_name)
+                
+            # end for an_epoch_name
         # end if use_direction_dependent_laps
         return curr_active_pipeline, directional_lap_specific_configs, split_directional_laps_dict, split_directional_laps_config_names, computed_base_epoch_names
 
 
 
 class DirectionalPlacefieldGlobalComputationFunctions(AllFunctionEnumeratingMixin, metaclass=ComputationFunctionRegistryHolder):
-	""" functions related to directional placefield computations. """
+    """ functions related to directional placefield computations. """
     _computationGroupName = 'directional_pfs'
     _computationPrecidence = 1000
     _is_global = True
@@ -169,25 +170,25 @@ class DirectionalPlacefieldGlobalComputationFunctions(AllFunctionEnumeratingMixi
             computation_result.computed_data['DirectionalLaps']
                 ['DirectionalLaps']['directional_lap_specific_configs']
                 ['DirectionalLaps']['split_directional_laps_dict']
-				['DirectionalLaps']['split_directional_laps_names']
+                ['DirectionalLaps']['split_directional_laps_names']
                 ['DirectionalLaps']['computed_base_epoch_names']
 
         
         """
         if include_includelist is None:
-			long_epoch_name, short_epoch_name, global_epoch_name = curr_active_pipeline.find_LongShortGlobal_epoch_names()
+            long_epoch_name, short_epoch_name, global_epoch_name = curr_active_pipeline.find_LongShortGlobal_epoch_names()
             # include_includelist = owning_pipeline_reference.active_completed_computation_result_names # ['maze', 'sprinkle']
-			include_includelist = [global_epoch_name] # ['maze'] # only for maze
+            include_includelist = [global_epoch_name] # ['maze'] # only for maze
         
-		## Adds ['*_even_laps', '*_odd_laps'] pseduofilters
-		curr_active_pipeline, directional_lap_specific_configs, split_directional_laps_dict, split_directional_laps_config_names, computed_base_epoch_names = DirectionalLapsHelpers.split_to_directional_laps(curr_active_pipeline, include_includelist=include_includelist, add_created_configs_to_pipeline=True)
-		# curr_active_pipeline, directional_lap_specific_configs = constrain_to_laps(curr_active_pipeline)
-		# list(directional_lap_specific_configs.keys())
+        ## Adds ['*_even_laps', '*_odd_laps'] pseduofilters
+        owning_pipeline_reference, directional_lap_specific_configs, split_directional_laps_dict, split_directional_laps_config_names, computed_base_epoch_names = DirectionalLapsHelpers.split_to_directional_laps(owning_pipeline_reference, include_includelist=include_includelist, add_created_configs_to_pipeline=True)
+        # curr_active_pipeline, directional_lap_specific_configs = constrain_to_laps(curr_active_pipeline)
+        # list(directional_lap_specific_configs.keys())
 
         global_computation_results.computed_data['DirectionalLaps'] = DynamicParameters.init_from_dict({
             'directional_lap_specific_configs': directional_lap_specific_configs,
-			'split_directional_laps_dict': split_directional_laps_dict,
-			'split_directional_laps_names': split_directional_laps_config_names,
+            'split_directional_laps_dict': split_directional_laps_dict,
+            'split_directional_laps_names': split_directional_laps_config_names,
             'computed_base_epoch_names': computed_base_epoch_names,
         })
         return global_computation_results
