c82de5ce456f5fd11020f5f2610a5c3db2d0d8d4
Pho Hale <PhoHale@gmail.com>
2023-11-03 10:12:22 -0400

    11/2 change

--- src/pyphoplacecellanalysis/GUI/PyQtPlot/Widgets/GraphicsWidgets/DirectionalTemplatesRastersDebugger.py
+++ src/pyphoplacecellanalysis/GUI/PyQtPlot/Widgets/GraphicsWidgets/DirectionalTemplatesRastersDebugger.py
@@ -1,5 +1,6 @@
 # DirectionalTemplatesRastersDebugger
 import numpy as np
+import pandas as pd
 from copy import deepcopy
 import pyphoplacecellanalysis.External.pyqtgraph as pg
 from pyphoplacecellanalysis.General.Mixins.DataSeriesColorHelpers import DataSeriesColorHelpers
@@ -7,6 +8,7 @@ from pyphoplacecellanalysis.General.Pipeline.Stages.DisplayFunctions.SpikeRaster
 from pyphoplacecellanalysis.General.Pipeline.Stages.DisplayFunctions.SpikeRasters import RasterScatterPlotManager, UnitSortOrderManager, _build_default_tick, _build_scatter_plotting_managers, _prepare_spikes_df_from_filter_epochs, _subfn_build_and_add_scatterplot_row
 from pyphoplacecellanalysis.General.Pipeline.Stages.DisplayFunctions.SpikeRasters import _plot_multi_sort_raster_browser
 
+from pyphoplacecellanalysis.GUI.PyQtPlot.Widgets.Mixins.Render2DScrollWindowPlot import Render2DScrollWindowPlotMixin, ScatterItemData
 
 from pyphocorehelpers.programming_helpers import metadata_attributes
 from pyphocorehelpers.function_helpers import function_attributes
@@ -89,3 +91,74 @@ def _debug_plot_directional_template_rasters(spikes_df, active_epochs_df, track_
 
     # app, win, plots, plots_data, on_update_active_epoch, on_update_active_scatterplot_kwargs = _plot_multi_sort_raster_browser(spikes_df, included_neuron_ids, unit_sort_orders_dict=unit_sort_orders_dict, unit_colors_list_dict=unit_colors_list_dict, scatter_app_name='pho_directional_laps_rasters', defer_show=False, active_context=None)
     return odd_display_outputs, even_display_outputs
+
+
+
+
+@function_attributes(short_name=None, tags=['not-yet-working', 'debug'], input_requires=[], output_provides=[], uses=[], used_by=[], creation_date='2023-11-02 17:12', related_items=[])
+def debug_plot_selected_spikes(plots, plots_data, shared_directional_aclus_only_neuron_IDs, directional_laps_epoch_selected_spikes_fragile_linear_neuron_IDX_dict, active_epoch_idx, _active_plot_identifier:str='long_odd'):
+	""" overlays the representitive times for each unit within the epoch (such as the median spike time, or the center-of-mass on the spike rasters. 
+	
+	Usage:
+		## Specific:
+		_active_plot_identifier = 'long_odd'
+		debug_plot_selected_spikes(plots=odd_plots, plots_data=odd_plots_data, shared_directional_aclus_only_neuron_IDs=track_templates.shared_LR_aclus_only_neuron_IDs, directional_laps_epoch_selected_spikes_fragile_linear_neuron_IDX_dict=odd_laps_epoch_selected_spikes_fragile_linear_neuron_IDX_dict, 
+				_active_plot_identifier=_active_plot_identifier)
+
+		_active_plot_identifier = 'short_odd'
+		debug_plot_selected_spikes(plots=odd_plots, plots_data=odd_plots_data, shared_directional_aclus_only_neuron_IDs=track_templates.shared_LR_aclus_only_neuron_IDs, directional_laps_epoch_selected_spikes_fragile_linear_neuron_IDX_dict=odd_laps_epoch_selected_spikes_fragile_linear_neuron_IDX_dict, 
+				_active_plot_identifier=_active_plot_identifier)
+
+			
+		_active_plot_identifier = 'long_even'
+		debug_plot_selected_spikes(plots=even_plots, plots_data=even_plots_data, shared_directional_aclus_only_neuron_IDs=track_templates.shared_RL_aclus_only_neuron_IDs, directional_laps_epoch_selected_spikes_fragile_linear_neuron_IDX_dict=even_laps_epoch_selected_spikes_fragile_linear_neuron_IDX_dict,
+				_active_plot_identifier=_active_plot_identifier)
+
+		_active_plot_identifier = 'short_even'
+		debug_plot_selected_spikes(plots=even_plots, plots_data=even_plots_data, shared_directional_aclus_only_neuron_IDs=track_templates.shared_RL_aclus_only_neuron_IDs, directional_laps_epoch_selected_spikes_fragile_linear_neuron_IDX_dict=even_laps_epoch_selected_spikes_fragile_linear_neuron_IDX_dict,
+				_active_plot_identifier=_active_plot_identifier)
+
+
+	"""
+	# Captures: odd_laps_epoch_selected_spikes_fragile_linear_neuron_IDX_dict, track_templates.shared_LR_aclus_only_neuron_IDs, active_epoch_idx
+	
+
+	# Per group of two (odd/even):
+	plots_data.all_selected_spots_dict = {}
+	plots_data.all_selected_scatterplot_tooltips_kwargs_dict = {}
+	plots_data_dict = plots_data.plots_data_dict # list(plots_data_dict.keys()) # ['long_odd', 'short_odd']
+
+
+	# an_ax = plots.ax[_active_plot_identifier]
+	a_scatter_plot = plots.scatter_plots[_active_plot_identifier]
+
+	# Converts the selected spikes information dict (containing the median/first spikes for each epoch) into a spikes_df capable of being rendered on the raster plot.
+	selected_spike_fragile_neuron_IDX = np.squeeze(directional_laps_epoch_selected_spikes_fragile_linear_neuron_IDX_dict[active_epoch_idx][:,0]).astype('int')
+	selected_spike_aclus = shared_directional_aclus_only_neuron_IDs[selected_spike_fragile_neuron_IDX].astype('int')
+	selected_spike_times = np.squeeze(directional_laps_epoch_selected_spikes_fragile_linear_neuron_IDX_dict[active_epoch_idx][:,1]) # (n_cells, 2)
+	selected_spike_epoch_idx = np.full_like(selected_spike_fragile_neuron_IDX, active_epoch_idx).astype('int')
+
+	selected_spike_df = pd.DataFrame({'t': selected_spike_times, 'fragile_linear_neuron_IDX': selected_spike_fragile_neuron_IDX, 'aclu': selected_spike_aclus, 'epoch_IDX': selected_spike_epoch_idx})
+	selected_spike_df['t_rel_seconds'] = selected_spike_df['t']
+	selected_spike_df['neuron_type'] = False # stupid workaround
+	selected_spike_df['flat_spike_idx'] = -1 # stupid workaround
+
+	# Update the dataframe
+	active_spikes_shit = deepcopy(selected_spike_df)
+	active_spikes_shit = plots_data_dict[_active_plot_identifier].unit_sort_manager.update_spikes_df_visualization_columns(active_spikes_shit)
+
+	## Build the spots for the raster plot:
+	plots_data.all_selected_spots_dict[_active_plot_identifier], plots_data.all_selected_scatterplot_tooltips_kwargs_dict[_active_plot_identifier] = Render2DScrollWindowPlotMixin.build_spikes_all_spots_from_df(active_spikes_shit, plots_data_dict[_active_plot_identifier].raster_plot_manager.config_fragile_linear_neuron_IDX_map, should_return_data_tooltips_kwargs=True)
+
+	# Override the pen for the selected spots, the default renders them looking exactly like normal spikes which is no good:
+	for a_point in plots_data.all_selected_spots_dict[_active_plot_identifier]:
+		a_point['pen'] = pg.mkPen('#FFFFFF', width=4.5)
+
+
+	# normal_all_spots_dict = plots_data.all_spots_dict[_active_plot_identifier]
+	selected_all_spots_dict = plots_data.all_selected_spots_dict[_active_plot_identifier]
+	# merged_all_spots_dict = (plots_data.all_spots_dict[_active_plot_identifier] + plots_data.all_selected_spots_dict[_active_plot_identifier])
+
+	## Add the median spikes to the plots:
+	a_scatter_plot.addPoints(selected_all_spots_dict)
+	
