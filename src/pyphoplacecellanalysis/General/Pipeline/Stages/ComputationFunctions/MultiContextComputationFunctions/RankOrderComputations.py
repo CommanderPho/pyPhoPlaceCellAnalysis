347eb639b06cda2b125b0bd66f36c70ed0a20dcf
Pho Hale <PhoHale@gmail.com>
2023-11-06 15:22:51 -0500

    working on highlighting the correct item in each epoch

--- src/pyphoplacecellanalysis/General/Pipeline/Stages/ComputationFunctions/MultiContextComputationFunctions/RankOrderComputations.py
+++ src/pyphoplacecellanalysis/General/Pipeline/Stages/ComputationFunctions/MultiContextComputationFunctions/RankOrderComputations.py
@@ -483,10 +483,21 @@ class RankOrderAnalyses:
             even_laps_short_z_score_values = even_laps_short_z_score_values[num_missing_points:]
             odd_laps_short_z_score_values = odd_laps_short_z_score_values[num_missing_points:]
 
-        long_even_out_plot_1D = p1.plot(epoch_idx_list, even_laps_long_z_score_values, pen=None, symbolBrush='orange', symbolPen='w', symbol='o', name='long_even') ## setting pen=None disables line drawing
-        long_odd_out_plot_1D = p1.plot(epoch_idx_list, odd_laps_long_z_score_values, pen=None, symbolBrush='red', symbolPen='w', symbol='p', name='long_odd') ## setting pen=None disables line drawing
-        short_even_out_plot_1D = p1.plot(epoch_idx_list, even_laps_short_z_score_values, pen=None, symbolBrush='blue', symbolPen='w', symbol='p', name='short_even') ## setting pen=None disables line drawing
-        short_odd_out_plot_1D = p1.plot(epoch_idx_list, odd_laps_short_z_score_values, pen=None, symbolBrush='teal', symbolPen='w', symbol='p', name='short_odd') ## setting pen=None disables line drawing
+
+        # Need to modify the symbol for each one, to emphasize the correct one?
+
+        ## Build indicators for the right index
+        # symbolPen = pg.mkPen('#FFFFFF')
+
+
+        symbolPens = [pg.mkPen('#FFFFFF11') for idx in epoch_idx_list]
+        # determine the "correct" items
+
+
+        long_even_out_plot_1D = p1.plot(epoch_idx_list, even_laps_long_z_score_values, pen=None, symbolBrush='orange', symbolPen=symbolPens, symbol='o', name='long_even') ## setting pen=None disables line drawing
+        long_odd_out_plot_1D = p1.plot(epoch_idx_list, odd_laps_long_z_score_values, pen=None, symbolBrush='red', symbolPen=symbolPens, symbol='p', name='long_odd') ## setting pen=None disables line drawing
+        short_even_out_plot_1D = p1.plot(epoch_idx_list, even_laps_short_z_score_values, pen=None, symbolBrush='blue', symbolPen=symbolPens, symbol='p', name='short_even') ## setting pen=None disables line drawing
+        short_odd_out_plot_1D = p1.plot(epoch_idx_list, odd_laps_short_z_score_values, pen=None, symbolBrush='teal', symbolPen=symbolPens, symbol='p', name='short_odd') ## setting pen=None disables line drawing
         return app, win, p1, (long_even_out_plot_1D, long_odd_out_plot_1D, short_even_out_plot_1D, short_odd_out_plot_1D)
 
     def _perform_plot_z_score_diff(epoch_idx_list, even_laps_long_short_z_score_diff_values, odd_laps_long_short_z_score_diff_values, variable_name='Lap'):
