0ce7640d4ee7f47e66444bbb4bce808d6b824fb4
Pho Hale <PhoHale@gmail.com>
2023-11-14 01:20:38 -0500

    improved docs

--- src/pyphoplacecellanalysis/Pho2D/matplotlib/visualize_heatmap.py
+++ src/pyphoplacecellanalysis/Pho2D/matplotlib/visualize_heatmap.py
@@ -76,7 +76,7 @@ import numpy as np
 import pyphoplacecellanalysis.External.pyqtgraph as pg
 from pyphoplacecellanalysis.External.pyqtgraph.Qt import QtCore, QtGui
 
-@function_attributes(short_name=None, tags=['pyqtgraph', 'app', 'window'], input_requires=[], output_provides=[], uses=[], used_by=['plot_kourosh_activity_style_figure'], creation_date='2023-06-21 15:27', related_items=[])
+@function_attributes(short_name='heatmap_pyqtgraph', tags=['pyqtgraph', 'heatmap', 'app', 'window'], input_requires=[], output_provides=[], uses=[], used_by=['plot_kourosh_activity_style_figure'], creation_date='2023-06-21 15:27', related_items=[])
 def visualize_heatmap_pyqtgraph(data, win=None, show_value_labels=False, title="Simple Heatmap", show_xticks=False, show_yticks=False, show_colorbar=False, defer_show:bool = False):
     """
     Creates a simple heatmap visualization of the given 2D numpy array data.
