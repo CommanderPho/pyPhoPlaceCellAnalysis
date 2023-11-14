faa2a283963fc44bdb46cff93c023438f72a4599
Pho Hale <PhoHale@gmail.com>
2023-11-14 07:37:05 -0500

    almost working RankOrderDebugger

--- src/pyphoplacecellanalysis/GUI/PyQtPlot/Widgets/ContainerBased/RankOrderDebugger.py
+++ src/pyphoplacecellanalysis/GUI/PyQtPlot/Widgets/ContainerBased/RankOrderDebugger.py
@@ -126,14 +126,15 @@ class RankOrderDebugger:
     global_spikes_df: pd.DataFrame = field()
     active_epochs_df: pd.DataFrame = field()
     track_templates: TrackTemplates = field()
-    even_laps_epoch_selected_spikes_fragile_linear_neuron_IDX_dict: Dict = field()
-    odd_laps_epoch_selected_spikes_fragile_linear_neuron_IDX_dict: Dict = field()
+    RL_active_epochs_selected_spikes_fragile_linear_neuron_IDX_dict: Dict = field()
+    LR_active_epochs_selected_spikes_fragile_linear_neuron_IDX_dict: Dict = field()
     
     plots: RenderPlots = field(init=False)
-    
+    plots_data: RenderPlotsData = field(init=False)
+
 
     @classmethod
-    def init_rank_order_debugger(cls, global_spikes_df: pd.DataFrame, global_epochs_df: pd.DataFrame, track_templates: TrackTemplates, even_laps_epoch_selected_spikes_fragile_linear_neuron_IDX_dict: Dict, odd_laps_epoch_selected_spikes_fragile_linear_neuron_IDX_dict: Dict):
+    def init_rank_order_debugger(cls, global_spikes_df: pd.DataFrame, active_epochs_df: pd.DataFrame, track_templates: TrackTemplates, RL_active_epoch_selected_spikes_fragile_linear_neuron_IDX_dict: Dict, LR_active_epoch_selected_spikes_fragile_linear_neuron_IDX_dict: Dict):
         """ NOT YET FINISHED!
         long_epoch_name, short_epoch_name, global_epoch_name = curr_active_pipeline.find_LongShortGlobal_epoch_names()
         global_spikes_df = deepcopy(curr_active_pipeline.computation_results[global_epoch_name]['computed_data'].pf1D.spikes_df)
@@ -141,49 +142,44 @@ class RankOrderDebugger:
         global_laps_epochs_df = global_laps.to_dataframe()
           
         """
-        _obj = cls(global_spikes_df=global_spikes_df, active_epochs_df=global_epochs_df.copy(), track_templates=track_templates,
-             even_laps_epoch_selected_spikes_fragile_linear_neuron_IDX_dict=even_laps_epoch_selected_spikes_fragile_linear_neuron_IDX_dict, odd_laps_epoch_selected_spikes_fragile_linear_neuron_IDX_dict=odd_laps_epoch_selected_spikes_fragile_linear_neuron_IDX_dict)
+        _obj = cls(global_spikes_df=global_spikes_df, active_epochs_df=active_epochs_df.copy(), track_templates=track_templates,
+             RL_laps_epoch_selected_spikes_fragile_linear_neuron_IDX_dict=RL_active_epoch_selected_spikes_fragile_linear_neuron_IDX_dict, odd_laps_epoch_selected_spikes_fragile_linear_neuron_IDX_dict=LR_active_epoch_selected_spikes_fragile_linear_neuron_IDX_dict)
 
+        name:str = 'RankOrderDebugger'
+        LR_display_outputs, RL_display_outputs = _debug_plot_directional_template_rasters(_obj.global_spikes_df, _obj.active_epochs_df, _obj.track_templates)
+        LR_app, LR_win, LR_plots, LR_plots_data, LR_on_update_active_epoch, LR_on_update_active_scatterplot_kwargs = LR_display_outputs
+        RL_app, RL_win, RL_plots, RL_plots_data, RL_on_update_active_epoch, RL_on_update_active_scatterplot_kwargs = RL_display_outputs
 
-        odd_display_outputs, even_display_outputs = _debug_plot_directional_template_rasters(global_spikes_df, self.active_epochs_df, self.track_templates)
-        odd_app, odd_win, odd_plots, odd_plots_data, odd_on_update_active_epoch, odd_on_update_active_scatterplot_kwargs = odd_display_outputs
-        even_app, even_win, even_plots, even_plots_data, even_on_update_active_epoch, even_on_update_active_scatterplot_kwargs = even_display_outputs
-
-
-
-
+        _obj.plots = RenderPlots(name=name, LR_app=LR_app, LR_win=LR_win, LR_plots=LR_plots, RL_app=RL_app, RL_win=RL_win, RL_plots=RL_plots, )
+        _obj.plots_data = RenderPlotsData(name=name, LR_plots_data=LR_plots_data, LR_on_update_active_epoch=LR_on_update_active_epoch, LR_on_update_active_scatterplot_kwargs=LR_on_update_active_scatterplot_kwargs,
+                                           RL_plots_data=RL_plots_data, RL_on_update_active_epoch=RL_on_update_active_epoch, RL_on_update_active_scatterplot_kwargs=RL_on_update_active_scatterplot_kwargs)
         
-
         ## Build the selected spikes df:
-
-        (even_selected_spike_df, even_neuron_id_to_new_IDX_map), (odd_selected_spike_df, odd_neuron_id_to_new_IDX_map) = build_selected_spikes_df(self.track_templates, self.active_epochs_df, self.even_laps_epoch_selected_spikes_fragile_linear_neuron_IDX_dict, self.odd_laps_epoch_selected_spikes_fragile_linear_neuron_IDX_dict)
+        (_obj.plots_data.RL_selected_spike_df, _obj.plots_data.RL_neuron_id_to_new_IDX_map), (_obj.plots_data.LR_selected_spike_df, _obj.plots_data.LR_neuron_id_to_new_IDX_map) = build_selected_spikes_df(_obj.track_templates, _obj.active_epochs_df,
+                                                                                                                                             _obj.RL_active_epochs_selected_spikes_fragile_linear_neuron_IDX_dict,
+                                                                                                                                            _obj.LR_active_epochs_selected_spikes_fragile_linear_neuron_IDX_dict)
 
         ## Add the spikes
-        add_selected_spikes_df_points_to_scatter_plot(plots_data=odd_plots_data, plots=odd_plots, selected_spikes_df=deepcopy(odd_selected_spike_df), _active_plot_identifier = 'long_odd')
-        add_selected_spikes_df_points_to_scatter_plot(plots_data=odd_plots_data, plots=odd_plots, selected_spikes_df=deepcopy(odd_selected_spike_df), _active_plot_identifier = 'short_odd')
-        add_selected_spikes_df_points_to_scatter_plot(plots_data=even_plots_data, plots=even_plots, selected_spikes_df=deepcopy(even_selected_spike_df), _active_plot_identifier = 'long_even')
-        add_selected_spikes_df_points_to_scatter_plot(plots_data=even_plots_data, plots=even_plots, selected_spikes_df=deepcopy(even_selected_spike_df), _active_plot_identifier = 'short_even')
-
-
-
-
+        add_selected_spikes_df_points_to_scatter_plot(plots_data=_obj.plots_data.LR_plots_data, plots=_obj.plots.LR_plots, selected_spikes_df=deepcopy(_obj.plots_data.LR_selected_spike_df), _active_plot_identifier = 'long_LR')
+        add_selected_spikes_df_points_to_scatter_plot(plots_data=_obj.plots_data.LR_plots_data, plots=_obj.plots.LR_plots, selected_spikes_df=deepcopy(_obj.plots_data.LR_selected_spike_df), _active_plot_identifier = 'short_LR')
+        add_selected_spikes_df_points_to_scatter_plot(plots_data=_obj.plots_data.RL_plots_data, plots=_obj.plots.RL_plots, selected_spikes_df=deepcopy(_obj.plots_data.RL_selected_spike_df), _active_plot_identifier = 'long_RL')
+        add_selected_spikes_df_points_to_scatter_plot(plots_data=_obj.plots_data.RL_plots_data, plots=_obj.plots.RL_plots, selected_spikes_df=deepcopy(_obj.plots_data.RL_selected_spike_df), _active_plot_identifier = 'short_RL')
 
         return _obj
 
 
 
     def on_update_active_epoch(self, an_epoch_idx, an_epoch):
-        """ captures: odd_on_update_active_epoch, even_on_update_active_epoch """
-        odd_on_update_active_epoch(an_epoch_idx, an_epoch=an_epoch)
-        even_on_update_active_epoch(an_epoch_idx, an_epoch=an_epoch)
+        """ captures: LR_on_update_active_epoch, RL_on_update_active_epoch """
+        self.plots_data.LR_on_update_active_epoch(an_epoch_idx, an_epoch=an_epoch)
+        self.plots_data.RL_on_update_active_epoch(an_epoch_idx, an_epoch=an_epoch)
 
 
     def on_update_epoch_IDX(self, an_epoch_idx):
         """ captures on_update_active_epoch, active_epochs_df to extract the epoch time range and call `on_update_active_epoch` """
         # curr_epoch_spikes = spikes_df[(spikes_df.new_lap_IDX == an_epoch_idx)]
-        curr_epoch_df = active_epochs_df[(active_epochs_df.lap_id == (an_epoch_idx+1))]
+        curr_epoch_df = self.active_epochs_df[(self.active_epochs_df.lap_id == (an_epoch_idx+1))]
         curr_epoch = list(curr_epoch_df.itertuples())[0]
-
         self.on_update_active_epoch(an_epoch_idx, curr_epoch)
                     
 
