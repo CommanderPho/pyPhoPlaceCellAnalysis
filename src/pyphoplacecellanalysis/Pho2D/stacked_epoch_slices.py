4c73d5d2f2ab43c59efe77c69fd78913bb3f0fa1
Pho Hale <PhoHale@gmail.com>
2024-01-06 10:51:36 -0500

    added scrollable stuff to stacked_epoch_slices_matplotlib_build_insets_view

--- src/pyphoplacecellanalysis/Pho2D/stacked_epoch_slices.py
+++ src/pyphoplacecellanalysis/Pho2D/stacked_epoch_slices.py
@@ -429,7 +429,7 @@ def stacked_epoch_slices_matplotlib_build_view(epoch_slices, name='stacked_epoch
 @function_attributes(short_name=None, tags=['matplotlib', 'plot', 'figure', 'variant', 'helper'], input_requires=[], output_provides=[], uses=['stacked_epoch_basic_setup', 'MatplotlibTimeSynchronizedWidget'], used_by=[], creation_date='2023-05-30 10:06', related_items=[])
 def stacked_epoch_slices_matplotlib_build_insets_view(epoch_slices, name='stacked_epoch_slices_matplotlib_INSET_subplots_laps', plot_function_name=None, epoch_labels=None,
                                                         single_plot_fixed_height=100.0, debug_test_max_num_slices=12,
-                                                        size=(15,15), dpi=72, constrained_layout=True, should_use_MatplotlibTimeSynchronizedWidget=True, 
+                                                        size=(15,15), dpi=72, constrained_layout=True, scrollable_figure=True, should_use_MatplotlibTimeSynchronizedWidget=True,
                                                         debug_print=False, **kwargs):
     """ Builds a matplotlib figure view with empty subplots that can be plotted after the fact by iterating through plots.axs
         
@@ -483,7 +483,7 @@ def stacked_epoch_slices_matplotlib_build_insets_view(epoch_slices, name='stacke
         ui.mw = None
     else:
         ## MatplotlibTimeSynchronizedWidget-embedded Version:
-        ui.mw = MatplotlibTimeSynchronizedWidget(size=size, dpi=dpi, constrained_layout=constrained_layout, name=name, plot_function_name=plot_function_name, **kwargs) # , clear=True
+        ui.mw = MatplotlibTimeSynchronizedWidget(size=size, dpi=dpi, constrained_layout=constrained_layout, scrollable_figure=scrollable_figure, scrollAreaContents_MinimumHeight=params.all_plots_height, name=name, plot_function_name=plot_function_name, **kwargs) # , clear=True
         plots.fig = ui.mw.getFigure()
         plots.parent_ax = plots.fig.subplots(ncols=1, nrows=1, sharex=False, sharey=False) # , figsize=(15,15), clear=True, constrained_layout=True
 
