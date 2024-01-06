030d02a0f61546ed7d66dc65229a65f3d66ee676
Pho Hale <PhoHale@gmail.com>
2024-01-06 15:23:55 -0500

    extended batch_extended_computations

--- src/pyphoplacecellanalysis/General/Batch/NonInteractiveProcessing.py
+++ src/pyphoplacecellanalysis/General/Batch/NonInteractiveProcessing.py
@@ -275,7 +275,8 @@ def batch_load_session(global_data_root_parent_path, active_data_mode_name, base
 
 
 @function_attributes(short_name='batch_extended_computations', tags=['batch', 'automated', 'session', 'compute'], input_requires=[], output_provides=[], uses=[], used_by=[], creation_date='2023-03-28 04:46')
-def batch_extended_computations(curr_active_pipeline, include_includelist=None, included_computation_filter_names=None, include_global_functions=False, fail_on_exception=False, progress_print=True, debug_print=False, force_recompute:bool=False, force_recompute_override_computations_includelist=None, dry_run:bool=False):
+def batch_extended_computations(curr_active_pipeline, include_includelist=None, included_computation_filter_names=None, include_global_functions=False, fail_on_exception=False, progress_print=True, debug_print=False,
+                                force_recompute:bool=False, force_recompute_override_computations_includelist=None, dry_run:bool=False):
     """ performs the remaining required global computations
 
     force_recompute:bool=False
@@ -321,7 +322,6 @@ def batch_extended_computations(curr_active_pipeline, include_includelist=None, 
         included_computation_filter_names = [global_epoch_name] # use only the global epoch: e.g. ['maze']
         if progress_print:
             print(f'Running batch_extended_computations(...) with global_epoch_name: "{global_epoch_name}"')
-
     else:
         if progress_print:
             print(f'Running batch_extended_computations(...) with included_computation_filter_names: "{included_computation_filter_names}"')
@@ -346,15 +346,19 @@ def batch_extended_computations(curr_active_pipeline, include_includelist=None, 
                         if not dry_run:
                             newly_computed_values += _comp_specifier.try_computation_if_needed(curr_active_pipeline, computation_filter_name=a_computation_filter_name, on_already_computed_fn=_subfn_on_already_computed, fail_on_exception=fail_on_exception, progress_print=progress_print, debug_print=debug_print, force_recompute=force_recompute)
                         else:
-                            print(f'dry-run: {_comp_specifier.short_name}, computation_filter_name={a_computation_filter_name}')
+                            print(f'dry-run: {_comp_specifier.short_name}, computation_filter_name={a_computation_filter_name}, force_recompute={force_recompute}')
 
                 else:
                     # Global-Only:
+                    _curr_force_recompute = force_recompute or ((_comp_specifier.short_name in force_recompute_override_computations_includelist) or (_comp_specifier.computation_fn_name in force_recompute_override_computations_includelist)) # force_recompute for this specific result if either of its name is included in `force_recompute_override_computations_includelist`
                     if not dry_run:
-                        _curr_force_recompute = force_recompute or ((_comp_specifier.short_name in force_recompute_override_computations_includelist) or (_comp_specifier.computation_fn_name in force_recompute_override_computations_includelist)) # force_recompute for this specific result if either of its name is included in `force_recompute_override_computations_includelist`
                         newly_computed_values += _comp_specifier.try_computation_if_needed(curr_active_pipeline, computation_filter_name=global_epoch_name, on_already_computed_fn=_subfn_on_already_computed, fail_on_exception=fail_on_exception, progress_print=progress_print, debug_print=debug_print, force_recompute=_curr_force_recompute)
                     else:
-                        print(f'dry-run: {_comp_specifier.short_name}')
+                        print(f'dry-run: {_comp_specifier.short_name}, force_recompute={force_recompute}, curr_force_recompute={_curr_force_recompute}')
+                        # Check for existing result:
+                        is_known_missing_provided_keys: bool = _comp_specifier.try_check_missing_provided_keys(curr_active_pipeline)
+                        if is_known_missing_provided_keys:
+                            print(f'{_comp_specifier.short_name} -- is_known_missing_provided_keys = True!')
 
                 if (_comp_specifier.short_name in include_includelist):
                     del remaining_include_function_names[_comp_specifier.short_name]
