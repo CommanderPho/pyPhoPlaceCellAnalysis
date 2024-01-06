1ea5ed819aa57d0b8c86bc5354e1abee3b57de8d
Pho Hale <PhoHale@gmail.com>
2024-01-06 03:00:05 -0500

    fixed window title order

--- src/pyphoplacecellanalysis/Pho2D/matplotlib/CustomMatplotlibWidget.py
+++ src/pyphoplacecellanalysis/Pho2D/matplotlib/CustomMatplotlibWidget.py
@@ -49,7 +49,7 @@ class CustomMatplotlibWidget(QtWidgets.QWidget):
         self.ui.connections = PhoUIContainer(name=name)
 
         self.params.name = name
-        self.params.window_title = f"{plot_function_name} - {name}" # kwargs.pop('plot_function_name', name)
+        self.params.window_title = f" - ".join([name, plot_function_name]) # name should be first so window title is rendered reasonably. kwargs.pop('plot_function_name', name)
         self.params.disable_toolbar = disable_toolbar
         self.params.scrollable_figure = scrollable_figure
         self.params.scrollAreaContents_MinimumHeight = kwargs.pop('scrollAreaContents_MinimumHeight', None)
