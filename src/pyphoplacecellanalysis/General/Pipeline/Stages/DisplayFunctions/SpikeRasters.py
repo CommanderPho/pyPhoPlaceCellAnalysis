65abfe007a6c8062ea07d7c9076368f96e9ec25d
Pho Hale <PhoHale@gmail.com>
2024-01-17 01:33:39 -0500

    fixed issue with output_references not being returned from `_build_additional_window_menus`

--- src/pyphoplacecellanalysis/General/Pipeline/Stages/DisplayFunctions/SpikeRasters.py
+++ src/pyphoplacecellanalysis/General/Pipeline/Stages/DisplayFunctions/SpikeRasters.py
@@ -1483,66 +1483,67 @@ def _build_additional_spikeRaster2D_menus(spike_raster_plt_2d, owning_pipeline_r
 
 @function_attributes(short_name=None, tags=['menu', 'spike_raster', 'gui'], input_requires=[], output_provides=[], uses=['_build_additional_spikeRaster2D_menus'], used_by=['_display_spike_rasters_window'], creation_date='2023-11-09 19:32', related_items=[])
 def _build_additional_window_menus(spike_raster_window, owning_pipeline_reference, computation_result, active_display_fn_identifying_ctx):
-        assert owning_pipeline_reference is not None
-        active_config_name: str = _recover_filter_config_name_from_display_context(owning_pipeline_reference, active_display_fn_identifying_ctx) # recover active_config_name from the context
+    assert owning_pipeline_reference is not None
+    active_config_name: str = _recover_filter_config_name_from_display_context(owning_pipeline_reference, active_display_fn_identifying_ctx) # recover active_config_name from the context
 
-        ## SpikeRaster2D Specific Items:
-        output_references = _build_additional_spikeRaster2D_menus(spike_raster_window.spike_raster_plt_2d, owning_pipeline_reference, computation_result, active_display_fn_identifying_ctx)
+    ## SpikeRaster2D Specific Items:
+    output_references = _build_additional_spikeRaster2D_menus(spike_raster_window.spike_raster_plt_2d, owning_pipeline_reference, computation_result, active_display_fn_identifying_ctx)
 
-        ## Note that curr_main_menu_window is usually not the same as spike_raster_window, instead curr_main_menu_window wraps it and produces the final output window
-        curr_main_menu_window, menuConnections, connections_actions_dict = ConnectionControlsMenuMixin.try_add_connections_menu(spike_raster_window)
+    ## Note that curr_main_menu_window is usually not the same as spike_raster_window, instead curr_main_menu_window wraps it and produces the final output window
+    curr_main_menu_window, menuConnections, connections_actions_dict = ConnectionControlsMenuMixin.try_add_connections_menu(spike_raster_window)
+    spike_raster_window.main_menu_window = curr_main_menu_window # to retain the changes
+
+    if owning_pipeline_reference is not None:
+        if active_display_fn_identifying_ctx not in owning_pipeline_reference.display_output:
+            owning_pipeline_reference.display_output[active_display_fn_identifying_ctx] = PhoUIContainer() # create a new context
+        
+        display_output = owning_pipeline_reference.display_output[active_display_fn_identifying_ctx]
+        # print(f'display_output: {display_output}')
+        curr_main_menu_window, menuCreateNewConnectedWidget, createNewConnected_actions_dict = CreateNewConnectedWidgetMenuHelper.try_add_create_new_connected_widget_menu(spike_raster_window, owning_pipeline_reference, active_config_name, display_output) 
         spike_raster_window.main_menu_window = curr_main_menu_window # to retain the changes
         
+    else:
+        print(f'ERROR: _build_additional_window_menus(...) has no owning_pipeline_reference in its parameters, so it cannot add the CreateNewConnectedWidgetMenuMixin menus.')
+        menuCreateNewConnectedWidget = None
+        createNewConnected_actions_dict = None
+        
+    # Debug Menu
+    _debug_menu_provider = DebugMenuProviderMixin(render_widget=spike_raster_window)
+    spike_raster_window.main_menu_window.ui.menus.global_window_menus.debug.menu_provider_obj = _debug_menu_provider
+
+    # Docked Menu
+    _docked_menu_provider = DockedWidgets_MenuProvider(render_widget=spike_raster_window)
+    _docked_menu_provider.DockedWidgets_MenuProvider_on_buildUI(spike_raster_window=spike_raster_window, owning_pipeline_reference=owning_pipeline_reference, context=active_display_fn_identifying_ctx, active_config_name=active_config_name, display_output=owning_pipeline_reference.display_output[active_display_fn_identifying_ctx])
+    spike_raster_window.main_menu_window.ui.menus.global_window_menus.docked_widgets.menu_provider_obj = _docked_menu_provider
+
+    # Create Linked Widget Menu
+    ## Adds the custom renderable menu to the top-level menu of the plots in Spike2DRaster
+    active_pf_2D_dt = computation_result.computed_data.get('pf2D_dt', None)
+    if active_pf_2D_dt is not None:
+        active_pf_2D_dt.reset()
+        active_pf_2D_dt.update(t=45.0, start_relative_t=True)
+
+        # _createLinkedWidget_menus = LocalMenus_AddRenderable.add_Create_Paired_Widget_menu(spike_raster_window, active_pf_2D_dt)  # Adds the custom context menus for SpikeRaster2D
+        _createLinkedWidget_menu_provider = CreateLinkedWidget_MenuProvider(render_widget=spike_raster_window)
         if owning_pipeline_reference is not None:
             if active_display_fn_identifying_ctx not in owning_pipeline_reference.display_output:
                 owning_pipeline_reference.display_output[active_display_fn_identifying_ctx] = PhoUIContainer() # create a new context
-            
+        
             display_output = owning_pipeline_reference.display_output[active_display_fn_identifying_ctx]
-            # print(f'display_output: {display_output}')
-            curr_main_menu_window, menuCreateNewConnectedWidget, createNewConnected_actions_dict = CreateNewConnectedWidgetMenuHelper.try_add_create_new_connected_widget_menu(spike_raster_window, owning_pipeline_reference, active_config_name, display_output) 
-            spike_raster_window.main_menu_window = curr_main_menu_window # to retain the changes
-            
+            _createLinkedWidget_menu_provider.CreateLinkedWidget_MenuProvider_on_buildUI(spike_raster_window=spike_raster_window, active_pf_2D_dt=active_pf_2D_dt, context=active_display_fn_identifying_ctx, display_output=display_output)
         else:
-            print(f'WARNING: _display_spike_rasters_window(...) has no owning_pipeline_reference in its parameters, so it cannot add the CreateNewConnectedWidgetMenuMixin menus.')
-            menuCreateNewConnectedWidget = None
-            createNewConnected_actions_dict = None
+            print(f'WARNING: owning_pipeline_reference is NONE in  _display_spike_rasters_window!')   
             
-        # Debug Menu
-        _debug_menu_provider = DebugMenuProviderMixin(render_widget=spike_raster_window)
-        spike_raster_window.main_menu_window.ui.menus.global_window_menus.debug.menu_provider_obj = _debug_menu_provider
-        
-        # Docked Menu
-        _docked_menu_provider = DockedWidgets_MenuProvider(render_widget=spike_raster_window)
-        _docked_menu_provider.DockedWidgets_MenuProvider_on_buildUI(spike_raster_window=spike_raster_window, owning_pipeline_reference=owning_pipeline_reference, context=active_display_fn_identifying_ctx, active_config_name=active_config_name, display_output=owning_pipeline_reference.display_output[active_display_fn_identifying_ctx])
-        spike_raster_window.main_menu_window.ui.menus.global_window_menus.docked_widgets.menu_provider_obj = _docked_menu_provider
-        
-        # Create Linked Widget Menu
-        ## Adds the custom renderable menu to the top-level menu of the plots in Spike2DRaster
-        active_pf_2D_dt = computation_result.computed_data.get('pf2D_dt', None)
-        if active_pf_2D_dt is not None:
-            active_pf_2D_dt.reset()
-            active_pf_2D_dt.update(t=45.0, start_relative_t=True)
+        spike_raster_window.main_menu_window.ui.menus.global_window_menus.create_linked_widget.menu_provider_obj = _createLinkedWidget_menu_provider # KeyError: 'create_linked_widget' was occuring before, so I moved it into the condition where dts were had and the menus were created 
+    else:
+        print(f'active_pf_2D_dt is None! Skipping Create Paired Widget Menu...')
+        _createLinkedWidget_menu_provider = None
 
-            # _createLinkedWidget_menus = LocalMenus_AddRenderable.add_Create_Paired_Widget_menu(spike_raster_window, active_pf_2D_dt)  # Adds the custom context menus for SpikeRaster2D
-            _createLinkedWidget_menu_provider = CreateLinkedWidget_MenuProvider(render_widget=spike_raster_window)
-            if owning_pipeline_reference is not None:
-                if active_display_fn_identifying_ctx not in owning_pipeline_reference.display_output:
-                    owning_pipeline_reference.display_output[active_display_fn_identifying_ctx] = PhoUIContainer() # create a new context
-            
-                display_output = owning_pipeline_reference.display_output[active_display_fn_identifying_ctx]
-                _createLinkedWidget_menu_provider.CreateLinkedWidget_MenuProvider_on_buildUI(spike_raster_window=spike_raster_window, active_pf_2D_dt=active_pf_2D_dt, context=active_display_fn_identifying_ctx, display_output=display_output)
-            else:
-                print(f'WARNING: owning_pipeline_reference is NONE in  _display_spike_rasters_window!')   
-        else:
-            print(f'active_pf_2D_dt is None! Skipping Create Paired Widget Menu...')
-            _createLinkedWidget_menu_provider = None
     
-        spike_raster_window.main_menu_window.ui.menus.global_window_menus.create_linked_widget.menu_provider_obj = _createLinkedWidget_menu_provider
-
-        output_references = output_references.extend([curr_main_menu_window, menuConnections, connections_actions_dict,
-            curr_main_menu_window, menuCreateNewConnectedWidget, createNewConnected_actions_dict,
-            _debug_menu_provider,
-            _docked_menu_provider,
-            _createLinkedWidget_menu_provider
-        ])
-        return output_references
+    output_references.extend([curr_main_menu_window, menuConnections, connections_actions_dict, # note .extend(...) function works in-place and does not return a result, which is why this function was returning None originally.
+        curr_main_menu_window, menuCreateNewConnectedWidget, createNewConnected_actions_dict,
+        _debug_menu_provider,
+        _docked_menu_provider,
+        _createLinkedWidget_menu_provider
+    ])
+    return output_references
