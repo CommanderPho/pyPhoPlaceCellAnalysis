bd99b23a5cc296f09602ffaaae6a21879ac70b49
Pho Hale <PhoHale@gmail.com>
2024-03-28 06:20:28 -0400

    docs improvements

--- src/pyphoplacecellanalysis/Pho2D/PyQtPlots/TimeSynchronizedPlotters/Mixins/helpers.py
+++ src/pyphoplacecellanalysis/Pho2D/PyQtPlots/TimeSynchronizedPlotters/Mixins/helpers.py
@@ -145,8 +145,6 @@ def build_connected_time_synchronized_placefields_plotter(active_pf_2D_dt, sync_
         
     return curr_placefields_plotter, placefields_raster_window_sync_connection
 
-        
-        
 def build_connected_time_synchronized_decoder_plotter(active_pf_2D_dt, sync_driver=None, should_defer_show=False):
     """ 
     sync_driver: spike_raster_window, 2DRaster, etc
