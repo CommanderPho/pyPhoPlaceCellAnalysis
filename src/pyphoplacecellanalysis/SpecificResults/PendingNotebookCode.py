acc4671ae5da42a1b7afebf907b0568e8e79372d
Pho Hale <PhoHale@gmail.com>
2024-06-04 06:42:52 -0400

    minor

--- src/pyphoplacecellanalysis/SpecificResults/PendingNotebookCode.py
+++ src/pyphoplacecellanalysis/SpecificResults/PendingNotebookCode.py
@@ -586,9 +586,9 @@ def _perform_filter_replay_epochs(curr_active_pipeline, global_epoch_name, track
     # df = _add_maze_id_to_epochs(df, t_delta)
     df["time_bin_size"] = ripple_decoding_time_bin_size
     df['is_user_annotated_epoch'] = True # if it's filtered here, it's true
-
-
     return filtered_epochs_df, filtered_decoder_filter_epochs_decoder_result_dict, filtered_ripple_all_epoch_bins_marginals_df
+
+
 
 @function_attributes(short_name=None, tags=['filter', 'epoch_selection', 'export', 'h5'], input_requires=[], output_provides=[], uses=[], used_by=[], creation_date='2024-03-08 13:28', related_items=[])
 def export_numpy_testing_filtered_epochs(curr_active_pipeline, global_epoch_name, track_templates, required_min_percentage_of_active_cells: float = 0.333333, epoch_id_key_name='ripple_epoch_id', no_interval_fill_value=-1):
