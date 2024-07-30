1095b445e879dbf64e69317a34725def74e4b21b
Pho Hale <PhoHale@gmail.com>
2024-07-30 16:24:29 -0400

    Position Derivatives

--- src/pyphoplacecellanalysis/GUI/Silx/EpochHeuristicPosteriorDebuggerWidget.py
+++ src/pyphoplacecellanalysis/GUI/Silx/EpochHeuristicPosteriorDebuggerWidget.py
@@ -19,6 +19,8 @@ from typing_extensions import TypeAlias
 from nptyping import NDArray
 import neuropy.utils.type_aliases as types
 
+from pyphocorehelpers.function_helpers import function_attributes
+
 from pyphoplacecellanalysis.Analysis.Decoder.reconstruction import SingleEpochDecodedResult
 from pyphoplacecellanalysis.Analysis.Decoder.reconstruction import DecodedFilterEpochsResult
 from pyphoplacecellanalysis.General.Pipeline.Stages.ComputationFunctions.MultiContextComputationFunctions.DirectionalPlacefieldGlobalComputationFunctions import DecoderDecodedEpochsResult
@@ -26,6 +28,54 @@ from pyphoplacecellanalysis.General.Pipeline.Stages.ComputationFunctions.MultiCo
 from pyphoplacecellanalysis.Analysis.Decoder.heuristic_replay_scoring import HeuristicReplayScoring, compute_local_peak_probabilities, get_peaks_mask, expand_peaks_mask, InversionCount, is_valid_sequence_index, _compute_sequences_spanning_ignored_intrusions
 from pyphoplacecellanalysis.Analysis.Decoder.heuristic_replay_scoring import _compute_diffusion_value, HeuristicScoresTuple
 
+
+@define(slots=False)
+class PositionDerivativesContainer:
+    """
+    Holds 
+    
+    from pyphoplacecellanalysis.GUI.Silx.EpochHeuristicPosteriorDebuggerWidget import PositionDerivativesContainer
+    
+    
+    """
+    pos: NDArray = field()
+    mass: float = field(default=1.0)
+    
+    ## Computable:
+    _curve_pos_t: NDArray = field(default=None)
+    vel: NDArray = field(default=None)
+    _curve_vel_t: NDArray = field(default=None)
+    accel: NDArray = field(default=None)
+    _curve_accel_t: NDArray = field(default=None)
+    kinetic_energy: NDArray = field(default=None)
+    total_energy: float = field(default=None)
+
+
+    def __attrs_post_init__(self):
+        # Recompute all:
+        self.compute()
+        
+
+    def compute(self):
+        """ called to recompute all computed properties after updating self.pos
+        """
+        self._curve_pos_t = np.arange(len(self.pos)) + 0.5  # Move forward by a half bin
+
+
+        # Compute velocity
+        self.vel = np.diff(self.pos)
+        self._curve_vel_t = self._curve_pos_t[:-1] + 0.5  # Center between the original position x-values
+
+        # Compute acceleration
+        self.accel = np.diff(self.vel)
+        self._curve_accel_t = self._curve_vel_t[:-1] + 0.5  # Center between the velocity x-values
+
+        # Compute kinetic energy at each time step
+        self.kinetic_energy: NDArray = 0.5 * self.mass * self.vel**2
+
+        # Total energy needed to move the particle along the trajectory
+        self.total_energy: float = np.sum(self.kinetic_energy)
+        
 
 ## Uses: xbin, t_start, pos_bin_size, time_bin_size
 
@@ -61,6 +111,9 @@ class EpochHeuristicDebugger:
     time_bin_size: float = field(default=None)
     time_bin_centers: NDArray = field(default=None)
 
+    position_derivatives: PositionDerivativesContainer = field(default=None)
+    
+
     ## Widgets/Plots:
     main_widget: qt.QWidget = field(default=None)
     main_layout: qt.QVBoxLayout = field(default=None)
@@ -75,13 +128,60 @@ class EpochHeuristicDebugger:
     plot_extra: Plot1D = field(factory=Plot1D)
 
 
-
     # most_likely_position_indicies
     @property
     def active_most_likely_position_indicies(self) -> NDArray:
         """The most_likely_position_indicies property."""
         assert len(self.active_single_epoch_result.most_likely_position_indicies) == 1, f" the [0] is to handle the fact that for some reason the list is double-wrapped: [[37  0 28 52 56 28 55]]"
         return self.active_single_epoch_result.most_likely_position_indicies[0] # the [0] is to handle the fact that for some reason the list is double-wrapped: [[37  0 28 52 56 28 55]]
+
+
+    @property
+    def position_plots_dict(self) -> Dict[str, Plot1D]:
+        """ convenince access to the dict of position plots 
+        """
+        position_plots_list = [self.plot_position, self.plot_velocity, self.plot_acceleration, self.plot_extra]
+        return dict(zip(['Position', 'Velocity', 'Acceleration', 'Extra'], position_plots_list))
+
+
+
+
+    @classmethod
+    def init_from_epoch_idx(cls, a_decoder_decoded_epochs_result: DecodedFilterEpochsResult, active_epoch_idx: int=0, **kwargs) -> "EpochHeuristicDebugger":
+        """ initializes to a specific epoch_idx
+        
+        """        
+
+        _obj = cls(active_decoder_decoded_epochs_result=deepcopy(a_decoder_decoded_epochs_result), **kwargs)
+        
+        # time_bin_size: float = a_decoder_decoded_epochs_result.decoding_time_bin_size
+        
+
+        # time_bin_centers = deepcopy(active_captured_single_epoch_result.time_bin_container.centers)
+        # t_start, t_end = active_captured_single_epoch_result.time_bin_edges[0], active_captured_single_epoch_result.time_bin_edges[-1]
+
+        # p_x_given_n = deepcopy(active_captured_single_epoch_result.p_x_given_n)
+        # most_likely_positions = deepcopy(active_captured_single_epoch_result.most_likely_positions)
+        # most_likely_positionIndicies = deepcopy(active_captured_single_epoch_result.most_likely_position_indicies)
+        
+
+        if _obj.active_single_epoch_result is None:
+            active_captured_single_epoch_result: SingleEpochDecodedResult = a_decoder_decoded_epochs_result.get_result_for_epoch(active_epoch_idx=active_epoch_idx)
+            _obj.active_single_epoch_result = deepcopy(active_captured_single_epoch_result)
+
+        if _obj.time_bin_size is None:
+             _obj.time_bin_size = a_decoder_decoded_epochs_result.decoding_time_bin_size
+             
+        if _obj.time_bin_centers is None:
+             _obj.time_bin_centers = deepcopy(_obj.active_single_epoch_result.time_bin_container.centers)
+
+                                    
+        _obj.update_active_epoch_data(active_epoch_idx=active_epoch_idx)
+        _obj.build_ui()
+        _obj.update_active_epoch(active_epoch_idx=active_epoch_idx)
+        return _obj
+        
+
 
 
     def build_ui(self):
@@ -100,21 +200,44 @@ class EpochHeuristicDebugger:
         print(f'img_scale: {img_scale}')
 
         # label_kwargs = dict(xlabel='t (sec)', ylabel='x (cm)')
-        label_kwargs = dict(xlabel='t (bin)', ylabel='x (bin)')
+        label_kwargs = dict(xlabel='t (tbin)', ylabel='x_pos (bin)')
         self.plot.addImage(self.p_x_given_n_masked, legend='p_x_given_n', replace=True, colormap=self.a_cmap, origin=img_origin, scale=img_scale, **label_kwargs, resetzoom=True) # , colormap="viridis", vmin=0, vmax=1
         prev_img: ImageBase = self.plot.getImage('p_x_given_n')
 
-
         empty_arr = np.array([], dtype='int64')
-        
-        # Add data to the plots
-        self.plot_position.addCurve(empty_arr, empty_arr, legend="Position", xlabel='t (tbin)', ylabel='x (bin)', replace=True)
-        
 
-        
-        self.plot_velocity.addCurve(empty_arr, empty_arr, legend="Velocity", xlabel='t (tbin)', ylabel='velocity (bin/tbin)', replace=True)
-        self.plot_acceleration.addCurve(empty_arr, empty_arr, legend="Acceleration", xlabel='t (tbin)', ylabel='accel. (bin/tbin^2)', replace=True)
-        self.plot_extra.addCurve(empty_arr, empty_arr, legend="Extra", xlabel='t (tbin)', ylabel='Extra', replace=True)
+        common_plot_config_dict = dict(symbol='o', linestyle=':', color='blue')
+
+        plot_configs_dict = {"Position": dict(legend="Position", xlabel='t (tbin)', ylabel='x_pos (bin)', **common_plot_config_dict),
+            "Velocity": dict(legend="Velocity", xlabel='t (tbin)', ylabel='velocity (bin/tbin)', **common_plot_config_dict),
+            "Acceleration": dict(legend="Acceleration", xlabel='t (tbin)', ylabel='accel. (bin/tbin^2)', **common_plot_config_dict),
+            "Extra": dict(legend="Extra", xlabel='t (tbin)', ylabel='Extra', **common_plot_config_dict),
+        }
+
+        position_plots_list = [self.plot_position, self.plot_velocity, self.plot_acceleration, self.plot_extra]
+        position_plots_dict = dict(zip(list(plot_configs_dict.keys()), position_plots_list))
+
+        # Add data to the plots
+        # self.plot_position.addCurve(empty_arr, empty_arr, legend="Position", xlabel='t (tbin)', ylabel='x (bin)', replace=True)
+        # self.plot_velocity.addCurve(empty_arr, empty_arr, legend="Velocity", xlabel='t (tbin)', ylabel='velocity (bin/tbin)', replace=True)
+        # self.plot_acceleration.addCurve(empty_arr, empty_arr, legend="Acceleration", xlabel='t (tbin)', ylabel='accel. (bin/tbin^2)', replace=True)
+        # self.plot_extra.addCurve(empty_arr, empty_arr, legend="Extra", xlabel='t (tbin)', ylabel='Extra', replace=True)
+
+        for a_plot_name, a_plot in position_plots_dict.items():
+            a_plot_config_dict = plot_configs_dict[a_plot_name]
+            ## add curves
+            a_plot.addCurve(empty_arr, empty_arr, **a_plot_config_dict, replace=True)
+            
+            ## Update plot properties:
+            a_plot.setActiveCurve(a_plot_name)
+            a_plot.setGraphGrid(which=True) # good
+            a_plot.getXAxis().setLabel(a_plot_config_dict["xlabel"])
+            a_plot.getYAxis().setLabel(a_plot_config_dict["ylabel"])
+            a_plot.setXAxisAutoScale(flag=False)
+            if a_plot_name == 'Position':
+                a_plot.setYAxisAutoScale(flag=False) # position y-axis is fixed to the total bins
+            else:
+                a_plot.setYAxisAutoScale(flag=True)
 
         # Create a main widget and set a vertical layout
         self.main_widget = qt.QWidget()
@@ -134,19 +257,17 @@ class EpochHeuristicDebugger:
         
 
     # def recompute(self):
-    
-    def update_active_epoch(self, active_epoch_idx: int):
-        """ called after the time-bin is updated.
+    @function_attributes(short_name=None, tags=['update'], input_requires=[], output_provides=[], uses=[], used_by=['update_active_epoch'], creation_date='2024-07-30 15:08', related_items=[])
+    def update_active_epoch_data(self, active_epoch_idx: int):
+        """ Data Update only - called after the time-bin is updated.
         
         TODO: this could be greatly optimized.
-        
-        requires: self.active_decoder_decoded_epochs_result
-        
+
+        Updates: self.p_x_given_n_masked, self.heuristic_scores
         """
-        print(f'update_active_epoch(active_epoch_idx={active_epoch_idx})')
+        print(f'update_active_epoch_data(active_epoch_idx={active_epoch_idx})')
         assert self.active_decoder_decoded_epochs_result is not None
-        
-        # Data Update ________________________________________________________________________________________________________ #
+        # Data Update Only ________________________________________________________________________________________________________ #
         # active_captured_single_epoch_result = a_decoder_decoded_epochs_result.get_result_for_epoch(active_epoch_idx=active_epoch_idx) 
         # self.p_x_given_n_masked = _get_epoch_posterior(active_epoch_idx=active_epoch_idx)
 
@@ -170,7 +291,6 @@ class EpochHeuristicDebugger:
 
         self.p_x_given_n_masked = ma.masked_array(p_x_given_n, mask=np.logical_not(is_higher_than_diffusion), fill_value=np.nan)
         
-
         self.heuristic_scores = HeuristicReplayScoring.compute_pho_heuristic_replay_scores(a_result=self.active_decoder_decoded_epochs_result, an_epoch_idx=self.active_single_epoch_result.epoch_data_index, debug_print=False)
         # longest_sequence_length, longest_sequence_length_ratio, direction_change_bin_ratio, congruent_dir_bins_ratio, total_congruent_direction_change, total_variation, integral_second_derivative, stddev_of_diff, position_derivatives_df = self.heuristic_scores
         # np.diff(active_captured_single_epoch_result.most_likely_position_indicies)
@@ -178,6 +298,25 @@ class EpochHeuristicDebugger:
             # print(f'heuristic_scores: {astuple(self.heuristic_scores)[:-1]}')
             print(f"heuristic_scores: {asdict(self.heuristic_scores, filter=(lambda an_attr, attr_value: an_attr.name not in ['position_derivatives_df']))}")
 
+        
+        # Update position data:
+        self.position_derivatives = PositionDerivativesContainer(pos=deepcopy(self.active_most_likely_position_indicies))
+        
+
+
+    @function_attributes(short_name=None, tags=['update'], input_requires=[], output_provides=[], uses=['update_active_epoch_data'], used_by=['on_slider_change'], creation_date='2024-07-30 15:09', related_items=[])
+    def update_active_epoch(self, active_epoch_idx: int):
+        """ called after the time-bin is updated.
+        
+        requires: self.active_decoder_decoded_epochs_result
+        
+        """
+        print(f'update_active_epoch(active_epoch_idx={active_epoch_idx})')
+        assert self.active_decoder_decoded_epochs_result is not None
+        
+        # Data Update ________________________________________________________________________________________________________ #
+        self.update_active_epoch_data(active_epoch_idx=active_epoch_idx)
+        
         # Plottings __________________________________________________________________________________________________________ #
         prev_img: ImageBase = self.plot.getImage('p_x_given_n')
         prev_img.setData(self.p_x_given_n_masked)
@@ -195,33 +334,57 @@ class EpochHeuristicDebugger:
         _max_path_Curve = self.plot.addCurve(x=_curve_x, y=max_path, color='r', symbol='s', legend='max_path', replace=True, yerror=effectively_same_location_num_bins)
         # _max_path_Curve
         
-
         ## Update position plots:
-        _curve_t = np.arange(len(self.active_most_likely_position_indicies)) # + 0.5 # move forward by a half bin
+        # _curve_pos_t = np.arange(len(self.active_most_likely_position_indicies)) + 0.5 # move forward by a half bin
+        # pos = deepcopy(self.active_most_likely_position_indicies)
+        # _curve_vel_t = _curve_pos_t[1:] # + 0.25 # move forward by a half bin
+        # vel = np.diff(pos)
+        # _curve_accel_t = _curve_pos_t[2:] # + 0.125 # move forward by a half bin
+        # accel = np.diff(vel)
+        
+        # Update position plots
         pos = deepcopy(self.active_most_likely_position_indicies)
-        
-        _curve_vel_t = _curve_t[1:] + 0.5 # move forward by a half bin
+        _curve_pos_t = np.arange(len(pos)) + 0.5  # Move forward by a half bin
+
+        # Compute velocity
         vel = np.diff(pos)
-        _curve_accel_t = _curve_t[2:] + 0.5 # move forward by a half bin
-        
+        _curve_vel_t = _curve_pos_t[:-1] + 0.5  # Center between the original position x-values
+
+        # Compute acceleration
         accel = np.diff(vel)
+        _curve_accel_t = _curve_vel_t[:-1] + 0.5  # Center between the velocity x-values
+
         
         if self.debug_print:
-            print(f'_curve_t: {_curve_t}')
+            print(f'_curve_t: {_curve_pos_t}')
             print(f'pos: {pos}')
             print(f'vel: {vel}')
             print(f'accel: {accel}')
         
-        # self.plot_position.getCurve("Position").setData(_curve_t, pos, xlabel='t (tbin)', ylabel='x (bin)')
-        # self.plot_velocity.getCurve("Velocity").setData(_curve_vel_t, vel, xlabel='t (tbin)', ylabel='velocity (bin/tbin)')
-        # self.plot_acceleration.getCurve("Acceleration").setData(_curve_accel_t, accel, xlabel='t (tbin)', ylabel='accel. (bin/tbin^2)')
-        # self.plot_extra.getCurve("Extra").setData(_curve_accel_t, accel)
+        self.plot_position.getCurve("Position").setData(_curve_pos_t, pos)
+        self.plot_velocity.getCurve("Velocity").setData(_curve_vel_t, vel)
+        self.plot_acceleration.getCurve("Acceleration").setData(_curve_accel_t, accel)
+        self.plot_extra.getCurve("Extra").setData(_curve_accel_t, accel)
 
-        self.plot_position.addCurve(_curve_t, pos, legend="Position", xlabel='t (tbin)', ylabel='x (bin)', replace=True)
-        self.plot_velocity.addCurve(_curve_vel_t, vel, legend="Velocity", xlabel='t (tbin)', ylabel='velocity (bin/tbin)', replace=True)
-        self.plot_acceleration.addCurve(_curve_accel_t, accel, legend="Acceleration", xlabel='t (tbin)', ylabel='accel. (bin/tbin^2)', replace=True)
-        self.plot_extra.addCurve(_curve_accel_t, accel, legend="Extra", xlabel='t (tbin)', ylabel='Extra', replace=True)
+        # self.plot_position.addCurve(_curve_pos_t, pos, legend="Position", xlabel='t (tbin)', ylabel='x (bin)', replace=True)
+        # self.plot_velocity.addCurve(_curve_vel_t, vel, legend="Velocity", xlabel='t (tbin)', ylabel='velocity (bin/tbin)', replace=True)
+        # self.plot_acceleration.addCurve(_curve_accel_t, accel, legend="Acceleration", xlabel='t (tbin)', ylabel='accel. (bin/tbin^2)', replace=True)
+        # self.plot_extra.addCurve(_curve_accel_t, accel, legend="Extra", xlabel='t (tbin)', ylabel='Extra', replace=True)
                 
+        src_plot = self.plot # main plot (Plot2D) is the source plot
+        x_range = src_plot.getXAxis().getLimits()
+        pos_y_range = src_plot.getYAxis().getLimits()
+        
+        for a_plot_name, a_plot in self.position_plots_dict.items():
+            # a_plot.addCurve(empty_arr, empty_arr, **a_plot_config_dict, replace=True)
+            
+            ## Update plot properties:
+            a_plot.getXAxis().setLimits(*x_range)
+            if a_plot_name == 'Position':
+                a_plot.getYAxis().setLimits(*pos_y_range)
+            else:
+                a_plot.resetZoom() # reset zoom resets the y-axis only
+
 
 
     def on_slider_change(self, change):
