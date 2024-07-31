f352175d35e0ee7371777ef4e6b6cd3a385fb053
Pho Hale <PhoHale@gmail.com>
2024-07-31 14:58:54 -0400

    build_programmatic_buttons, ProgrammaticButtonConfig

--- src/pyphoplacecellanalysis/GUI/Qt/Widgets/ThinButtonBar/ThinButtonBarWidget.py
+++ src/pyphoplacecellanalysis/GUI/Qt/Widgets/ThinButtonBar/ThinButtonBarWidget.py
@@ -2,6 +2,10 @@
 # Generated from c:\Users\pho\repos\Spike3DWorkEnv\pyPhoPlaceCellAnalysis\src\pyphoplacecellanalysis\GUI\Qt\Widgets\ThinButtonBar\ThinButtonBarWidget.ui automatically by PhoPyQtClassGenerator VSCode Extension
 import sys
 import os
+from typing import Dict, List, Tuple, Optional, Callable, Union, Any
+from nptyping import NDArray
+from types import MethodType
+from attrs import asdict, astuple, define, field, Factory
 
 from PyQt5 import QtCore, QtGui, QtWidgets, uic
 from PyQt5.QtWidgets import QMessageBox, QToolTip, QStackedWidget, QHBoxLayout, QVBoxLayout, QSplitter, QFormLayout, QLabel, QFrame, QPushButton, QTableWidget, QTableWidgetItem
@@ -26,6 +30,118 @@ uiFile = os.path.join(path, 'ThinButtonBarWidget.ui')
 # from pyphoplacecellanalysis.GUI.Qt.Widgets.ThinButtonBar.Uic_AUTOGEN_ExtraButtonsWidget import Ui_ExtraButtonsWidget as Ui_Form
 from pyphoplacecellanalysis.GUI.Qt.Widgets.ThinButtonBar.ExtraButtonsWidget import add_buttons_to_existing_form
 
+# ==================================================================================================================== #
+# Programmatic Button Helpers                                                                                          #
+# ==================================================================================================================== #
+
+@function_attributes(short_name=None, tags=['button', 'ui', 'programmatic', 'toolbar'], input_requires=[], output_provides=[], uses=[], used_by=[], creation_date='2024-07-31 09:32', related_items=[])
+def build_button(icon_path: str=':/png/gui/icons/crosshair.png', name: str="Refresh", parent=None) -> QtWidgets.QToolButton:
+    """ builds a simple toolbar tool button programmatically from the icon_path and the name. """
+    if parent is not None:
+        new_btn = QtWidgets.QToolButton(parent)
+    else:
+        new_btn = QtWidgets.QToolButton()
+        
+    new_btn.setToolTip(name)
+    new_btn.setStatusTip(name)
+    new_btn.setText(name)
+    icon = QtGui.QIcon()
+    icon.addPixmap(QtGui.QPixmap(icon_path), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+    new_btn.setIcon(icon)
+    new_btn.setAutoRaise(True)
+    new_btn.setObjectName(f"toolButton_{name}")
+    return new_btn
+
+
+@define(slots=True, eq=False)
+class ProgrammaticButtonConfig:
+    """ holds the basic info needed to build a tool button.
+    """
+    icon_path: str = field()
+    name: str = field()
+    
+    @property
+    def lower_name(self) -> str:
+        """The lower_name property."""
+        lower_name: str = self.name.lower()
+        lower_name = lower_name.replace(' ', '_')
+        return lower_name
+    
+    @property
+    def fn_name(self) -> str:
+        """The fn_name property."""
+        fn_name: str = f'on_click_{self.lower_name}'
+        return fn_name
+
+    def to_dict(self) -> Dict:
+        return asdict(self, filter=(lambda an_attr, attr_value: an_attr.name in ['icon_path', 'name']))
+
+
+def _create_on_click_fn(a_fn_name):
+    def an_on_click_fn(self, *args, **kwargs):
+        """ captures: a_fn_name """
+        print(f'{a_fn_name}(*args: {args or []}, **kwargs: {kwargs or {}})')
+        self.label_message = f'{a_fn_name}()'
+    return an_on_click_fn
+
+
+
+def build_programmatic_buttons(global_thin_button_bar_widget, button_config_dict):
+    """
+    
+    """
+    new_buttons_config_dict = {}
+    new_buttons_list = []
+    new_buttons_dict = {}
+
+    for k, a_btn_config_dict in button_config_dict.items():    
+        a_dummy_btn_config = ProgrammaticButtonConfig(**a_btn_config_dict)
+        new_buttons_config_dict[k] = a_dummy_btn_config
+
+        a_btn = build_button(**a_dummy_btn_config.to_dict(), parent=global_thin_button_bar_widget)
+        a_fn_name: str = a_dummy_btn_config.fn_name
+        
+        ## add the widget:
+        global_thin_button_bar_widget.horizontalLayout_ButtonContainer.addWidget(a_btn)
+        new_buttons_dict[k] = a_btn
+
+        def an_on_click_fn(self, *args, **kwargs):
+            """ captures: a_fn_name
+            """
+            print(f'{a_fn_name}(*args: {args or []}, **kwargs: {kwargs or {}})')
+            self.label_message = f'{a_fn_name}()'
+
+
+        ## add callback function to `global_thin_button_bar_widget`
+        # on_click_fn = an_on_click_fn 
+        # on_click_fn = pyqtExceptionPrintingSlot(a_dummy_btn_config.on_click_fn)
+        # on_click_fn = (lambda x, *args, **kwargs: an_on_click_fn(x, *args, **kwargs))
+        on_click_fn = _create_on_click_fn(a_fn_name)
+        
+        # ## Bind the method to the class
+        # bound_method = MethodType(on_click_fn, ThinButtonBarWidget) # add to type instead of instance
+        # print(f"bound_method: {bound_method}, type(bound_method): {type(bound_method)}")
+        # setattr(ThinButtonBarWidget, a_fn_name, bound_method)
+        # a_btn.pressed.connect(getattr(ThinButtonBarWidget, a_fn_name)) ## connect button
+        
+        ## Bind the method to the instance
+        bound_method = MethodType(on_click_fn, global_thin_button_bar_widget) # add to type instead of instance
+        print(f"bound_method: {bound_method}, type(bound_method): {type(bound_method)}")
+        setattr(global_thin_button_bar_widget, a_fn_name, bound_method)
+        a_btn.pressed.connect(getattr(global_thin_button_bar_widget, a_fn_name)) ## connect button
+        
+        
+    ## update the buttons properties:
+    global_thin_button_bar_widget.ui.buttons_list = new_buttons_list
+    global_thin_button_bar_widget.ui.buttons_dict = new_buttons_dict
+
+    return new_buttons_config_dict, new_buttons_dict
+
+
+
+# ==================================================================================================================== #
+# Main ThinButtonBarWidget widget                                                                                      #
+# ==================================================================================================================== #
 @metadata_attributes(short_name=None, tags=['ui', 'widget', 'button-bar'], input_requires=[], output_provides=[], uses=[], used_by=[], creation_date='2024-03-01 08:30', related_items=[])
 class ThinButtonBarWidget(QWidget):
     """ 
@@ -115,8 +231,27 @@ class ThinButtonBarWidget(QWidget):
         # currentTextChanged.connect(self.on_jump_combo_series_changed)
         pass
 
+    @classmethod
+    def clear_layout(cls, a_layout):
+        """ works to remove the programmatically added buttons """
+        while a_layout.count():
+            item = a_layout.takeAt(0)
+            widget = item.widget()
+            if widget is not None:
+                widget.deleteLater()
+            else:
+                a_layout.removeItem(item)
+            
 
+
+    def clear_all_buttons(self):
+        """ works to remove the programmatically added buttons """
+        self.clear_layout(a_layout=self.horizontalLayout_ButtonContainer)
+        ## remove stored buttons:
+        self.ui.buttons_list = []
+        self.ui.buttons_dict = {}
         
+            
     @pyqtExceptionPrintingSlot()
     def on_perform_refresh(self):
         """ 
