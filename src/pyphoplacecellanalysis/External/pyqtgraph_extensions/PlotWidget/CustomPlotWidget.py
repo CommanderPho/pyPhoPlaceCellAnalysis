251ab1638bb35b21882602685daf1995021cbd5e
Pho Hale <halechr@umich.edu>
2024-10-22 12:05:52 -0400

    CustomPlotWidget.item_data, self passed to signals

--- src/pyphoplacecellanalysis/External/pyqtgraph_extensions/PlotWidget/CustomPlotWidget.py
+++ src/pyphoplacecellanalysis/External/pyqtgraph_extensions/PlotWidget/CustomPlotWidget.py
@@ -1,6 +1,10 @@
 from PyQt5 import QtWidgets
 import pyqtgraph as pg
 import sys
+from copy import deepcopy
+from typing import Dict, List, Tuple, Optional, Callable, Union, Any
+from typing_extensions import TypeAlias
+from nptyping import NDArray
 
 import numpy as np
 from PyQt5.QtCore import *
@@ -21,13 +25,23 @@ class CustomPlotWidget(pg.PlotWidget):
     # sigTransformChanged = pyqtSignal(object)
 
     ## Signals from GraphicsScene
-    sigMouseHover = pyqtSignal(object) ## emits a list of objects hovered over
-    sigMouseMoved = pyqtSignal(object) ## emits position of mouse on every move
-    sigMouseClicked = pyqtSignal(object) ## emitted when mouse is clicked. Check for event.isAccepted() to see whether the event has already been acted on.
+    sigMouseHover = pyqtSignal(object, object) ## emits a list of objects hovered over
+    sigMouseMoved = pyqtSignal(object, object) ## emits position of mouse on every move
+    sigMouseClicked = pyqtSignal(object, object) ## emitted when mouse is clicked. Check for event.isAccepted() to see whether the event has already been acted on.
 
+    @property
+    def item_data(self) -> Dict:
+        """Data to identify this item."""
+        return self._item_data
+    @item_data.setter
+    def item_data(self, value):
+        self._item_data = value
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args, item_data: Optional[Dict]=None, **kwargs):
         super(CustomPlotWidget, self).__init__(*args, **kwargs)
+        if item_data is None:
+            item_data = {}
+        self._item_data = deepcopy(item_data)
         self.init_UI()
         
 
@@ -43,21 +57,22 @@ class CustomPlotWidget(pg.PlotWidget):
     def mousePressEvent(self, ev):
         """ not sure how this version interacts with the others. """
         super().mousePressEvent(ev)
-        self.sigMouseClicked.emit(ev)
-
+        # self.sigMouseClicked.emit(self, ev)
+        self.scene().sigMouseClicked.emit(ev)
+        
     def mouse_clicked(self, mouseClickEvent):
         # mouseClickEvent is a pyqtgraph.GraphicsScene.mouseEvents.MouseClickEvent
         print('clicked plot 0x{:x}, event: {}'.format(id(self), mouseClickEvent))
-        self.sigMouseClicked.emit(mouseClickEvent)
+        self.sigMouseClicked.emit(self, mouseClickEvent) # pass self
         
     def mouse_moved(self, evt):
         # mouseClickEvent is a pyqtgraph.GraphicsScene.mouseEvents.MouseClickEvent
         # print('clicked plot 0x{:x}, event: {}'.format(id(self), evt))
-        self.sigMouseMoved.emit(evt)
+        self.sigMouseMoved.emit(self, evt)
         
     def mouse_hover(self, evt):
         # mouseClickEvent is a pyqtgraph.GraphicsScene.mouseEvents.MouseClickEvent
         # print('clicked plot 0x{:x}, event: {}'.format(id(self), evt))
-        self.sigMouseHover.emit(evt)
+        self.sigMouseHover.emit(self, evt)
 
 
