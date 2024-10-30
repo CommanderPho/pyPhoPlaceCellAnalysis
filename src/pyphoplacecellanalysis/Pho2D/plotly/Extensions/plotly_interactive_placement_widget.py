9df4082917edf7fee3d879a6f7b5476daad81f89
Pho Hale <PhoHale@gmail.com>
2024-10-30 17:59:31 -0400

    converted PlotlyInteractivePlacementWidget to an attrs class

--- src/pyphoplacecellanalysis/Pho2D/plotly/Extensions/plotly_interactive_placement_widget.py
+++ src/pyphoplacecellanalysis/Pho2D/plotly/Extensions/plotly_interactive_placement_widget.py
@@ -4,8 +4,10 @@ import ipywidgets as widgets
 from ipywidgets import interact, interactive, interactive_output, Output
 from IPython.display import display, clear_output  # Imports for display and clearing output  # Import for the display function
 from pyphocorehelpers.programming_helpers import copy_to_clipboard
+from attrs import field, Factory, define
 
 
+@define(slots=False)
 class PlotlyInteractivePlacementWidget:
     """ an interactive widget with sliders and controls that allow you to test Plotly annotations parameters in real time
 
@@ -16,82 +18,126 @@ class PlotlyInteractivePlacementWidget:
         interactive_plot = PlotlyInteractivePlacementWidget()
         interactive_plot.run()
         
-    """    
-    def __init__(self, base_fig=None):
-        # Initialize the figure widget
-        
-        # Initialize default values
-        annotation_defaults = {
-            'text': "This is a footer label",
-            'x': 0.5,
-            'y': -0.1,
-            'xref': 'paper',
-            'yref': 'paper',
-            'showarrow': False,
-            'font': {'size': 10, 'color': 'gray'},
-            'textangle': 0,
-            'xanchor': 'center',
-            'yanchor': 'middle'
-        }
-        margin_defaults = {'t': 50, 'b': 100, 'l': 50, 'r': 50}
-        
-        if base_fig is not None:
-            print(f'using passed figure')
-            # self.fig = go.FigureWidget(data=base_fig.data, layout=base_fig.layout.to_plotly_json()) # , frames=base_fig.frames	
-            self.fig = go.FigureWidget(deepcopy(base_fig).to_plotly_json()) # , frames=base_fig.frames
+    """
+    base_fig: go.FigureWidget = field(default=None)
+    annotation_defaults: dict = field(default=Factory(lambda: {
+        'text': "This is a footer label",
+        'x': 0.5,
+        'y': -0.1,
+        'xref': 'paper',
+        'yref': 'paper',
+        'showarrow': False,
+        'font': {'size': 10, 'color': 'gray'},
+        'textangle': 0,
+        'xanchor': 'center',
+        'yanchor': 'middle'
+    }))
+    margin_defaults: dict = field(default=Factory(lambda: {'t': 50, 'b': 100, 'l': 50, 'r': 50}))
+    
+    # Widgets
+    x_widget: widgets.FloatSlider = field(init=False)
+    y_widget: widgets.FloatSlider = field(init=False)
+    xref_widget: widgets.Dropdown = field(init=False)
+    yref_widget: widgets.Dropdown = field(init=False)
+    font_size_widget: widgets.IntSlider = field(init=False)
+    text_widget: widgets.Text = field(init=False)
+    showarrow_widget: widgets.Checkbox = field(init=False)
+    textangle_widget: widgets.IntSlider = field(init=False)
+    xanchor_widget: widgets.Dropdown = field(init=False)
+    yanchor_widget: widgets.Dropdown = field(init=False)
+    margin_top_widget: widgets.IntSlider = field(init=False)
+    margin_bottom_widget: widgets.IntSlider = field(init=False)
+    margin_left_widget: widgets.IntSlider = field(init=False)
+    margin_right_widget: widgets.IntSlider = field(init=False)
+    print_button: widgets.Button = field(init=False)
+    reset_button: widgets.Button = field(init=False)
+    
+    active_annotation_index_widget: widgets.IntSlider = field(init=False)
 
-            #TODO 2024-10-29 17:47: - [ ] Get default values for widgets from the values in the passed figure.
-            # Get the first annotation if it exists
-            if self.fig.layout.annotations and len(self.fig.layout.annotations) > 0:
-                # annotation = self.fig.layout.annotations[0]
-                annotation = self.fig.layout.annotations[-1]
-                # Update defaults with values from annotation
-                for key in annotation_defaults.keys():
-                    if key in annotation:
-                        annotation_defaults[key] = annotation[key]
-                # For font size
-                if 'font' in annotation and 'size' in annotation.font:
-                    annotation_defaults['font']['size'] = annotation.font.size
-            else:
-                print("No annotations found in the passed figure. Using default values.")
-
-            # Get margins
-            if self.fig.layout.margin:
-                for side in ['t', 'b', 'l', 'r']:
-                    margin_value = getattr(self.fig.layout.margin, side, None)
-                    if margin_value is not None:
-                        margin_defaults[side] = margin_value
-                        
+    @property
+    def num_annotations(self) -> int:
+        """The num_annotations property."""
+        if not self.base_fig.layout.annotations:
+            return 0
         else:
-            print(f'no passed figure, generating a default figure.')
-            self.fig = go.FigureWidget()
-            self.fig.add_scatter(x=[1, 2, 3], y=[4, 5, 6])
-            # Use default values
+            return len(self.base_fig.layout.annotations)
+
+    @property
+    def active_annotation_index(self) -> int:
+        """The active_annotation_index property."""
+        return self.active_annotation_index_widget.value
+    @active_annotation_index.setter
+    def active_annotation_index(self, value):
+        self.active_annotation_index_widget.value = value        
+
+    @property
+    def active_annotation(self):
+        """The num_annotations property."""
+        active_annotation_index = self.active_annotation_index_widget.value
+        if active_annotation_index > 0:
+            if self.base_fig is not None:
+                if self.base_fig.layout.annotations and (len(self.base_fig.layout.annotations) > 0):
+                    active_annotation = self.base_fig.layout.annotations[active_annotation_index]
+                    # ## update defaults when annotation selection changes:
+                    # for key in self.annotation_defaults.keys():
+                    #     if key in active_annotation:
+                    #         self.annotation_defaults[key] = active_annotation[key]
+                    # if 'font' in active_annotation and 'size' in active_annotation.font:
+                    #     self.annotation_defaults['font']['size'] = active_annotation.font.size
+                    return active_annotation                        
+            else:
+                return None
+        else:
+            return None           
 
 
-        # Store defaults for reset
-        self.annotation_defaults = annotation_defaults
-        self.margin_defaults = margin_defaults
+    @classmethod
+    def init_with_figure_and_annotation_index(cls, base_fig, annotation_index: int):
+        _obj = cls(base_fig=base_fig)
+        _obj.active_annotation_index = annotation_index
+        return _obj
+    
+    
+    def __attrs_post_init__(self):
+        # Initialize the figure widget
+        if self.base_fig is not None:
+            self.base_fig = go.FigureWidget(deepcopy(self.base_fig).to_plotly_json())
+            if self.base_fig.layout.annotations and len(self.base_fig.layout.annotations) > 0:
+                annotation = self.base_fig.layout.annotations[self.active_annotation_index]
+                for key in self.annotation_defaults.keys():
+                    if key in annotation:
+                        self.annotation_defaults[key] = annotation[key]
+                if 'font' in annotation and 'size' in annotation.font:
+                    self.annotation_defaults['font']['size'] = annotation.font.size
+            if self.base_fig.layout.margin:
+                for side in ['t', 'b', 'l', 'r']:
+                    margin_value = getattr(self.base_fig.layout.margin, side, None)
+                    if margin_value is not None:
+                        self.margin_defaults[side] = margin_value
+        else:
+            self.base_fig = go.FigureWidget()
+            self.base_fig.add_scatter(x=[1, 2, 3], y=[4, 5, 6])
 
-        # Set up widgets for interactive adjustment with default values
-        self.x_widget = widgets.FloatSlider(min=0, max=1, step=0.01, value=annotation_defaults['x'], description="X Position")
-        self.y_widget = widgets.FloatSlider(min=-0.5, max=1.5, step=0.01, value=annotation_defaults['y'], description="Y Position")
-        self.xref_widget = widgets.Dropdown(options=['paper', 'x'], value=annotation_defaults['xref'], description="X Reference")
-        self.yref_widget = widgets.Dropdown(options=['paper', 'y'], value=annotation_defaults['yref'], description="Y Reference")
-        self.font_size_widget = widgets.IntSlider(min=5, max=30, step=1, value=annotation_defaults['font']['size'], description="Font Size")
-        self.text_widget = widgets.Text(value=annotation_defaults['text'], description="Text")
-        self.showarrow_widget = widgets.Checkbox(value=annotation_defaults['showarrow'], description="Show Arrow")
-        self.textangle_widget = widgets.IntSlider(min=-180, max=180, step=1, value=annotation_defaults['textangle'], description="Text Angle")
-        self.xanchor_widget = widgets.Dropdown(options=['auto', 'left', 'center', 'right'], value=annotation_defaults['xanchor'], description="X Anchor")
-        self.yanchor_widget = widgets.Dropdown(options=['auto', 'top', 'middle', 'bottom'], value=annotation_defaults['yanchor'], description="Y Anchor")
-        self.margin_top_widget = widgets.IntSlider(min=0, max=200, step=10, value=margin_defaults['t'], description="Top Margin")
-        self.margin_bottom_widget = widgets.IntSlider(min=0, max=200, step=10, value=margin_defaults['b'], description="Bottom Margin")
-        self.margin_left_widget = widgets.IntSlider(min=0, max=200, step=10, value=margin_defaults['l'], description="Left Margin")
-        self.margin_right_widget = widgets.IntSlider(min=0, max=200, step=10, value=margin_defaults['r'], description="Right Margin")
+        # Initialize widgets
+        self.x_widget = widgets.FloatSlider(min=0, max=1, step=0.01, value=self.annotation_defaults['x'], description="X Position")
+        self.y_widget = widgets.FloatSlider(min=-0.5, max=1.5, step=0.01, value=self.annotation_defaults['y'], description="Y Position")
+        self.xref_widget = widgets.Dropdown(options=['paper', 'x'], value=self.annotation_defaults['xref'], description="X Reference")
+        self.yref_widget = widgets.Dropdown(options=['paper', 'y'], value=self.annotation_defaults['yref'], description="Y Reference")
+        self.font_size_widget = widgets.IntSlider(min=5, max=30, step=1, value=self.annotation_defaults['font']['size'], description="Font Size")
+        self.text_widget = widgets.Text(value=self.annotation_defaults['text'], description="Text")
+        self.showarrow_widget = widgets.Checkbox(value=self.annotation_defaults['showarrow'], description="Show Arrow")
+        self.textangle_widget = widgets.IntSlider(min=-180, max=180, step=1, value=self.annotation_defaults['textangle'], description="Text Angle")
+        self.xanchor_widget = widgets.Dropdown(options=['auto', 'left', 'center', 'right'], value=self.annotation_defaults['xanchor'], description="X Anchor")
+        self.yanchor_widget = widgets.Dropdown(options=['auto', 'top', 'middle', 'bottom'], value=self.annotation_defaults['yanchor'], description="Y Anchor")
+        self.margin_top_widget = widgets.IntSlider(min=0, max=200, step=10, value=self.margin_defaults['t'], description="Top Margin")
+        self.margin_bottom_widget = widgets.IntSlider(min=0, max=200, step=10, value=self.margin_defaults['b'], description="Bottom Margin")
+        self.margin_left_widget = widgets.IntSlider(min=0, max=200, step=10, value=self.margin_defaults['l'], description="Left Margin")
+        self.margin_right_widget = widgets.IntSlider(min=0, max=200, step=10, value=self.margin_defaults['r'], description="Right Margin")
         self.print_button = widgets.Button(description="Print Annotation Kwargs")
         self.print_button.on_click(self.print_annotations_kwargs)
         self.reset_button = widgets.Button(description="Reset Values")
         self.reset_button.on_click(self.reset_values)
+        self.active_annotation_index_widget = widgets.IntSlider(min=0, max=(self.num_annotations-1), step=1, value=0, description="Annotation Label Index")
         
         # Set initial layout
         self.update_plot()
@@ -100,6 +146,7 @@ class PlotlyInteractivePlacementWidget:
         self.observe_widgets()
         
 
+
     def observe_widgets(self):
         # List of all widgets to observe
         widget_list = [
@@ -107,14 +154,34 @@ class PlotlyInteractivePlacementWidget:
             self.font_size_widget, self.text_widget, self.showarrow_widget,
             self.textangle_widget, self.xanchor_widget, self.yanchor_widget,
             self.margin_top_widget, self.margin_bottom_widget,
-            self.margin_left_widget, self.margin_right_widget
+            self.margin_left_widget, self.margin_right_widget, self.active_annotation_index_widget,
         ]
         for widget in widget_list:
             widget.observe(self.on_widget_change, names='value')
+        ## register special annotation widget one:
+        self.active_annotation_index_widget.observe(self.on_annotation_index_changed, names='value')
+        
     
     def on_widget_change(self, change):
         self.update_plot()
     
+
+    def on_annotation_index_changed(self, change):
+        """ todo: not yet called """
+        print(f'on_annotation_index_changed(change: {change})')
+        active_annotation_index = self.active_annotation_index_widget.value
+        
+        if self.base_fig is not None:
+            if self.base_fig.layout.annotations and (len(self.base_fig.layout.annotations) > 0):
+                active_annotation = self.base_fig.layout.annotations[active_annotation_index]
+                for key in self.annotation_defaults.keys():
+                    if key in active_annotation:
+                        self.annotation_defaults[key] = active_annotation[key]
+                if 'font' in active_annotation and 'size' in active_annotation.font:
+                    self.annotation_defaults['font']['size'] = active_annotation.font.size
+
+            
+
     def update_plot(self):
         # Extract current widget values
         x = self.x_widget.value
@@ -131,7 +198,8 @@ class PlotlyInteractivePlacementWidget:
         margin_bottom = self.margin_bottom_widget.value
         margin_left = self.margin_left_widget.value
         margin_right = self.margin_right_widget.value
-
+        active_annotation_index = self.active_annotation_index_widget.value
+        active_annotation = self.active_annotation
         # self.fig.update_annotations(row=1, col=1, text=text,
         #         x=x,
         #         y=y,
@@ -145,23 +213,54 @@ class PlotlyInteractivePlacementWidget:
         
     
         # Update the figure's annotations
-        self.fig.layout.annotations = [
-            dict(
-                text=text,
-                x=x,
-                y=y,
-                xref=xref,
-                yref=yref,
-                showarrow=showarrow,
-                font=dict(size=font_size, color='gray'),
-                textangle=textangle,
-                xanchor=xanchor,
-                yanchor=yanchor
-            )
-        ]
+        # self.base_fig.layout.annotations = [
+        #     dict(
+        #         text=text,
+        #         x=x,
+        #         y=y,
+        #         xref=xref,
+        #         yref=yref,
+        #         showarrow=showarrow,
+        #         font=dict(size=font_size, color='gray'),
+        #         textangle=textangle,
+        #         xanchor=xanchor,
+        #         yanchor=yanchor
+        #     )
+        # ]
+
+        # Convert annotations to a list for mutable operations
+        annotations = list(self.base_fig.layout.annotations)
+
+
+        # Update a specific annotation without overwriting the entire list
+        # Replace with the index of the annotation you want to update
+        new_annotation = dict(
+            text=text,
+            x=x,
+            y=y,
+            xref=xref,
+            yref=yref,
+            showarrow=showarrow,
+            font=dict(size=font_size, color='gray'),
+            textangle=textangle,
+            xanchor=xanchor,
+            yanchor=yanchor
+        )
+
+        # Update only the target annotation if it exists; otherwise, add it to the list
+        if len(annotations) > active_annotation_index:
+            annotations[active_annotation_index] = new_annotation
+        else:
+            annotations.append(new_annotation)
+                    
+        # Reassign the modified list back as a tuple to layout.annotations
+        self.base_fig.layout.annotations = tuple(annotations)
+
+
         # Update the figure's margins
-        self.fig.layout.margin = dict(t=margin_top, b=margin_bottom, l=margin_left, r=margin_right)
+        self.base_fig.layout.margin = dict(t=margin_top, b=margin_bottom, l=margin_left, r=margin_right)
     
+
     def run(self):
         # Arrange widgets in a vertical box
         ui = widgets.VBox([
@@ -180,10 +279,11 @@ class PlotlyInteractivePlacementWidget:
             self.margin_left_widget,
             self.margin_right_widget,
             self.print_button,
-            self.reset_button
+            self.reset_button,
+            self.active_annotation_index_widget,
         ])
         # Display the UI and the figure
-        display(ui, self.fig)
+        display(ui, self.base_fig)
     
     def print_annotations_kwargs(self, b=None):
         kwargs = {
@@ -232,7 +332,9 @@ class PlotlyInteractivePlacementWidget:
         self.margin_bottom_widget.value = self.margin_defaults['b']
         self.margin_left_widget.value = self.margin_defaults['l']
         self.margin_right_widget.value = self.margin_defaults['r']
-        
+        self.active_annotation_index_widget.disabled = (self.num_annotations == 0)
+        self.active_annotation_index_widget.value = 0
+        self.active_annotation_index_widget.max = (self.num_annotations-1)
 
 
 if __name__ == "__main__":
