c8c3a0ce959805b1b26be00568826589716d1ac8
Pho Hale <PhoHale@gmail.com>
2024-11-19 04:41:02 -0500

    enabled `job_suffix` for batch script generation

--- src/pyphoplacecellanalysis/General/Batch/pythonScriptTemplating.py
+++ src/pyphoplacecellanalysis/General/Batch/pythonScriptTemplating.py
@@ -238,7 +238,7 @@ class ProcessingScriptPhases(Enum):
 @function_attributes(short_name=None, tags=['slurm','jobs','files','batch'], input_requires=[], output_provides=[], uses=[], used_by=[], creation_date='2023-08-09 19:14', related_items=[])
 def generate_batch_single_session_scripts(global_data_root_parent_path, session_batch_basedirs: Dict[IdentifyingContext, Path], included_session_contexts: Optional[List[IdentifyingContext]], output_directory='output/gen_scripts/', use_separate_run_directories:bool=True,
          create_slurm_scripts:bool=False, create_non_slurm_bash_scripts:bool=False, should_create_vscode_workspace:bool=True, should_use_neptune_logging:bool=True,  should_generate_run_scripts = True, should_generate_figure_scripts = True, should_generate_run_notebooks: bool = False,
-         should_use_file_redirected_output_logging:bool=False, # , should_create_powershell_scripts:bool=True
+         job_suffix:Optional[str]=None, should_use_file_redirected_output_logging:bool=False, # , should_create_powershell_scripts:bool=True
          separate_execute_and_figure_gen_scripts:bool=True, should_perform_figure_generation_to_file:bool=False, force_recompute_override_computations_includelist: Optional[List[str]]=None, force_recompute_override_computation_kwargs_dict: Optional[Dict[str, Dict]]=None, 
         batch_session_completion_handler_kwargs=None, **renderer_script_generation_kwargs) -> BatchScriptsCollection:
     """ Creates a series of standalone scripts (one for each included_session_contexts) in the `output_directory`
@@ -267,10 +267,10 @@ def generate_batch_single_session_scripts(global_data_root_parent_path, session_
 
         
     """
-    def _subfn_build_slurm_script(curr_batch_script_rundir, a_python_script_path, a_curr_session_context, a_slurm_script_name_prefix:str='run', should_use_virtual_framebuffer:bool=False):
+    def _subfn_build_slurm_script(curr_batch_script_rundir, a_python_script_path, a_curr_session_context, a_slurm_script_name_prefix:str='run', should_use_virtual_framebuffer:bool=False, job_suffix=None):
         slurm_script_path = os.path.join(curr_batch_script_rundir, f'{a_slurm_script_name_prefix}_{a_curr_session_context}.sh')
         with open(slurm_script_path, 'w') as script_file:
-            script_content = slurm_template.render(curr_session_context=f"{a_curr_session_context}", python_script_path=a_python_script_path, curr_batch_script_rundir=curr_batch_script_rundir, should_use_virtual_framebuffer=should_use_virtual_framebuffer)
+            script_content = slurm_template.render(curr_session_context=f"{a_curr_session_context}", python_script_path=a_python_script_path, curr_batch_script_rundir=curr_batch_script_rundir, should_use_virtual_framebuffer=should_use_virtual_framebuffer, job_suffix=job_suffix)
             script_file.write(script_content)
         return slurm_script_path
     
@@ -369,11 +369,11 @@ def generate_batch_single_session_scripts(global_data_root_parent_path, session_
         # Create the SLURM script
         if create_slurm_scripts:
             if should_generate_run_scripts:
-                slurm_run_script_path = _subfn_build_slurm_script(a_python_script_path=python_script_path, curr_batch_script_rundir=curr_batch_script_rundir, a_curr_session_context=curr_session_context, a_slurm_script_name_prefix='run', should_use_virtual_framebuffer=False)
+                slurm_run_script_path = _subfn_build_slurm_script(a_python_script_path=python_script_path, curr_batch_script_rundir=curr_batch_script_rundir, a_curr_session_context=curr_session_context, a_slurm_script_name_prefix='run', should_use_virtual_framebuffer=False, job_suffix=job_suffix)
                 output_slurm_scripts['run'].append(slurm_run_script_path)
 
             if should_generate_figure_scripts or should_perform_figure_generation_to_file:
-                slurm_figure_script_path = _subfn_build_slurm_script(a_python_script_path=python_figures_script_path, curr_batch_script_rundir=curr_batch_script_rundir, a_curr_session_context=curr_session_context, a_slurm_script_name_prefix='figs', should_use_virtual_framebuffer=False)
+                slurm_figure_script_path = _subfn_build_slurm_script(a_python_script_path=python_figures_script_path, curr_batch_script_rundir=curr_batch_script_rundir, a_curr_session_context=curr_session_context, a_slurm_script_name_prefix='figs', should_use_virtual_framebuffer=False, job_suffix=job_suffix)
                 output_slurm_scripts['figs'].append(slurm_figure_script_path)
 
 
@@ -405,7 +405,14 @@ def generate_batch_single_session_scripts(global_data_root_parent_path, session_
 
 
             # script_dir = script_path.parent.resolve()
-            notebook_path = script_path.with_suffix('.ipynb')
+            print(F'job_suffix: "{job_suffix}"')
+            
+            if (job_suffix is not None) and (len(job_suffix) > 0):
+                # notebook_path = script_path.with_name(f'{script_path.name}_{job_suffix}').with_suffix(f'.ipynb')
+                notebook_path = script_path.with_stem(f'{script_path.stem}_{job_suffix}').with_suffix(f'.ipynb')
+            else:
+                notebook_path = script_path.with_suffix('.ipynb')
+                
             convert_script_to_notebook(_temp_notebook_python_script_path, notebook_path)
             ## remove temporary script when done
             try:
