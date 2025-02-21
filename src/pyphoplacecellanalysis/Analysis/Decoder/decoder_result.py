dab224b20320da8a2916c313aead1e61302b305a
Pho Hale <PhoHale@gmail.com>
2025-02-20 19:25:33 -0500

    docs

--- src/pyphoplacecellanalysis/Analysis/Decoder/decoder_result.py
+++ src/pyphoplacecellanalysis/Analysis/Decoder/decoder_result.py
@@ -44,6 +44,8 @@ from neuropy.utils.mixins.HDF5_representable import HDFMixin
 from pyphocorehelpers.indexing_helpers import find_neighbours
 from pyphocorehelpers.function_helpers import function_attributes
 from pyphocorehelpers.indexing_helpers import safe_np_vstack # for `_new_compute_surprise`
+from pyphocorehelpers.programming_helpers import metadata_attributes
+from pyphocorehelpers.function_helpers import function_attributes
 
 from pyphoplacecellanalysis.Analysis.Decoder.reconstruction import BayesianPlacemapPositionDecoder, DecodedFilterEpochsResult # perform_leave_one_aclu_out_decoding_analysis
 
@@ -119,6 +121,7 @@ class DecoderResultDisplayingBaseClass:
     def most_likely_positions(self):
         return self.decoder.most_likely_positions
 
+@metadata_attributes(short_name=None, tags=['matplotlib', 'figure'], input_requires=[], output_provides=[], uses=[], used_by=[], creation_date='2025-02-20 18:36', related_items=[])
 class DecoderResultDisplayingPlot2D(DecoderResultDisplayingBaseClass):
     """ Displays the decoder for 2D position.
 
