5ab856319bd7e8ac3e5fbddf8b51a8bc36940562
Pho Hale <PhoHale@gmail.com>
2025-03-07 21:24:56 -0500

    SpecificDockWidgetManipulatingMixin: implemented `.add_docked_decoded_posterior_track`, `.add_docked_marginal_track`

--- src/pyphoplacecellanalysis/GUI/PyQtPlot/DockingWidgets/SpecificDockWidgetManipulatingMixin.py
+++ src/pyphoplacecellanalysis/GUI/PyQtPlot/DockingWidgets/SpecificDockWidgetManipulatingMixin.py
@@ -1,4 +1,14 @@
+from copy import deepcopy
+from typing import Dict, List, Tuple, Optional, Callable, Union, Any
+from typing_extensions import TypeAlias
+from nptyping import NDArray
+import numpy as np
+import pandas as pd
+import pyphoplacecellanalysis.General.type_aliases as types
+from neuropy.utils.mixins.indexing_helpers import get_dict_subset
 from neuropy.utils.mixins.dynamic_conformance_updating_mixin import BaseDynamicInstanceConformingMixin
+from pyphocorehelpers.programming_helpers import metadata_attributes
+from pyphocorehelpers.function_helpers import function_attributes
 from pyphoplacecellanalysis.External.pyqtgraph.dockarea.Dock import Dock
 from pyphoplacecellanalysis.GUI.PyQtPlot.DockingWidgets.DynamicDockDisplayAreaContent import DynamicDockDisplayAreaOwningMixin, DynamicDockDisplayAreaContentMixin
 from pyphoplacecellanalysis.Analysis.Decoder.reconstruction import BasePositionDecoder, BayesianPlacemapPositionDecoder, DecodedFilterEpochsResult, Zhang_Two_Step
@@ -7,22 +17,221 @@ from neuropy.utils.mixins.AttrsClassHelpers import AttrsBasedClassHelperMixin, c
 from neuropy.utils.mixins.HDF5_representable import HDF_DeserializationMixin, post_deserialize, HDF_SerializationMixin, HDFMixin
 from neuropy.utils.mixins.indexing_helpers import UnpackableMixin
 from neuropy.utils.indexing_helpers import PandasHelpers
+from pyphoplacecellanalysis.General.Model.Configs.LongShortDisplayConfig import DisplayColorsEnum
+from pyphoplacecellanalysis.GUI.PyQtPlot.DockingWidgets.DynamicDockDisplayAreaContent import CustomDockDisplayConfig, CustomCyclicColorsDockDisplayConfig, NamedColorScheme
+from pyphoplacecellanalysis.General.Pipeline.Stages.ComputationFunctions.MultiContextComputationFunctions.DirectionalPlacefieldGlobalComputationFunctions import DirectionalDecodersContinuouslyDecodedResult, DecodedFilterEpochsResult
+from pyphoplacecellanalysis.Analysis.Decoder.reconstruction import SingleEpochDecodedResult
 
 
-
-class SpecificDockWidgetManipulatingMixin(BaseDynamicInstanceConformingMixin)
+class SpecificDockWidgetManipulatingMixin(BaseDynamicInstanceConformingMixin):
     """ Factors out the specific plots added to Spike2DRaster
 
     from pyphoplacecellanalysis.GUI.PyQtPlot.DockingWidgets.SpecificDockWidgetManipulatingMixin import SpecificDockWidgetManipulatingMixin
 
 
+    Variables:
+
+    self.dock_manager_widget: DynamicDockDisplayAreaContentMixin
+    self.ui.matplotlib_view_widgets: Dict
+    self.ui.connections['tracks']: Dict
+        
+
     """
+    @classmethod
+    def _perform_overlay_measured_position(cls, matplotlib_fig_axes, measured_position_df: pd.DataFrame, variable_name = 'x'):
+        """ 
+        
+        if a_position_decoder.pf.filtered_pos_df is not None:
+            measured_position_df = deepcopy(a_position_decoder.pf.filtered_pos_df)
+        else:
+            # fallback to session
+            measured_position_df = curr_active_pipeline.sess.position.to_dataframe()
+            
+        """
+        # identifier_name, widget, matplotlib_fig, matplotlib_fig_axes
+        actual_postion_plot_kwargs = {'color': '#ff000066', 'alpha': 0.35, 'marker': 'none', 'animated': False}
+        _out_artists = {}
+        for an_ax in matplotlib_fig_axes:                
+            line_measured_position = an_ax.plot(measured_position_df['t'].to_numpy(), measured_position_df[variable_name].to_numpy(), label=f'measured {variable_name}', **actual_postion_plot_kwargs) # Opaque RED # , linestyle='dashed', linewidth=2, color='#ff0000ff'
+            _out_artists[an_ax] = line_measured_position
+        return _out_artists
+
+
+    def add_docked_decoded_continuous_result_track(self, name: str):
+        pass
+
+    def add_docked_decoded_posterior_track(self, name: str, time_window_centers: NDArray, a_1D_posterior: NDArray, xbin: Optional[NDArray]=None, measured_position_df: Optional[pd.DataFrame]=None, a_variable_name: Optional[str]=None, a_dock_config: Optional[CustomDockDisplayConfig]=None, extended_dock_title_info: Optional[str]=None):
+        """ adds a decoded 1D posterior 
+
+        Aims to replace `AddNewDecodedPosteriors_MatplotlibPlotCommand._perform_add_new_decoded_posterior_row`
+        
+        
+        Usage 0:
+            time_bin_size = epochs_decoding_time_bin_size
+            info_string: str = f" - t_bin_size: {time_bin_size}"
+            identifier_name, widget, matplotlib_fig, matplotlib_fig_axes, dock_item = active_2d_plot.add_docked_decoded_posterior_track(name='non-PBE_marginal_over_track_ID',
+                                                                                                    time_window_centers=time_window_centers, a_1D_posterior=non_PBE_marginal_over_track_ID,
+                                                                                                    xbin = deepcopy(a_position_decoder.xbin), measured_position_df=deepcopy(curr_active_pipeline.sess.position.to_dataframe()),
+                                                                                                    extended_dock_title_info=info_string)
+                                                                                                    
+        Usage 1:
+            _out_tuple = active_2d_plot.add_docked_decoded_posterior_track(name=f'DirectionalDecodersDecoded[{a_decoder_name}]', a_dock_config=a_dock_config,
+                                                                                            time_window_centers=a_1D_continuous_decoded_result.time_bin_container.centers, a_1D_posterior=a_1D_continuous_decoded_result.p_x_given_n,
+                                                                                            xbin = deepcopy(a_1D_decoder.xbin), measured_position_df=deepcopy(curr_active_pipeline.sess.position.to_dataframe()),
+                                                                                            extended_dock_title_info=info_string)
+        """
+        from pyphoplacecellanalysis.General.Pipeline.Stages.DisplayFunctions.DecoderPredictionError import plot_1D_most_likely_position_comparsions
+        from neuropy.utils.matplotlib_helpers import get_heatmap_cmap
+        if a_variable_name is None:
+            a_variable_name = name
+
+        if a_dock_config is None:
+            override_dock_group_name: str = None ## this feature doesn't work
+            a_dock_config = CustomCyclicColorsDockDisplayConfig(showCloseButton=True, named_color_scheme=NamedColorScheme.grey)
+            a_dock_config.dock_group_names = [override_dock_group_name] # , 'non-PBE Continuous Decoding'
+
+
+        n_xbins, n_t_bins = np.shape(a_1D_posterior)
+
+        if xbin is None:
+            xbin = np.arange(n_xbins)
+
+        ## ✅ Add a new row for each of the four 1D directional decoders:
+        identifier_name: str = name
+        if extended_dock_title_info is not None:
+            identifier_name += extended_dock_title_info ## add extra info like the time_bin_size in ms
+        # print(f'identifier_name: {identifier_name}')
+        widget, matplotlib_fig, matplotlib_fig_axes, dock_item = self.add_new_matplotlib_render_plot_widget(name=identifier_name, dockSize=(65, 200), display_config=a_dock_config)
+        an_ax = matplotlib_fig_axes[0]
+
+        variable_name: str = a_variable_name
+        
+        # active_most_likely_positions = active_marginals.most_likely_positions_1D # Raw decoded positions
+        active_most_likely_positions = None
+        active_posterior = deepcopy(a_1D_posterior)
+        
+        posterior_heatmap_imshow_kwargs = dict(
+            cmap = get_heatmap_cmap(cmap='viridis', bad_color='black', under_color='white', over_color='red'),
+        )
+
+        # most_likely_positions_mode: 'standard'|'corrected'
+        ## Actual plotting portion:
+        fig, an_ax = plot_1D_most_likely_position_comparsions(measured_position_df=None, time_window_centers=time_window_centers, xbin=deepcopy(xbin),
+                                                                posterior=active_posterior,
+                                                                active_most_likely_positions_1D=active_most_likely_positions,
+                                                                ax=an_ax, variable_name=variable_name, debug_print=True, enable_flat_line_drawing=False,
+                                                                posterior_heatmap_imshow_kwargs=posterior_heatmap_imshow_kwargs)
+
+        ## Update the params
+        widget.params.variable_name = variable_name
+        widget.params.posterior_heatmap_imshow_kwargs = deepcopy(posterior_heatmap_imshow_kwargs)
+        widget.params.enable_flat_line_drawing = False
+        if extended_dock_title_info is not None:
+            widget.params.extended_dock_title_info = deepcopy(extended_dock_title_info)
+            
+        ## Update the plots_data - used for crosshairs tracing and other things
+        if time_window_centers is not None:
+            widget.plots_data.time_window_centers = deepcopy(time_window_centers)
+        if xbin is not None:
+            widget.plots_data.xbin = deepcopy(xbin)
+        if active_most_likely_positions is not None:
+            widget.plots_data.active_most_likely_positions = deepcopy(active_most_likely_positions)
+        widget.plots_data.variable_name = variable_name
+        if a_1D_posterior is not None:
+            widget.plots_data.matrix = deepcopy(active_posterior)
+        # if a_position_decoder is not None:
+        #     widget.plots_data.a_decoder = deepcopy(a_position_decoder)
+
+        ## measured positions
+        widget.plots_data.measured_position_df = None
+        widget.plots.measured_position_artists = None
+        if measured_position_df is not None:
+            widget.plots_data.measured_position_df = measured_position_df
+            _out_artists = self._perform_overlay_measured_position(matplotlib_fig_axes=[an_ax], measured_position_df=measured_position_df)
+            widget.plots.measured_position_artists = _out_artists
+
+
+        widget.draw() # alternative to accessing through full path?
+        self.sync_matplotlib_render_plot_widget(identifier_name) # Sync it with the active window:
+        return identifier_name, widget, matplotlib_fig, matplotlib_fig_axes, dock_item
+    
+
+    def add_docked_marginal_track(self, name: str, time_window_centers: NDArray, a_1D_posterior: NDArray, xbin: Optional[NDArray]=None, a_variable_name: Optional[str]=None, a_dock_config: Optional[CustomDockDisplayConfig]=None, extended_dock_title_info: Optional[str]=None):
+        """ adds a marginal 
+        
+        time_bin_size = epochs_decoding_time_bin_size
+        info_string: str = f" - t_bin_size: {time_bin_size}"
+        identifier_name, widget, matplotlib_fig, matplotlib_fig_axes, dock_item = active_2d_plot.add_docked_marginal_track(name='non-PBE_marginal_over_track_ID',
+                                                                                                time_window_centers=time_window_centers, a_1D_posterior=non_PBE_marginal_over_track_ID, extended_dock_title_info=info_string)
+        """
+        from pyphoplacecellanalysis.General.Pipeline.Stages.DisplayFunctions.DecoderPredictionError import plot_1D_most_likely_position_comparsions
+        from neuropy.utils.matplotlib_helpers import get_heatmap_cmap
+        if a_variable_name is None:
+            a_variable_name = name
+
+        if a_dock_config is None:
+            override_dock_group_name: str = None ## this feature doesn't work
+            a_dock_config = CustomCyclicColorsDockDisplayConfig(showCloseButton=True, named_color_scheme=NamedColorScheme.grey)
+            a_dock_config.dock_group_names = [override_dock_group_name] # , 'non-PBE Continuous Decoding'
+
+
+        n_xbins, n_t_bins = np.shape(a_1D_posterior)
+
+        if xbin is None:
+            xbin = np.arange(n_xbins)
+
+        ## ✅ Add a new row for each of the four 1D directional decoders:
+        identifier_name: str = name
+        if extended_dock_title_info is not None:
+            identifier_name += extended_dock_title_info ## add extra info like the time_bin_size in ms
+        print(f'identifier_name: {identifier_name}')
+        widget, matplotlib_fig, matplotlib_fig_axes, dock_item = self.add_new_matplotlib_render_plot_widget(name=identifier_name, dockSize=(25, 200), display_config=a_dock_config)
+        an_ax = matplotlib_fig_axes[0]
+
+        variable_name: str = a_variable_name
+        
+        # active_most_likely_positions = active_marginals.most_likely_positions_1D # Raw decoded positions
+        active_most_likely_positions = None
+        active_posterior = deepcopy(a_1D_posterior)
+        
+        posterior_heatmap_imshow_kwargs = dict()	
+        
+        # most_likely_positions_mode: 'standard'|'corrected'
+        ## Actual plotting portion:
+        fig, an_ax = plot_1D_most_likely_position_comparsions(measured_position_df=None, time_window_centers=time_window_centers, xbin=deepcopy(xbin),
+                                                                posterior=active_posterior,
+                                                                active_most_likely_positions_1D=active_most_likely_positions,
+                                                                ax=an_ax, variable_name=variable_name, debug_print=True, enable_flat_line_drawing=False,
+                                                                posterior_heatmap_imshow_kwargs=posterior_heatmap_imshow_kwargs)
+
+        ## Update the params
+        widget.params.variable_name = variable_name
+        widget.params.posterior_heatmap_imshow_kwargs = deepcopy(posterior_heatmap_imshow_kwargs)
+        widget.params.enable_flat_line_drawing = False
+        if extended_dock_title_info is not None:
+            widget.params.extended_dock_title_info = deepcopy(extended_dock_title_info)
+            
+        ## Update the plots_data
+        if time_window_centers is not None:
+            widget.plots_data.time_window_centers = deepcopy(time_window_centers)
+        if xbin is not None:
+            widget.plots_data.xbin = deepcopy(xbin)
+        if active_most_likely_positions is not None:
+            widget.plots_data.active_most_likely_positions = deepcopy(active_most_likely_positions)
+        widget.plots_data.variable_name = variable_name
+        if a_1D_posterior is not None:
+            widget.plots_data.matrix = deepcopy(a_1D_posterior)
+
+
+        widget.draw() # alternative to accessing through full path?
+        self.sync_matplotlib_render_plot_widget(identifier_name) # Sync it with the active window:
+        return identifier_name, widget, matplotlib_fig, matplotlib_fig_axes, dock_item
 
 
 
     @function_attributes(short_name=None, tags=['UNFINISHED', 'plotting', 'computing'], input_requires=[], output_provides=[], uses=['AddNewDecodedPosteriors_MatplotlibPlotCommand', '_perform_plot_multi_decoder_meas_pred_position_track'], used_by=[], creation_date='2025-02-13 14:58', related_items=['_perform_plot_multi_decoder_meas_pred_position_track'])
     @classmethod
-    def add_continuous_decoded_posterior(spike_raster_window, curr_active_pipeline, desired_time_bin_size: float, debug_print=True):
+    def add_continuous_decoded_posterior(cls, spike_raster_window, curr_active_pipeline, desired_time_bin_size: float, debug_print=True):
         """ computes the continuously decoded position posteriors (if needed) using the pipeline, then adds them as a new track to the SpikeRaster2D 
         
         Usage:
