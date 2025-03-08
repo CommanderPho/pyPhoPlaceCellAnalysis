4e1958bdf51d1c8e84d0b35fc1aaff2dc3052edb
Pho Hale <PhoHale@gmail.com>
2025-03-08 05:26:20 -0500

    SpecificDockWidgetManipulatingMixin.add_docked_decoded_posterior_slices_track

--- src/pyphoplacecellanalysis/GUI/PyQtPlot/DockingWidgets/SpecificDockWidgetManipulatingMixin.py
+++ src/pyphoplacecellanalysis/GUI/PyQtPlot/DockingWidgets/SpecificDockWidgetManipulatingMixin.py
@@ -161,6 +161,112 @@ class SpecificDockWidgetManipulatingMixin(BaseDynamicInstanceConformingMixin):
         
         return identifier_name, widget, matplotlib_fig, matplotlib_fig_axes, dock_item
     
+    def add_docked_decoded_posterior_slices_track(self, name: str, slices_time_window_centers: List[NDArray], slices_posteriors: List[NDArray], xbin: Optional[NDArray]=None, measured_position_df: Optional[pd.DataFrame]=None, a_variable_name: Optional[str]=None, a_dock_config: Optional[CustomDockDisplayConfig]=None, extended_dock_title_info: Optional[str]=None, should_defer_render:bool=False):
+        """ adds a decoded 1D posterior 
+
+        Aims to replace `AddNewDecodedPosteriors_MatplotlibPlotCommand._perform_add_new_decoded_posterior_row`
+        
+        
+        Usage 0:
+            time_bin_size = epochs_decoding_time_bin_size
+            info_string: str = f" - t_bin_size: {time_bin_size}"
+            identifier_name, widget, matplotlib_fig, matplotlib_fig_axes, dock_item = active_2d_plot.add_docked_decoded_posterior_slices_track(name='non-PBE_marginal_over_track_ID',
+                                                                                                    slices_time_window_centers=[v.centers for v in long_results_obj.time_bin_containers], slices_posteriors=long_results_obj.p_x_given_n_list,
+                                                                                                    xbin = deepcopy(a_position_decoder.xbin), measured_position_df=deepcopy(curr_active_pipeline.sess.position.to_dataframe()),
+                                                                                                    extended_dock_title_info=info_string)
+                                                                                                    
+        Usage 1:
+            _out_tuple = active_2d_plot.add_docked_decoded_posterior_slices_track(name=f'DirectionalDecodersDecoded[{a_decoder_name}]', a_dock_config=a_dock_config,
+                                                                                            slices_time_window_centers=a_1D_continuous_decoded_result.time_bin_container.centers, slices_posteriors=long_results_obj.p_x_given_n_list,
+                                                                                            xbin = deepcopy(a_1D_decoder.xbin), measured_position_df=deepcopy(curr_active_pipeline.sess.position.to_dataframe()),
+                                                                                            extended_dock_title_info=info_string)
+        """
+        from pyphoplacecellanalysis.General.Pipeline.Stages.DisplayFunctions.DecoderPredictionError import plot_slices_1D_most_likely_position_comparsions
+        from neuropy.utils.matplotlib_helpers import get_heatmap_cmap
+        if a_variable_name is None:
+            a_variable_name = name
+
+        if a_dock_config is None:
+            override_dock_group_name: str = None ## this feature doesn't work
+            a_dock_config = CustomCyclicColorsDockDisplayConfig(showCloseButton=True, named_color_scheme=NamedColorScheme.grey)
+            a_dock_config.dock_group_names = [override_dock_group_name] # , 'non-PBE Continuous Decoding'
+
+
+        # TODO: get flattened posterior instead of the first slice only
+        a_1D_posterior = slices_posteriors[0]
+        n_xbins, n_t_bins = np.shape(a_1D_posterior)
+
+        if xbin is None:
+            xbin = np.arange(n_xbins)
+
+        ## ✅ Add a new row for each of the four 1D directional decoders:
+        identifier_name: str = name
+        if extended_dock_title_info is not None:
+            identifier_name += extended_dock_title_info ## add extra info like the time_bin_size in ms
+        # print(f'identifier_name: {identifier_name}')
+        widget, matplotlib_fig, matplotlib_fig_axes, dock_item = self.add_new_matplotlib_render_plot_widget(name=identifier_name, dockSize=(65, 200), display_config=a_dock_config)
+        an_ax = matplotlib_fig_axes[0]
+
+        variable_name: str = a_variable_name
+        
+        # active_most_likely_positions = active_marginals.most_likely_positions_1D # Raw decoded positions
+        slices_active_most_likely_positions_1D = None
+        
+        posterior_heatmap_imshow_kwargs = dict(
+            cmap = get_heatmap_cmap(cmap='viridis', bad_color='black', under_color='white', over_color='red'),
+        )
+
+        # most_likely_positions_mode: 'standard'|'corrected'
+        ## Actual plotting portion:
+        fig, an_ax, out_img_list = plot_slices_1D_most_likely_position_comparsions(measured_position_df=None, slices_time_window_centers=slices_time_window_centers, xbin=deepcopy(xbin),
+                                                        slices_posteriors=slices_posteriors,
+                                                        slices_active_most_likely_positions_1D=slices_active_most_likely_positions_1D,
+                                                        ax=an_ax, variable_name=variable_name, debug_print=True, enable_flat_line_drawing=False,
+                                                        posterior_heatmap_imshow_kwargs=posterior_heatmap_imshow_kwargs)
+        
+
+
+        ## Update the params
+        widget.params.variable_name = variable_name
+        widget.params.posterior_heatmap_imshow_kwargs = deepcopy(posterior_heatmap_imshow_kwargs)
+        widget.params.enable_flat_line_drawing = False
+        if extended_dock_title_info is not None:
+            widget.params.extended_dock_title_info = deepcopy(extended_dock_title_info)
+            
+        ## Update the plots_data - used for crosshairs tracing and other things
+        if slices_time_window_centers is not None:
+            widget.plots_data.slices_time_window_centers = deepcopy(slices_time_window_centers)
+        if xbin is not None:
+            widget.plots_data.xbin = deepcopy(xbin)
+        if slices_active_most_likely_positions_1D is not None:
+            widget.plots_data.slices_active_most_likely_positions_1D = deepcopy(slices_active_most_likely_positions_1D)
+        widget.plots_data.variable_name = variable_name
+        if slices_posteriors is not None:
+            widget.plots_data.slices_posteriors = deepcopy(slices_posteriors)
+            
+        if a_1D_posterior is not None:
+            widget.plots_data.matrix = deepcopy(a_1D_posterior)
+        # if a_position_decoder is not None:
+        #     widget.plots_data.a_decoder = deepcopy(a_position_decoder)
+
+        ## measured positions
+        widget.plots_data.measured_position_df = None
+        widget.plots.measured_position_artists = None
+        if measured_position_df is not None:
+            widget.plots_data.measured_position_df = measured_position_df
+            _out_artists = self._perform_overlay_measured_position(matplotlib_fig_axes=[an_ax], measured_position_df=measured_position_df)
+            widget.plots.measured_position_artists = _out_artists
+
+
+        if not should_defer_render:
+            widget.draw() # alternative to accessing through full path?
+        # end if not should_defer_render
+        self.sync_matplotlib_render_plot_widget(identifier_name) # Sync it with the active window:
+        
+        return identifier_name, widget, matplotlib_fig, matplotlib_fig_axes, dock_item
+    
+
+
     def add_docked_decoded_posterior_track_from_result(self, name: str, a_1D_decoded_result: Union[SingleEpochDecodedResult, DecodedFilterEpochsResult], xbin: Optional[NDArray]=None, measured_position_df: Optional[pd.DataFrame]=None, **kwargs):
             """ adds a decoded 1D posterior from a decoded result
 
@@ -181,8 +287,20 @@ class SpecificDockWidgetManipulatingMixin(BaseDynamicInstanceConformingMixin):
                                                                                                 xbin = deepcopy(a_1D_decoder.xbin), measured_position_df=deepcopy(curr_active_pipeline.sess.position.to_dataframe()),
                                                                                                 extended_dock_title_info=info_string)
             """
-            return self.add_docked_decoded_posterior_track(name=name, time_window_centers=a_1D_decoded_result.time_bin_container.centers, a_1D_posterior=a_1D_decoded_result.p_x_given_n, xbin=xbin, measured_position_df=measured_position_df, **kwargs)
-        
+            if isinstance(a_1D_decoded_result, SingleEpochDecodedResult):
+                return self.add_docked_decoded_posterior_track(name=name, time_window_centers=a_1D_decoded_result.time_bin_container.centers, a_1D_posterior=a_1D_decoded_result.p_x_given_n, xbin=xbin, measured_position_df=measured_position_df, **kwargs)
+            elif isinstance(a_1D_decoded_result, DecodedFilterEpochsResult):
+                ## multiple
+                if a_1D_decoded_result.num_filter_epochs == 1:
+                    ## equivalent to a single result
+                    a_1D_decoded_result = a_1D_decoded_result.get_result_for_epoch(0) ## will be a `SingleEpochDecodedResult`
+                    return self.add_docked_decoded_posterior_track(name=name, time_window_centers=a_1D_decoded_result.time_bin_container.centers, a_1D_posterior=a_1D_decoded_result.p_x_given_n, xbin=xbin, measured_position_df=measured_position_df, **kwargs)
+                else:
+                    ## more than one
+                    return self.add_docked_decoded_posterior_slices_track(name=name, slices_time_window_centers=[v.centers for v in a_1D_decoded_result.time_bin_containers], slices_posteriors=a_1D_decoded_result.p_x_given_n_list, xbin=xbin, measured_position_df=measured_position_df, **kwargs)
+                    
+            else:
+                raise NotImplementedError(f'type(a_1D_decoded_result): {type(a_1D_decoded_result)} is unknown')
 
 
     def add_docked_marginal_track(self, name: str, time_window_centers: NDArray, a_1D_posterior: NDArray, xbin: Optional[NDArray]=None, a_variable_name: Optional[str]=None, a_dock_config: Optional[CustomDockDisplayConfig]=None, extended_dock_title_info: Optional[str]=None):
@@ -257,6 +375,10 @@ class SpecificDockWidgetManipulatingMixin(BaseDynamicInstanceConformingMixin):
         return identifier_name, widget, matplotlib_fig, matplotlib_fig_axes, dock_item
 
 
+
+    # ==================================================================================================================== #
+    # Multiple Tracks at once:                                                                                             #
+    # ==================================================================================================================== #
     def add_docked_decoded_results_dict_tracks(self, name: str, a_decoded_result_dict: Dict[str, SingleEpochDecodedResult], dock_configs: Dict[str, CustomDockDisplayConfig], pf1D_Decoder_dict: Dict[str, BasePositionDecoder], measured_position_df: Optional[pd.DataFrame]=None, **kwargs):
         """
         info_string: str = f'{active_time_bin_size:.2f}'
@@ -316,18 +438,9 @@ class SpecificDockWidgetManipulatingMixin(BaseDynamicInstanceConformingMixin):
             all_time_bin_sizes_output_dict: Dict[float, Dict[types.DecoderName, SingleEpochDecodedResult]] = directional_decoders_decode_result.split_pseudo2D_continuous_result_to_1D_continuous_result()
             a_split_pseudo2D_continuous_result_to_1D_continuous_result_dict: Dict[types.DecoderName, SingleEpochDecodedResult] = all_time_bin_sizes_output_dict.get(desired_time_bin_size, None)
             
-
             assert a_continuously_decoded_dict is not None, f"a_continuously_decoded_dict is None even after recomputing!"
             assert a_split_pseudo2D_continuous_result_to_1D_continuous_result_dict is not None, f"a_split_pseudo2D_continuous_result_to_1D_continuous_result_dict is None even after recomputing!"
-            # if a_continuously_decoded_dict is None:
-            #     ## recompute
-            #     curr_active_pipeline.perform_specific_computation(computation_functions_name_includelist=['directional_decoders_decode_continuous'], computation_kwargs_list=[{'time_bin_size': desired_time_bin_size, 'should_disable_cache': False}], enabled_filter_names=None, fail_on_exception=True, debug_print=False)
-            #     directional_decoders_decode_result: DirectionalDecodersContinuouslyDecodedResult = curr_active_pipeline.global_computation_results.computed_data['DirectionalDecodersDecoded']
-            #     pseudo2D_decoder: BasePositionDecoder = directional_decoders_decode_result.pseudo2D_decoder
-            #     all_directional_pf1D_Decoder_dict: Dict[str, BasePositionDecoder] = directional_decoders_decode_result.pf1D_Decoder_dict
-            #     a_continuously_decoded_dict: Dict[str, DecodedFilterEpochsResult] = directional_decoders_decode_result.continuously_decoded_result_cache_dict.get(desired_time_bin_size, None)
-            #     assert a_continuously_decoded_dict is not None, f"a_continuously_decoded_dict is None even after recomputing!"
-            info_string: str = f" - t_bin_size: {desired_time_bin_size:.2f}"
+            info_string: str = f" - t_bin_size: {desired_time_bin_size:.3f}"
 
         except (KeyError, AttributeError) as e:
             # KeyError: 'DirectionalDecodersDecoded'
@@ -340,46 +453,12 @@ class SpecificDockWidgetManipulatingMixin(BaseDynamicInstanceConformingMixin):
             raise
 
 
-        # # output_dict = _cmd.prepare_and_perform_add_pseudo2D_decoder_decoded_epoch_marginals(curr_active_pipeline=_cmd._active_pipeline, active_2d_plot=active_2d_plot, continuously_decoded_dict=deepcopy(a_continuously_decoded_dict), info_string=info_string, **enable_rows_config_kwargs)
-        # output_dict = AddNewDecodedPosteriors_MatplotlibPlotCommand.prepare_and_perform_add_add_pseudo2D_decoder_decoded_epochs(curr_active_pipeline=curr_active_pipeline, active_2d_plot=active_2d_plot, continuously_decoded_dict=deepcopy(a_continuously_decoded_dict), info_string=info_string, a_pseudo2D_decoder=pseudo2D_decoder, debug_print=debug_print, **kwargs)
-        # for a_key, an_output_tuple in output_dict.items():
-        #     identifier_name, widget, matplotlib_fig, matplotlib_fig_axes, dDisplayItem = an_output_tuple                
-        #     # if a_key not in all_time_bin_sizes_output_dict:
-        #     #     all_time_bin_sizes_output_dict[a_key] = [] ## init empty list
-        #     # all_time_bin_sizes_output_dict[a_key].append(an_output_tuple)
-            
-        #     assert (identifier_name not in flat_all_time_bin_sizes_output_tuples_dict), f"identifier_name: {identifier_name} already in flat_all_time_bin_sizes_output_tuples_dict: {list(flat_all_time_bin_sizes_output_tuples_dict.keys())}"
-        #     flat_all_time_bin_sizes_output_tuples_dict[identifier_name] = an_output_tuple
-
-
-
         # ==================================================================================================================== #
         # PLOTTING                                                                                                             #
         # ==================================================================================================================== #
-        # assert a_continuously_decoded_dict is not None
-
-        ## INPUTS: most_recent_continuously_decoded_dict: Dict[str, DecodedFilterEpochsResult], info_string
-        
-        # all_directional_continuously_decoded_dict = most_recent_continuously_decoded_dict or {}
-        # pseudo2D_decoder_continuously_decoded_result: DecodedFilterEpochsResult = a_continuously_decoded_dict.get('pseudo2D', None)
-        # assert len(pseudo2D_decoder_continuously_decoded_result.p_x_given_n_list) == 1
-        # p_x_given_n = pseudo2D_decoder_continuously_decoded_result.p_x_given_n_list[0]
-        # # p_x_given_n = pseudo2D_decoder_continuously_decoded_result.p_x_given_n_list[0]['p_x_given_n']
-        # # time_bin_containers = pseudo2D_decoder_continuously_decoded_result.time_bin_containers[0]
-        # # time_window_centers = time_bin_containers.centers
-        # # p_x_given_n.shape # (62, 4, 209389)
-
-        # ## Split across the 2nd axis to make 1D posteriors that can be displayed in separate dock rows:
-        # assert p_x_given_n.shape[1] == 4, f"expected the 4 pseudo-y bins for the decoder in p_x_given_n.shape[1]. but found p_x_given_n.shape: {p_x_given_n.shape}"
-        # split_pseudo2D_posteriors_dict = {k:np.squeeze(p_x_given_n[:, i, :]) for i, k in enumerate(('long_LR', 'long_RL', 'short_LR', 'short_RL'))}
-
         dock_group_sep_character: str = '_'
         showCloseButton = True
         _common_dock_config_kwargs = {'dock_group_names': [dock_group_sep_character.join([f'ContinuousDecode', info_string])], 'showCloseButton': showCloseButton}
-        # dock_configs: Dict[str, CustomDockDisplayConfig] = dict(zip(('long_LR', 'long_RL', 'short_LR', 'short_RL'), (CustomDockDisplayConfig(custom_get_colors_callback_fn=DisplayColorsEnum.Laps.get_LR_dock_colors, **_common_dock_config_kwargs), CustomDockDisplayConfig(custom_get_colors_callback_fn=DisplayColorsEnum.Laps.get_RL_dock_colors, **_common_dock_config_kwargs),
-        #                 CustomDockDisplayConfig(custom_get_colors_callback_fn=DisplayColorsEnum.Laps.get_LR_dock_colors, **_common_dock_config_kwargs), CustomDockDisplayConfig(custom_get_colors_callback_fn=DisplayColorsEnum.Laps.get_RL_dock_colors, **_common_dock_config_kwargs))))
-
-        # info_string: str = f'{desired_time_bin_size:.2f}'
         dock_configs: Dict[str, CustomDockDisplayConfig] = dict(zip(('long_LR', 'long_RL', 'short_LR', 'short_RL'),
                                 (CustomDockDisplayConfig(custom_get_colors_callback_fn=DisplayColorsEnum.Laps.get_LR_dock_colors, **_common_dock_config_kwargs),
                                 CustomDockDisplayConfig(custom_get_colors_callback_fn=DisplayColorsEnum.Laps.get_RL_dock_colors, **_common_dock_config_kwargs),
@@ -389,19 +468,9 @@ class SpecificDockWidgetManipulatingMixin(BaseDynamicInstanceConformingMixin):
         output_dict = self.add_docked_decoded_results_dict_tracks(name=f'ContinuousDecode', a_decoded_result_dict=a_split_pseudo2D_continuous_result_to_1D_continuous_result_dict, dock_configs=dock_configs, pf1D_Decoder_dict=all_directional_pf1D_Decoder_dict,
                                                                                                     measured_position_df=deepcopy(curr_active_pipeline.sess.position.to_dataframe()),
                                                                                                     extended_dock_title_info=info_string)
+        ## layout the dockGroups:
+        nested_dock_items, nested_dynamic_docked_widget_container_widgets = self.ui.dynamic_docked_widget_container.layout_dockGroups()
         
-
-        # # Need all_directional_pf1D_Decoder_dict
-        # output_dict = {}
-
-        # for a_decoder_name, a_1D_posterior in split_pseudo2D_posteriors_dict.items():
-        #     a_dock_config = dock_configs[a_decoder_name]
-        #     _out_tuple = cls._perform_add_new_decoded_posterior_row(curr_active_pipeline=curr_active_pipeline, active_2d_plot=self, a_dock_config=a_dock_config, a_decoder_name=a_decoder_name, a_position_decoder=a_pseudo2D_decoder,
-        #                                                              time_window_centers=time_window_centers, a_1D_posterior=a_1D_posterior, extended_dock_title_info=info_string)
-        #     # identifier_name, widget, matplotlib_fig, matplotlib_fig_axes, dDisplayItem = _out_tuple
-        #     output_dict[a_decoder_name] = _out_tuple
-        
-
         # OUTPUTS: output_dict
         return output_dict
 
