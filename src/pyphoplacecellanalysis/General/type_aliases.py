e1db158f8936f8c52b4a2b5577c7ab48e846d640
Pho Hale <PhoHale@gmail.com>
2025-03-17 14:30:17 -0400

    factored all filtering constants out to `pyphoplacecellanalysis/General/type_aliases`

--- src/pyphoplacecellanalysis/General/type_aliases.py
+++ src/pyphoplacecellanalysis/General/type_aliases.py
@@ -1,9 +1,10 @@
-from typing import Dict, List, Tuple, Optional, Callable, Union, Any
+from typing import Dict, List, Tuple, Optional, Callable, Union, Any, Literal
 from typing_extensions import TypeAlias  # "from typing_extensions" in Python 3.9 and earlier
 from typing import NewType
 import nptyping as ND
 from nptyping import NDArray
 from neuropy.utils.type_aliases import aclu_index, DecoderName ## other imports?
+from neuropy.utils.result_context import IdentifyingContext
 
 """ Usage:
 
@@ -14,7 +15,6 @@ from nptyping import NDArray
 # import neuropy.utils.type_aliases as types
 import pyphoplacecellanalysis.General.type_aliases as types
 
-
 e.g. Dict[types.FilterContextName, Dict[types.ComputationFunctionName: CapturedException]]
 """
 
@@ -24,6 +24,14 @@ e.g. Dict[types.FilterContextName, Dict[types.ComputationFunctionName: CapturedE
 """
 FilterContextName: TypeAlias = str # a string identifier of a specific filtering context -- 'maze1_odd', 'maze2_odd', 'maze_odd' -- used in `.get_failed_computations(...)`
 ComputationFunctionName: TypeAlias = str # a string identifier of a computation function -- can be either the long or short name -- '_split_to_directional_laps' -- used in `.get_failed_computations(...)`
+
+KnownNamedDecoderTrainedComputeEpochsType = Literal['laps', 'non_pbe']
+KnownNamedDecodingEpochsType = Literal['laps', 'replay', 'ripple', 'pbe', 'non_pbe']
+# Define a type that can only be one of these specific strings
+MaskedTimeBinFillType = Literal['ignore', 'last_valid', 'nan_filled', 'dropped'] ## used in `DecodedFilterEpochsResult.mask_computed_DecodedFilterEpochsResult_by_required_spike_counts_per_time_bin(...)` to specify how invalid bins (due to too few spikes) are treated.
+DataTimeGrain = Literal['per_epoch', 'per_time_bin']
+
+GenericResultTupleIndexType: TypeAlias = IdentifyingContext # an template/stand-in variable that aims to abstract away the unique-hashable index of a single result computed with a given set of parameters. Not yet fully implemented 2025-03-09 17:50 
 
 
 # FilterContextName = NewType('FilterContextName', str) 
