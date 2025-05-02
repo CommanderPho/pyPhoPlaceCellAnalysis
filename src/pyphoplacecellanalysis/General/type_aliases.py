3a63b50d610ac6bda368ffa8b509dc6cd6da19b1
Pho Hale <PhoHale@gmail.com>
2025-05-02 05:52:17 -0400

    added df column names

--- src/pyphoplacecellanalysis/General/type_aliases.py
+++ src/pyphoplacecellanalysis/General/type_aliases.py
@@ -26,10 +26,10 @@ FilterContextName: TypeAlias = str # a string identifier of a specific filtering
 ComputationFunctionName: TypeAlias = str # a string identifier of a computation function -- can be either the long or short name -- '_split_to_directional_laps' -- used in `.get_failed_computations(...)`
 
 KnownNamedDecoderTrainedComputeEpochsType = Literal['laps', 'non_pbe']
-KnownNamedDecodingEpochsType = Literal['laps', 'replay', 'ripple', 'pbe', 'non_pbe', 'non_pbe_endcaps', 'global']
+KnownNamedDecodingEpochsType = Literal['laps', 'replay', 'ripple', 'pbe', 'non_pbe', 'non_pbe_endcaps', 'global'] # 'known_named_decoding_epochs_type'
 # Define a type that can only be one of these specific strings
 MaskedTimeBinFillType = Literal['ignore', 'last_valid', 'nan_filled', 'dropped'] ## used in `DecodedFilterEpochsResult.mask_computed_DecodedFilterEpochsResult_by_required_spike_counts_per_time_bin(...)` to specify how invalid bins (due to too few spikes) are treated.
-DataTimeGrain = Literal['per_epoch', 'per_time_bin']
+DataTimeGrain = Literal['per_epoch', 'per_time_bin'] # 'data_grain'
 PrePostDeltaCategory = Literal['pre_delta', 'post_delta']
 
 
