3e905ae002b3d5bd37c68638fa9a3d054b1fa1ab
Pho Hale <PhoHale@gmail.com>
2025-05-30 04:46:10 -0400

    color and greys

--- src/pyphoplacecellanalysis/General/Pipeline/Stages/ComputationFunctions/MultiContextComputationFunctions/DirectionalPlacefieldGlobalComputationFunctions.py
+++ src/pyphoplacecellanalysis/General/Pipeline/Stages/ComputationFunctions/MultiContextComputationFunctions/DirectionalPlacefieldGlobalComputationFunctions.py
@@ -8336,7 +8336,7 @@ class DirectionalPlacefieldGlobalDisplayFunctions(AllFunctionEnumeratingMixin, m
             ## use this default instead:
             custom_export_formats: Dict[str, HeatmapExportConfig] = {
                 'greyscale': HeatmapExportConfig.init_greyscale(),
-                # 'color': HeatmapExportConfig(colormap='Oranges', desired_height=400), # #TODO 2025-05-30 03:45: - [ ] previous config
+                'color': HeatmapExportConfig(colormap='Oranges', desired_height=400), # #TODO 2025-05-30 03:45: - [ ] previous config
                 # 'color': HeatmapExportConfig(colormap=additional_cmaps['long_LR']),
                 # 'color': HeatmapExportConfig(colormap=cmap1, desired_height=200),
             }
