8004407517ba6fc73b943663b4a92d50d136fd78
Pho Hale <CommanderPho@users.noreply.github.com>
2026-03-28 05:26:12 -0400

    CanvasEventHandlerMixin - unused

--- src/pyphoplacecellanalysis/Pho2D/vispy/vispy_widgets.py
+++ src/pyphoplacecellanalysis/Pho2D/vispy/vispy_widgets.py
@@ -399,6 +399,98 @@ class VispyCanvasContainingWindow(PhoDockAreaContainingWindow):
         self.setWindowTitle(title)
 
 
+
+class CanvasEventHandlerMixin: # Canvas(app.Canvas):
+    """ Implementors respond to app.Canvas events
+    SceneCanvas
+    
+    from pyphoplacecellanalysis.Pho2D.vispy.vispy_widgets import CanvasEventHandlerMixin
+    
+    
+    """
+    def on_close(self, event):
+        print('closing!')
+
+    def on_resize(self, event):
+        print('Resize %r' % (event.size, ))
+
+    def on_key_press(self, event):
+        modifiers = [key.name for key in event.modifiers]
+        print('Key pressed - text: %r, key: %s, modifiers: %r' % (
+            event.text, event.key.name, modifiers))
+
+    def on_key_release(self, event):
+        modifiers = [key.name for key in event.modifiers]
+        print('Key released - text: %r, key: %s, modifiers: %r' % (
+            event.text, event.key.name, modifiers))
+
+    def on_mouse_press(self, event):
+        self.print_mouse_event(event, 'Mouse press')
+
+    def on_mouse_release(self, event):
+        self.print_mouse_event(event, 'Mouse release')
+
+    def on_mouse_move(self, event):
+        self.print_mouse_event(event, 'Mouse move')
+
+    def on_mouse_wheel(self, event):
+        self.print_mouse_event(event, 'Mouse wheel')
+
+    def print_mouse_event(self, event, what):
+        modifiers = ', '.join([key.name for key in event.modifiers])
+        print('%s - pos: %r, button: %s, modifiers: %s, delta: %r' %
+              (what, event.pos, event.button, modifiers, event.delta))
+
+    # def on_draw(self, event):
+    #     gloo.clear(color=True, depth=True)
+        
+
+# class CustomCanvas(CanvasEventHandlerMixin, app.Canvas):
+#     """ Implementors respond to app.Canvas events
+#     SceneCanvas
+#     """
+#     def __init__(self, *args, **kwargs):
+#         app.Canvas.__init__(self, *args, **kwargs)
+#         self.title = 'CustomCanvas'
+
+#     def on_close(self, event):
+#         print('closing!')
+
+#     def on_resize(self, event):
+#         print('Resize %r' % (event.size, ))
+
+#     def on_key_press(self, event):
+#         modifiers = [key.name for key in event.modifiers]
+#         print('Key pressed - text: %r, key: %s, modifiers: %r' % (
+#             event.text, event.key.name, modifiers))
+
+#     def on_key_release(self, event):
+#         modifiers = [key.name for key in event.modifiers]
+#         print('Key released - text: %r, key: %s, modifiers: %r' % (
+#             event.text, event.key.name, modifiers))
+
+#     def on_mouse_press(self, event):
+#         self.print_mouse_event(event, 'Mouse press')
+
+#     def on_mouse_release(self, event):
+#         self.print_mouse_event(event, 'Mouse release')
+
+#     def on_mouse_move(self, event):
+#         self.print_mouse_event(event, 'Mouse move')
+
+#     def on_mouse_wheel(self, event):
+#         self.print_mouse_event(event, 'Mouse wheel')
+
+#     def print_mouse_event(self, event, what):
+#         modifiers = ', '.join([key.name for key in event.modifiers])
+#         print('%s - pos: %r, button: %s, modifiers: %s, delta: %r' %
+#               (what, event.pos, event.button, modifiers, event.delta))
+
+#     def on_draw(self, event):
+#         gloo.clear(color=True, depth=True)
+        
+
+
 class VispySceneWrappingWidget(QtWidgets.QWidget):  # type: ignore[misc]
     """Composite Qt widget: vispy ``SceneCanvas.native`` plus an optional ``VispySceneTreeWidget``.
 
